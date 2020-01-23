058511fe3ba9dd3dd8bd5352e87d67c2cf31228e
Ladislas de Toldi <ladislas@detoldi.me>
2020-01-23 17:24:03 +0100

    Add main.cpp with Hello, World!

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "mbed.h"
+
+uint8_t myCount = 0;
+
+int main(void) {
+
+	while (true) {
+		printf("%d - Hello, World!\n", myCount);
+        myCount++;
+		rtos::ThisThread::sleep_for(1000);
+	}
+
+	return 0;
+
+}
