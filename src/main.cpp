c92725781e0b227d5b0a4cef015c059d74931f08
Ladislas de Toldi <ladislas@detoldi.me>
2020-09-17 10:56:20 +0200

    Clang format + add license

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
-#include "mbed.h"
+// Leka - LekaOS
+// Copyright 2020 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
 #include "HelloWorld.h"
+#include "mbed.h"
 
 HelloWorld hello;
 
@@ -8,8 +12,8 @@ static BufferedSerial serial(USBTX, USBRX, 9600);
 constexpr uint8_t buff_size = 128;
 char buff[buff_size] {};
 
-int main(void) {
-
+int main(void)
+{
 	auto start = Kernel::Clock::now();
 
 	printf("\nHello, Investigation Day!\n\n");
@@ -19,8 +23,9 @@ int main(void) {
 	hello.start();
 
 	while (true) {
-		auto t = Kernel::Clock::now() - start;
-		int length = sprintf(buff, "A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME, hello.world, int(t.count()/1000));
+		auto t	   = Kernel::Clock::now() - start;
+		int length = sprintf(buff, "A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME,
+							 hello.world, int(t.count() / 1000));
 		serial.write(buff, length);
 		rtos::ThisThread::sleep_for(1s);
 	}
