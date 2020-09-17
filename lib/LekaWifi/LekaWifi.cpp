bcfa7729d519338d65631c1bac6be58b6096e9ad
Yann Locatelli <yann.locatelli2@gmail.com>
2020-09-17 10:29:19 +0200

    Add LekaWifi files

---
+++ lib/LekaWifi/LekaWifi.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file LekaWifi.cpp
+ * @author Yann Locatelli
+ *
+ * @version 0.0
+ * @date 2020-09-17
+ *
+ * @copyright Copyright (c) 2020
+ */
+
+#include "LekaWifi.h"
---
+++ lib/LekaWifi/LekaWifi.h
@@ -0,0 +1,15 @@
+/**
+ * @file LekaWifi.h
+ * @author Yann Locatelli
+ *
+ * @version 0.0
+ * @date 2020-09-17
+ *
+ * @copyright Copyright (c) 2020
+ */
+#ifndef _LK_OS_MBED_LEKAWIFI_H_
+#define _LK_OS_MBED_LEKAWIFI_H_
+
+#include "mbed.h"
+
+#endif
