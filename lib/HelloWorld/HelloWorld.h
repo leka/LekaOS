dca9e4ce1bf2b54b4d2c344ad62df7fdff9677c6
Ladislas de Toldi <ladislas@detoldi.me>
2020-09-28 22:36:17 +0200

    Refactor, use tickers and improve comments

--- lib/HelloWorld/HelloWorld.h
+++ lib/HelloWorld/HelloWorld.h
@@ -1,53 +1,45 @@
-/**
- * @file HelloWorld.h
- * @author Ladislas de Toldi (ladislas [at] detoldi.me)
- *
- * @version 1.0
- * @date 2019-09-09
- *
- * @copyright Copyright (c) 2019
- *
- */
-#ifndef _LK_ALPHA_OS_MBED_HELLOWORLD_H_
-#define _LK_ALPHA_OS_MBED_HELLOWORLD_H_
+// Mbed CMake Template
+// Copyright 2020 Ladislas de Toldi (ladislas [at] detoldi.me)
+// SPDX-License-Identifier: Apache-2.0
 
-#include "mbed.h"
+#ifndef _LEKA_OS_LIB_HELLOWORLD_H_
+#define _LEKA_OS_LIB_HELLOWORLD_H_
+
 #include "chrono"
+#include "mbed.h"
 
-/**
- * @class HelloWorld
- * @brief This class is used to for debugging purpose to make sure the program is still running
- */
-
-class HelloWorld {
-
-	/**
-	 * @brief Struct used to hold led pin & sleep time information
-	 */
+///
+/// @class HelloWorld
+/// @brief This class is used to for debugging purpose to make sure the program is still running
+///
+class HelloWorld
+{
+  public:
+	///
+	/// @brief Struct used to hold led pin & sleep time information
+	///
 	struct Data {
 		DigitalOut led;
 		chrono::milliseconds sleepTime;
 	};
 
-  public:
 	HelloWorld(void);
 	~HelloWorld(void) {};
 
 	void start(void);
 
-	char const *world = "Hello, World!";
+	char const *world = "Hello, Mbed CMake Template!";
 
   private:
-	Thread thread1;
-	Thread thread2;
-	Thread thread3;
+	LowPowerTicker flipper1;
+	LowPowerTicker flipper2;
+	LowPowerTicker flipper3;
 
 	struct Data led1Data = {DigitalOut(LED1), 500ms};
 	struct Data led2Data = {DigitalOut(LED2), 1000ms};
 	struct Data led3Data = {DigitalOut(LED3), 1500ms};
 
 	static void blink(Data *data);
-
 };	 // class HelloWorld
 
-#endif
+#endif	 // _LEKA_OS_LIB_HELLOWORLD_H_
