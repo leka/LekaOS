4afd88b677bc0631e7032e275930bcfc0cf8416b
Yann Locatelli <yann.locatelli2@gmail.com>
2020-12-03 17:07:24 +0100

    Audio - main.cpp with Interface definition

---
+++ spikes/lk_audio/main.cpp
@@ -0,0 +1,41 @@
+// Leka - LekaOS
+// Copyright 2020 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "mbed.h"
+
+#include "PinNames.h"
+
+#include "HelloWorld.h"
+
+using namespace leka;
+
+HelloWorld hello;
+
+AnalogOut audio_output(MCU_SOUND_OUT);
+DigitalOut audio_enable(SOUND_ENABLE, 1);
+
+static BufferedSerial serial(USBTX, USBRX, 9600);
+
+constexpr uint8_t buff_size = 128;
+char buff[buff_size] {};
+
+int main(void)
+{
+	auto start = Kernel::Clock::now();
+
+	printf("\nHello LekaOS, Audio test!\n\n");
+
+	rtos::ThisThread::sleep_for(2s);
+
+	hello.start();
+
+	while (true) {
+		auto t	   = Kernel::Clock::now() - start;
+		int length = sprintf(buff, "A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME,
+							 hello.world, int(t.count() / 1000));
+		serial.write(buff, length);
+	}
+
+	return 0;
+}
