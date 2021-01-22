adc34467a6200bdbff7a6d262c5e1bad7233a950
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-22 11:13:02 +0100

    :white_check_mark: (stubs): Add PwmOut and gpio_api stubs

---
+++ tests/unit/mbed-os/stubs/stub_PwmOut.cpp
@@ -0,0 +1,24 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "PwmOut.h"
+
+namespace mbed {
+
+PwmOut::PwmOut(PinName)
+{
+	return;
+}
+
+PwmOut::~PwmOut()
+{
+	return;
+}
+
+void PwmOut::write(float value)
+{
+	return;
+}
+
+}	// namespace mbed
---
+++ tests/unit/mbed-os/stubs/stub_gpio_api.cpp
@@ -0,0 +1,15 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "gpio_api.h"
+
+void gpio_init_out(gpio_t *gpio, PinName pin)
+{
+	return;
+}
+
+void gpio_write(gpio_t *gpio, int value)
+{
+	return;
+}
