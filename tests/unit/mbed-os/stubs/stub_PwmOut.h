43baa59c7b007f6104a6b0f4dfa6f32fe0b0ad29
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-22 11:13:02 +0100

    :white_check_mark: (stubs): Add PwmOut and gpio_api stubs

---
+++ tests/unit/mbed-os/stubs/stub_PwmOut.h
@@ -0,0 +1,12 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "PwmOut.h"
+
+namespace leka {
+
+extern float spy_PwmOut_value;
+float spy_PwmOut_getValue();
+
+}	// namespace leka
