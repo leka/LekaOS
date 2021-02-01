fef4ccf5e7ea72c82ef887a3842c87190b9d1931
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-01 16:41:54 +0100

    :sparkles: (cert): Add emc_ble_bt_lcd_led_motors
    :sparkles: (cert): Add cert2 from emc_lcd_leds_mot
    
    :sparkles: (cert): Battery and Still alive message
    - Add batteries level
    - Duration with hours and minutes
    
    :sparkles: (cert): Add BLE
    - BLE Beacon, named LekaBeacon
    - Add delay of 10s to check ble mac address
    - Add std:: before chrono due to ambiguous use
    
    :sparkles: (cert): Add Audio
    - Play wav file on haptic actuator
    - Need to speed up SD frequency (25000000Hz)
    
    :sparkles: (cert) Add bluetooth in main loop
    - Message via bluetooth
    - Can play audio via bluetooth
    
    :sparkles: (cert): Add RFID main loop
    - Remove a printf in RFID lib
    
    :sparkles: (cert): Add Touch main loop
    
    :sparkles: (cert): Choice of features
    - Use of #define for features and #if/#endif to apply

---
+++ tests/functional/cert/emc_ble_bt_lcd_led_motors/MotorsUtils.h
@@ -0,0 +1,55 @@
+// Leka - LekaOS
+// Copyright 2020 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_SPIKE_MOTORS_UTILS_H_
+#define _LEKA_OS_SPIKE_MOTORS_UTILS_H_
+
+#include "Motor.h"
+
+using namespace leka;
+
+DigitalOut motor_right_dir_1(PE_4);
+DigitalOut motor_right_dir_2(PE_5);
+PwmOut motor_right_speed(PA_2);
+
+DigitalOut motor_left_dir_1(PC_13);
+DigitalOut motor_left_dir_2(PI_8);
+PwmOut motor_left_speed(PA_1);
+
+Motor motor_right(motor_right_dir_1, motor_right_dir_2, motor_right_speed);
+Motor motor_left(motor_left_dir_1, motor_left_dir_2, motor_left_speed);
+
+void spinLeft()
+{
+	motor_right.spin(Rotation::clockwise, 0.5f);
+	motor_left.spin(Rotation::clockwise, 0.5f);
+}
+
+void spinRight()
+{
+	motor_right.spin(Rotation::counterClockwise, 0.5f);
+	motor_left.spin(Rotation::counterClockwise, 0.5f);
+}
+
+void stop()
+{
+	motor_right.stop();
+	motor_left.stop();
+}
+
+void motor_thread()
+{
+	while (true) {
+		spinLeft();
+		rtos::ThisThread::sleep_for(5s);
+
+		spinRight();
+		rtos::ThisThread::sleep_for(5s);
+
+		stop();
+		rtos::ThisThread::sleep_for(5s);
+	}
+}
+
+#endif	 // _LEKA_OS_SPIKE_MOTORS_UTILS_H_
---
+++ tests/functional/cert/emc_ble_bt_lcd_led_motors/main.cpp
@@ -0,0 +1,165 @@
+/* CHOICE OF FEATURES */
+#define USE_LEDS	  1
+#define USE_RFID	  1
+#define USE_TOUCH	  1
+#define USE_MOTORS	  1
+#define USE_HAPTIC	  1
+#define USE_SD		  1
+#define USE_SCREEN	  1
+#define USE_BLE		  1
+#define USE_BLUETOOTH 1
+
+#if USE_BLUETOOTH
+	#define USE_BLUETOOTH_AS_SERIAL 1
+#endif
+/* END OF CHOICE OF FEATURES */
+
+#include "mbed.h"
+
+#if USE_BLE
+	#include "BLEUtils.h"
+Thread thread_ble;
+#endif
+
+#if USE_HAPTIC
+DigitalOut audio_enable(SOUND_ENABLE, 1);
+	#if USE_SD and !USE_BLUETOOTH
+		#include "AudioUtils.h"
+Thread thread_audio;
+AnalogOut audio_output(MCU_SOUND_OUT);
+	#endif
+#endif
+
+#if USE_SD
+	#include "FileManager.h"
+leka::FileManager sd_card;
+#endif
+
+#if USE_LEDS
+	#include "LedUtils.h"
+Thread thread_led;
+#endif
+
+#if USE_BLUETOOTH
+	#include "LekaBluetooth.h"
+Thread thread_bluetooth;
+Bluetooth leka_bluetooth;
+#endif
+
+#if USE_SCREEN
+	#include "LekaScreen.h"
+Thread thread_lcd;
+Screen lcd;
+#endif
+
+#if USE_TOUCH
+	#include "LekaTouch.h"
+Touch leka_touch;
+#endif
+
+#if USE_MOTORS
+	#include "MotorsUtils.h"
+Thread thread_motors;
+#endif
+
+#if USE_RFID
+	#include "RFIDUtils.h"
+char buff_rfid[40] {};
+#endif
+
+#include "WatchdogUtils.h"
+
+Thread thread_watchdog;
+
+AnalogIn batteries_level(BATTERY_VOLTAGE);
+
+static BufferedSerial serial(USBTX, USBRX, 9600);
+
+constexpr uint8_t buff_size = 128;
+char buff[buff_size] {};
+
+Watchdog &watchdog = Watchdog::get_instance();
+
+int main(void)
+{
+	printf("\nHello, Investigation Day!\n\n");
+
+	auto start	  = Kernel::Clock::now();
+	auto duration = Kernel::Clock::now() - start;
+
+	watchdog.start(5000);
+	thread_watchdog.start(watchdog_thread);
+
+#if USE_BLUETOOTH
+	thread_bluetooth.start({&leka_bluetooth, &Bluetooth::start});
+#endif
+
+#if USE_MOTORS
+	thread_motors.start(motor_thread);
+#endif
+
+#if USE_SCREEN
+	thread_lcd.start({&lcd, &Screen::start});
+#endif
+
+#if USE_LEDS
+	initLed();
+	rtos::ThisThread::sleep_for(2s);
+	thread_led.start(led_thread);
+#endif
+
+#if USE_HAPTIC and USE_SD and !USE_BLUETOOTH
+	audio_pause_duration = 20s;
+	thread_audio.start(callback(playSoundPeriodically, &audio_output));
+#endif
+
+#if USE_BLE
+	BLE &ble = BLE::Instance();
+	ble.onEventsToProcess(schedule_ble_events);
+	BeaconDemo ble_beacon(ble, event_queue);
+	thread_ble.start({&ble_beacon, &BeaconDemo::start});
+#endif
+
+#if USE_RFID
+	initRfid();
+#endif
+
+	rtos::ThisThread::sleep_for(10s);
+	while (true) {
+		duration   = Kernel::Clock::now() - start;
+		int length = sprintf(buff, "Leka is still alive after: %2i:%2i:%2i\nBattery at 0x%X\n\n",
+							 int(std::chrono::duration_cast<std::chrono::hours>(duration).count()),
+							 int(std::chrono::duration_cast<std::chrono::minutes>(duration).count()) % 60,
+							 int(std::chrono::duration_cast<std::chrono::seconds>(duration).count()) % 60,
+							 batteries_level.read_u16());
+#if USE_BLUETOOTH_AS_SERIAL
+		leka_bluetooth.sendMessage(buff, length);
+#else
+		serial.write(buff, length);
+#endif
+
+#if USE_TOUCH
+		length = sprintf(buff, "Touch value: %x | LF(+1) LB(+2) RB(+4) RF(+8) LE(+10) RE(+20)\n",
+						 leka_touch.updateSensorsStatus());
+	#if USE_BLUETOOTH_AS_SERIAL
+		leka_bluetooth.sendMessage(buff, length);
+	#else
+		serial.write(buff, length);
+	#endif
+#endif
+
+#if USE_RFID
+		getRfid(buff_rfid);
+		length = snprintf(buff, buff_size, "RFID response is %s\n\n", buff_rfid);
+	#if USE_BLUETOOTH_AS_SERIAL
+		leka_bluetooth.sendMessage(buff, length);
+	#else
+		serial.write(buff, length);
+	#endif
+#endif
+
+		rtos::ThisThread::sleep_for(1s);
+	}
+
+	return 0;
+}
