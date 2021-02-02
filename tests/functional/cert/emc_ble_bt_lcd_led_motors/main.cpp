6fadd87a295a1847505cbfdfb4b19bb9eabbfb77
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-02 15:38:53 +0100

    Investigation Day 2 - Choice by #if

--- tests/functional/cert/emc_ble_bt_lcd_led_motors/main.cpp
+++ tests/functional/cert/emc_ble_bt_lcd_led_motors/main.cpp
@@ -1,25 +1,51 @@
+#define USE_LEDS	  1
+#define USE_RFID	  1
+#define USE_TOUCH	  1
+#define USE_MOTORS	  1
+#define USE_HAPTIC	  1
+#define USE_SD		  1
+#define USE_SCREEN	  1
+#define USE_BLE		  1
+#define USE_BLUETOOTH 0
+
 #include "mbed.h"
 
-#include "BLEUtils.h"
-
-#include "AudioUtils.h"
-#include "FileManager.h"
-#include "LedUtils.h"
-#include "LekaScreen.h"
-#include "MotorsUtils.h"
-#include "WatchdogUtils.h"
-
-Thread thread_audio;
-Thread thread_led;
-Thread thread_motors;
-Thread thread_watchdog;
-Thread thread_lcd;
+#if USE_BLE
+	#include "BLEUtils.h"
 Thread thread_ble;
+#endif
 
-FileManager sd_card;
-DigitalOut audio_enable(SOUND_ENABLE, 1);
+#if USE_HAPTIC and USE_SD and !USE_BLUETOOTH
+	#include "AudioUtils.h"
+Thread thread_audio;
 AnalogOut audio_output(MCU_SOUND_OUT);
+#endif
+
+#if USE_SD
+	#include "FileManager.h"
+leka::FileManager sd_card;
+#endif
+
+#if USE_LEDS
+	#include "LedUtils.h"
+Thread thread_led;
+#endif
+
+#if USE_SCREEN
+	#include "LekaScreen.h"
+Thread thread_lcd;
 Screen lcd;
+#endif
+
+#if USE_MOTORS
+	#include "MotorsUtils.h"
+Thread thread_motors;
+#endif
+
+#include "WatchdogUtils.h"
+Thread thread_watchdog;
+
+DigitalOut audio_enable(SOUND_ENABLE, 1);
 AnalogIn batteries_level(BATTERY_VOLTAGE);
 
 static BufferedSerial serial(USBTX, USBRX, 9600);
@@ -36,24 +62,34 @@ int main(void)
 	auto start	  = Kernel::Clock::now();
 	auto duration = Kernel::Clock::now() - start;
 
+#if USE_MOTORS
 	thread_motors.start(motor_thread);
+#endif
 
+#if USE_SCREEN
 	thread_lcd.start({&lcd, &Screen::start});
+#endif
 
 	watchdog.start(5000);
 	thread_watchdog.start(watchdog_thread);
 
+#if USE_LEDS
 	initLed();
 	rtos::ThisThread::sleep_for(2s);
 	thread_led.start(led_thread);
+#endif
 
+#if USE_HAPTIC and !USE_BLUETOOTH
 	audio_pause_duration = 20s;
 	thread_audio.start(callback(playSoundPeriodically, &audio_output));
+#endif
 
+#if USE_BLE
 	BLE &ble = BLE::Instance();
 	ble.onEventsToProcess(schedule_ble_events);
 	BeaconDemo ble_beacon(ble, event_queue);
 	thread_ble.start({&ble_beacon, &BeaconDemo::start});
+#endif
 
 	rtos::ThisThread::sleep_for(10s);
 	while (true) {
@@ -63,7 +99,10 @@ int main(void)
 							 int(std::chrono::duration_cast<std::chrono::minutes>(duration).count()) % 60,
 							 int(std::chrono::duration_cast<std::chrono::seconds>(duration).count()) % 60,
 							 batteries_level.read_u16());
+#if USE_BLUETOOTH
+#else
 		serial.write(buff, length);
+#endif
 		rtos::ThisThread::sleep_for(1s);
 	}
 
