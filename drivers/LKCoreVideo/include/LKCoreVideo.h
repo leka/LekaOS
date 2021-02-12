516dd93ef0fedcfa9bb03633d626249991487617
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-12 11:34:53 +0100

    :sparkles: Add LKCoreVideo driver (empty)

---
+++ drivers/LKCoreVideo/include/LKCoreVideo.h
@@ -0,0 +1,16 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_DRIVER_LKCOREVIDEO_H_
+#define _LEKA_OS_DRIVER_LKCOREVIDEO_H_
+
+namespace leka {
+
+class LKCoreVideo
+{
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_DRIVER_LKCOREVIDEO_H_
