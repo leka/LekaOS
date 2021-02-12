9c490a45bed962e38e3760eb5002ad4bd82bbeae
YannL <yann.locatelli2@gmail.com>
2021-02-12 13:44:30 +0100

    Apply suggestions from code review
    Co-authored-by: Ladislas de Toldi <ladislas@detoldi.me>

--- drivers/LKCoreVideo/source/LKCoreDMA2D.cpp
+++ drivers/LKCoreVideo/source/LKCoreDMA2D.cpp
@@ -12,8 +12,8 @@ LKCoreDMA2D::LKCoreDMA2D()
 	_hdma2d.Init.Mode		   = DMA2D_M2M_PFC;
 	_hdma2d.Init.ColorMode	   = DMA2D_OUTPUT_ARGB8888;
 	_hdma2d.Init.OutputOffset  = 0;
-	_hdma2d.Init.AlphaInverted = DMA2D_REGULAR_ALPHA; /* No Output Alpha Inversion*/
-	_hdma2d.Init.RedBlueSwap   = DMA2D_RB_REGULAR;	  /* No Output Red & Blue swap */
+	_hdma2d.Init.AlphaInverted = DMA2D_REGULAR_ALPHA;	// No Output Alpha Inversion
+	_hdma2d.Init.RedBlueSwap   = DMA2D_RB_REGULAR;		// No Output Red & Blue swap
 
 	// MARK: Configure DMA2D background
 	_hdma2d.LayerCfg[0].AlphaMode	   = DMA2D_REPLACE_ALPHA;
@@ -21,8 +21,8 @@ LKCoreDMA2D::LKCoreDMA2D()
 	_hdma2d.LayerCfg[0].InputColorMode = DMA2D_INPUT_ARGB8888;
 
 	_hdma2d.LayerCfg[0].InputOffset	  = 0;
-	_hdma2d.LayerCfg[0].RedBlueSwap	  = DMA2D_RB_REGULAR;	 /* No Background Red/Blue swap */
-	_hdma2d.LayerCfg[0].AlphaInverted = DMA2D_REGULAR_ALPHA; /* No Background Alpha inversion */
+	_hdma2d.LayerCfg[0].RedBlueSwap	  = DMA2D_RB_REGULAR;	   // No Background Red/Blue swap
+	_hdma2d.LayerCfg[0].AlphaInverted = DMA2D_REGULAR_ALPHA;   // No Background Alpha inversion
 
 	// MARK: Configure DMA2D foreground
 	_hdma2d.LayerCfg[1].AlphaMode	   = DMA2D_REPLACE_ALPHA;
@@ -30,8 +30,8 @@ LKCoreDMA2D::LKCoreDMA2D()
 	_hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_ARGB8888;
 
 	_hdma2d.LayerCfg[1].InputOffset	  = 0;
-	_hdma2d.LayerCfg[1].RedBlueSwap	  = DMA2D_RB_REGULAR;	 /* No ForeGround Red/Blue swap */
-	_hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA; /* No ForeGround Alpha inversion */
+	_hdma2d.LayerCfg[1].RedBlueSwap	  = DMA2D_RB_REGULAR;	   // No ForeGround Red/Blue swap
+	_hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA;   // No ForeGround Alpha inversion
 
 	// MARK: Set DMA2D instance
 	_hdma2d.Instance = DMA2D;
@@ -40,6 +40,8 @@ LKCoreDMA2D::LKCoreDMA2D()
 void LKCoreDMA2D::initialize()
 {
 	// MARK: Initializer DMA2D
+	// This part **must not** be moved to the constructor as LCD
+	// initialization must be performed in a very specific order
 	HAL_DMA2D_Init(&_hdma2d);
 	HAL_DMA2D_ConfigLayer(&_hdma2d, 0);
 	HAL_DMA2D_ConfigLayer(&_hdma2d, 1);
