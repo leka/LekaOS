3caa06e775b3b35c03008651f5aaf62ce8cfaaa9
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-12 12:11:49 +0100

    :sparkles: (drivers): Add DMA2D driver
    - Initialization and use imported from example of STM32CubeF7
    
    :adhesive_bandage: (drivers): Apply suggestion CR
    
    - Use Mark
    - Rename files with capital letters
    - Underscore prefix for private members
    - Remove commented code for callback
    
    Co-authored-by: Ladislas de Toldi <ladislas@detoldi.me>
    
    :building_construction: Remove static + move handler init in constructor
    
    Apply suggestions from code review
    
    Co-authored-by: Ladislas de Toldi <ladislas@detoldi.me>

---
+++ drivers/LKCoreVideo/include/internal/LKCoreDMA2D.h
@@ -0,0 +1,36 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_DMA2D_H_
+#define _LEKA_OS_LIB_DMA2D_H_
+
+#include "stm32f7xx_hal.h"
+
+namespace leka {
+
+class LKCoreDMA2D
+{
+  public:
+	enum class Mode
+	{
+		M2M,		 // Memory-to-memory for 2D memory copy operations
+		M2M_PFC,	 // Memory-to-memory with pixel format conversion for bitmap drawing with format conversion
+		M2M_BLEND,	 // Memory-to-memory with pixel format conversion and blending for text drawing with transparency
+		R2M			 // Register-to-memory for rectangle filling operations
+	};
+
+	LKCoreDMA2D();
+
+	void initialize();
+
+	void load(Mode mode, uint32_t pdata_or_color, uint32_t destination_address, uint16_t xsize, uint16_t ysize,
+			  uint16_t output_offset, uint32_t width_offset = 0);
+
+  private:
+	DMA2D_HandleTypeDef _hdma2d;
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_LIB_DMA2D_H_
