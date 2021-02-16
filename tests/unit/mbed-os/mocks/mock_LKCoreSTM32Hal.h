6d9372e955bce04c09ef768c7f6310de6c633a7c
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-16 22:45:57 +0100

    :clown_face: (mocks): Add LKCoreSTM32HAL mock

---
+++ tests/unit/mbed-os/mocks/mock_LKCoreSTM32Hal.h
@@ -0,0 +1,28 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_STM32_HAL_MOCK_H_
+#define _LEKA_OS_STM32_HAL_MOCK_H_
+
+#include "LKCoreSTM32Hal.h"
+#include "gmock/gmock.h"
+
+namespace leka {
+
+class LKCoreSTM32HalMock : public LKCoreSTM32Hal
+{
+  public:
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DMA2D_Init, (DMA2D_HandleTypeDef * hdma2d), (override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DMA2D_ConfigLayer, (DMA2D_HandleTypeDef * hdma2d, uint32_t LayerIdx),
+				(override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DMA2D_Start,
+				(DMA2D_HandleTypeDef * hdma2d, uint32_t pdata, uint32_t DstAddress, uint32_t Width, uint32_t Height),
+				(override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DMA2D_PollForTransfer, (DMA2D_HandleTypeDef * hdma2d, uint32_t Timeout),
+				(override));
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_STM32_HAL_H_
