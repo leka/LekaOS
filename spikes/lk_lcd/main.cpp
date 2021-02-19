f73a8b2a6016866feb3649c75c625d86e8b1a4ca
Ladislas de Toldi <ladislas@detoldi.me>
2021-02-19 15:00:47 +0100

    :recycle: (jpeg): Remove the need of decode polling + move callbacks implementation details to LKCoreJPEG

--- spikes/lk_lcd/main.cpp
+++ spikes/lk_lcd/main.cpp
@@ -4,13 +4,27 @@
 
 #include "mbed.h"
 
+#include "FileManager.h"
 #include "HelloWorld.h"
-#include "LekaScreen.h"
+#include "LKCoreDMA2D.h"
+#include "LKCoreDSI.h"
+#include "LKCoreJPEG.h"
+#include "LKCoreLCD.h"
+#include "LKCoreLTDC.h"
+#include "LKCoreSTM32Hal.h"
+#include "st_sdram.h"
 
 using namespace leka;
 
 HelloWorld hello;
-Screen leka_screen;
+
+LKCoreSTM32Hal hal;
+LKCoreDMA2D coredma2d(hal);
+LKCoreDSI coredsi;
+LKCoreJPEG corejpeg(coredma2d);
+LKCoreLCD corelcd(SCREEN_BACKLIGHT_PWM, 0.5f);
+LKCoreLTDC coreltdc;
+FileManager sd_card;
 
 static BufferedSerial serial(USBTX, USBRX, 9600);
 
@@ -18,6 +32,88 @@ constexpr uint8_t buff_size = 128;
 char buff[buff_size] {};
 
 Thread screen_thread;
+
+void init()
+{
+	coredsi.reset();
+
+	/** @brief Enable the LTDC clock */
+	__HAL_RCC_LTDC_CLK_ENABLE();
+
+	/** @brief Toggle Sw reset of LTDC IP */
+	__HAL_RCC_LTDC_FORCE_RESET();
+	__HAL_RCC_LTDC_RELEASE_RESET();
+
+	/** @brief Enable the DMA2D clock */
+	__HAL_RCC_DMA2D_CLK_ENABLE();
+
+	/** @brief Toggle Sw reset of DMA2D IP */
+	__HAL_RCC_DMA2D_FORCE_RESET();
+	__HAL_RCC_DMA2D_RELEASE_RESET();
+
+	/** @brief Enable DSI Host and wrapper clocks */
+	__HAL_RCC_DSI_CLK_ENABLE();
+
+	/** @brief Soft Reset the DSI Host and wrapper */
+	__HAL_RCC_DSI_FORCE_RESET();
+	__HAL_RCC_DSI_RELEASE_RESET();
+
+	/** @brief NVIC configuration for LTDC interrupt that is now enabled */
+	HAL_NVIC_SetPriority(LTDC_IRQn, 3, 0);
+	HAL_NVIC_EnableIRQ(LTDC_IRQn);
+
+	/** @brief NVIC configuration for DMA2D interrupt that is now enabled */
+	HAL_NVIC_SetPriority(DMA2D_IRQn, 3, 0);
+	HAL_NVIC_EnableIRQ(DMA2D_IRQn);
+
+	/** @brief NVIC configuration for DSI interrupt that is now enabled */
+	HAL_NVIC_SetPriority(DSI_IRQn, 3, 0);
+	HAL_NVIC_EnableIRQ(DSI_IRQn);
+
+	coredsi.initialize();
+
+	DSI_VidCfgTypeDef dsi_config = coredsi.getConfig();
+	coreltdc.initialize(&dsi_config);
+	coreltdc.configureLayer();
+
+	coredsi.start();
+
+	BSP_SDRAM_Init();
+
+	corelcd.initialize();
+	corejpeg.initialize();
+	coredma2d.initialize();
+
+	HAL_JPEG_RegisterInfoReadyCallback(corejpeg.getHandlePointer(),
+									   [](JPEG_HandleTypeDef *hjpeg, JPEG_ConfTypeDef *info)
+									   {
+										   corejpeg.onInfoReadyCallback(hjpeg, info);
+									   });
+
+	HAL_JPEG_RegisterGetDataCallback(corejpeg.getHandlePointer(),
+									 [](JPEG_HandleTypeDef *hjpeg, uint32_t size)
+									 {
+										 corejpeg.onDataAvailableCallback(hjpeg, size);
+									 });
+
+	HAL_JPEG_RegisterDataReadyCallback(corejpeg.getHandlePointer(),
+									   [](JPEG_HandleTypeDef *hjpeg, uint8_t *pDataOut, uint32_t size)
+									   {
+										   corejpeg.onDataReadyCallback(hjpeg, pDataOut, size);
+									   });
+
+	HAL_JPEG_RegisterCallback(corejpeg.getHandlePointer(), HAL_JPEG_DECODE_CPLT_CB_ID,
+							  [](JPEG_HandleTypeDef *hjpeg)
+							  {
+								  corejpeg.onDecodeCompleteCallback(hjpeg);
+							  });
+
+	HAL_JPEG_RegisterCallback(corejpeg.getHandlePointer(), HAL_JPEG_ERROR_CB_ID,
+							  [](JPEG_HandleTypeDef *hjpeg)
+							  {
+								  corejpeg.onErrorCallback(hjpeg);
+							  });
+}
 
 int main(void)
 {
@@ -27,7 +123,13 @@ int main(void)
 
 	rtos::ThisThread::sleep_for(2s);
 
-	screen_thread.start({&leka_screen, &Screen::start});
+	init();
+
+	char filename1[] = "assets/images/Leka/logo.jpg";
+	char filename2[] = "assets/images/Leka/emotion-happy.jpg";
+
+	FIL JPEG_File;
+
 	hello.start();
 
 	while (true) {
@@ -36,6 +138,18 @@ int main(void)
 							 hello.world, int(t.count() / 1000));
 		serial.write(buff, length);
 		rtos::ThisThread::sleep_for(1s);
+
+		if (f_open(&JPEG_File, filename1, FA_READ) == FR_OK) {
+			corejpeg.displayImage(&JPEG_File);
+			f_close(&JPEG_File);
+			rtos::ThisThread::sleep_for(2s);
+		}
+
+		if (f_open(&JPEG_File, filename2, FA_READ) == FR_OK) {
+			corejpeg.displayImage(&JPEG_File);
+			f_close(&JPEG_File);
+			rtos::ThisThread::sleep_for(2s);
+		}
 	}
 
 	return 0;
