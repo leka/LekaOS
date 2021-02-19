ced4912e4923286aa1bc224dc02c20d4f66f2358
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-19 14:16:04 +0100

    :sparkles: (hal): Add LKCoreSTM32Hal

---
+++ drivers/LKCoreVideo/source/LKCoreSTM32Hal.cpp
@@ -0,0 +1,26 @@
+#include "LKCoreSTM32Hal.h"
+
+namespace leka {
+
+LKCoreSTM32Hal::LKCoreSTM32Hal() {}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_Init(DMA2D_HandleTypeDef *hdma2d)
+{
+	return HAL_DMA2D_Init(hdma2d);
+}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_ConfigLayer(DMA2D_HandleTypeDef *hdma2d, uint32_t LayerIdx)
+{
+	return HAL_DMA2D_ConfigLayer(hdma2d, LayerIdx);
+}
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_Start(DMA2D_HandleTypeDef *hdma2d, uint32_t pdata, uint32_t DstAddress,
+												  uint32_t Width, uint32_t Height)
+{
+	return HAL_DMA2D_Start(hdma2d, pdata, DstAddress, Width, Height);
+}
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_PollForTransfer(DMA2D_HandleTypeDef *hdma2d, uint32_t Timeout)
+{
+	return HAL_DMA2D_PollForTransfer(hdma2d, Timeout);
+}
+
+}	// namespace leka
