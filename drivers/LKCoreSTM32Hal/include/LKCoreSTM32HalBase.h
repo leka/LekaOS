ed66aa8cd23ad9110a8197c9ddfab7eb97291be3
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-23 16:04:49 +0100

    :white_check_mark: (hal): Add HAL function for DSI
    :zap: (hal): Use final keyword for virtual function
    
    Co-authored-by: Ladislas de Toldi <ladislas@detoldi.me>

--- drivers/LKCoreSTM32Hal/include/LKCoreSTM32HalBase.h
+++ drivers/LKCoreSTM32Hal/include/LKCoreSTM32HalBase.h
@@ -14,11 +14,25 @@ class LKCoreSTM32HalBase
   public:
 	virtual ~LKCoreSTM32HalBase() = default;
 
+	virtual void HAL_RCC_GPIOJ_CLK_ENABLE(void) = 0;
+
+	virtual void HAL_GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init)				   = 0;
+	virtual void HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) = 0;
+
 	virtual HAL_StatusTypeDef HAL_DMA2D_Init(DMA2D_HandleTypeDef *hdma2d)							   = 0;
 	virtual HAL_StatusTypeDef HAL_DMA2D_ConfigLayer(DMA2D_HandleTypeDef *hdma2d, uint32_t LayerIdx)	   = 0;
 	virtual HAL_StatusTypeDef HAL_DMA2D_Start(DMA2D_HandleTypeDef *hdma2d, uint32_t pdata, uint32_t DstAddress,
 											  uint32_t Width, uint32_t Height)						   = 0;
 	virtual HAL_StatusTypeDef HAL_DMA2D_PollForTransfer(DMA2D_HandleTypeDef *hdma2d, uint32_t Timeout) = 0;
+
+	virtual HAL_StatusTypeDef HAL_DSI_Init(DSI_HandleTypeDef *hdsi, DSI_PLLInitTypeDef *PLLInit)			  = 0;
+	virtual HAL_StatusTypeDef HAL_DSI_DeInit(DSI_HandleTypeDef *hdsi)										  = 0;
+	virtual HAL_StatusTypeDef HAL_DSI_ConfigVideoMode(DSI_HandleTypeDef *hdsi, DSI_VidCfgTypeDef *VidCfg)	  = 0;
+	virtual HAL_StatusTypeDef HAL_DSI_Start(DSI_HandleTypeDef *hdsi)										  = 0;
+	virtual HAL_StatusTypeDef HAL_DSI_ShortWrite(DSI_HandleTypeDef *hdsi, uint32_t ChannelID, uint32_t Mode,
+												 uint32_t Param1, uint32_t Param2)							  = 0;
+	virtual HAL_StatusTypeDef HAL_DSI_LongWrite(DSI_HandleTypeDef *hdsi, uint32_t ChannelID, uint32_t Mode,
+												uint32_t NbParams, uint32_t Param1, uint8_t *ParametersTable) = 0;
 
   private:
 };
--- drivers/LKCoreSTM32Hal/source/LKCoreSTM32Hal.cpp
+++ drivers/LKCoreSTM32Hal/source/LKCoreSTM32Hal.cpp
@@ -4,6 +4,21 @@ namespace leka {
 
 LKCoreSTM32Hal::LKCoreSTM32Hal() {}
 
+void LKCoreSTM32Hal::HAL_RCC_GPIOJ_CLK_ENABLE(void)
+{
+	__HAL_RCC_GPIOJ_CLK_ENABLE();
+}
+
+void LKCoreSTM32Hal::HAL_GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init)
+{
+	::HAL_GPIO_Init(GPIOx, GPIO_Init);
+}
+
+void LKCoreSTM32Hal::HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
+{
+	::HAL_GPIO_WritePin(GPIOx, GPIO_Pin, PinState);
+}
+
 HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_Init(DMA2D_HandleTypeDef *hdma2d)
 {
 	return ::HAL_DMA2D_Init(hdma2d);
@@ -13,14 +28,48 @@ HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_ConfigLayer(DMA2D_HandleTypeDef *hdm
 {
 	return ::HAL_DMA2D_ConfigLayer(hdma2d, LayerIdx);
 }
+
 HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_Start(DMA2D_HandleTypeDef *hdma2d, uint32_t pdata, uint32_t DstAddress,
 												  uint32_t Width, uint32_t Height)
 {
 	return ::HAL_DMA2D_Start(hdma2d, pdata, DstAddress, Width, Height);
 }
+
 HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DMA2D_PollForTransfer(DMA2D_HandleTypeDef *hdma2d, uint32_t Timeout)
 {
 	return ::HAL_DMA2D_PollForTransfer(hdma2d, Timeout);
+}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DSI_Init(DSI_HandleTypeDef *hdsi, DSI_PLLInitTypeDef *PLLInit)
+{
+	return ::HAL_DSI_Init(hdsi, PLLInit);
+}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DSI_DeInit(DSI_HandleTypeDef *hdsi)
+{
+	return ::HAL_DSI_DeInit(hdsi);
+}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DSI_ConfigVideoMode(DSI_HandleTypeDef *hdsi, DSI_VidCfgTypeDef *VidCfg)
+{
+	return ::HAL_DSI_ConfigVideoMode(hdsi, VidCfg);
+}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DSI_Start(DSI_HandleTypeDef *hdsi)
+{
+	return ::HAL_DSI_Start(hdsi);
+}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DSI_ShortWrite(DSI_HandleTypeDef *hdsi, uint32_t ChannelID, uint32_t Mode,
+													 uint32_t Param1, uint32_t Param2)
+{
+	return ::HAL_DSI_ShortWrite(hdsi, ChannelID, Mode, Param1, Param2);
+}
+
+HAL_StatusTypeDef LKCoreSTM32Hal::HAL_DSI_LongWrite(DSI_HandleTypeDef *hdsi, uint32_t ChannelID, uint32_t Mode,
+													uint32_t NbParams, uint32_t Param1, uint8_t *ParametersTable)
+{
+	return ::HAL_DSI_LongWrite(hdsi, ChannelID, Mode, NbParams, Param1, ParametersTable);
 }
 
 }	// namespace leka
