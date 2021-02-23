23291ac9f10c950a53b996d5b90b8500b1bdb91d
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-23 16:04:49 +0100

    :white_check_mark: (hal): Add HAL function for DSI
    :zap: (hal): Use final keyword for virtual function
    
    Co-authored-by: Ladislas de Toldi <ladislas@detoldi.me>

--- tests/unit/mbed-os/mocks/mock_LKCoreSTM32Hal.h
+++ tests/unit/mbed-os/mocks/mock_LKCoreSTM32Hal.h
@@ -13,6 +13,11 @@ namespace leka {
 class LKCoreSTM32HalMock : public LKCoreSTM32HalBase
 {
   public:
+	MOCK_METHOD(void, HAL_RCC_GPIOJ_CLK_ENABLE, (), (override));
+
+	MOCK_METHOD(void, HAL_GPIO_Init, (GPIO_TypeDef * GPIOx, GPIO_InitTypeDef *GPIO_Init), (override));
+	MOCK_METHOD(void, HAL_GPIO_WritePin, (GPIO_TypeDef * GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState), (override));
+
 	MOCK_METHOD(HAL_StatusTypeDef, HAL_DMA2D_Init, (DMA2D_HandleTypeDef * hdma2d), (override));
 	MOCK_METHOD(HAL_StatusTypeDef, HAL_DMA2D_ConfigLayer, (DMA2D_HandleTypeDef * hdma2d, uint32_t LayerIdx),
 				(override));
@@ -20,6 +25,19 @@ class LKCoreSTM32HalMock : public LKCoreSTM32HalBase
 				(DMA2D_HandleTypeDef * hdma2d, uint32_t pdata, uint32_t DstAddress, uint32_t Width, uint32_t Height),
 				(override));
 	MOCK_METHOD(HAL_StatusTypeDef, HAL_DMA2D_PollForTransfer, (DMA2D_HandleTypeDef * hdma2d, uint32_t Timeout),
+				(override));
+
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DSI_Init, (DSI_HandleTypeDef * hdsi, DSI_PLLInitTypeDef *PLLInit), (override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DSI_DeInit, (DSI_HandleTypeDef * hdsi), (override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DSI_ConfigVideoMode, (DSI_HandleTypeDef * hdsi, DSI_VidCfgTypeDef *VidCfg),
+				(override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DSI_Start, (DSI_HandleTypeDef * hdsi), (override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DSI_ShortWrite,
+				(DSI_HandleTypeDef * hdsi, uint32_t ChannelID, uint32_t Mode, uint32_t Param1, uint32_t Param2),
+				(override));
+	MOCK_METHOD(HAL_StatusTypeDef, HAL_DSI_LongWrite,
+				(DSI_HandleTypeDef * hdsi, uint32_t ChannelID, uint32_t Mode, uint32_t NbParams, uint32_t Param1,
+				 uint8_t *ParametersTable),
 				(override));
 };
 
