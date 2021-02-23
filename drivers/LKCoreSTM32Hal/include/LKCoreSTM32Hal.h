9eb5eb8335493dea9e1b938d85f44c2d2929c5e7
Yann Locatelli <yann.locatelli2@gmail.com>
2021-02-23 16:04:49 +0100

    :white_check_mark: (hal): Add HAL function for DSI
    :zap: (hal): Use final keyword for virtual function
    
    Co-authored-by: Ladislas de Toldi <ladislas@detoldi.me>

--- drivers/LKCoreSTM32Hal/include/LKCoreSTM32Hal.h
+++ drivers/LKCoreSTM32Hal/include/LKCoreSTM32Hal.h
@@ -15,11 +15,25 @@ class LKCoreSTM32Hal : public LKCoreSTM32HalBase
   public:
 	LKCoreSTM32Hal();
 
-	virtual HAL_StatusTypeDef HAL_DMA2D_Init(DMA2D_HandleTypeDef *hdma2d);
-	virtual HAL_StatusTypeDef HAL_DMA2D_ConfigLayer(DMA2D_HandleTypeDef *hdma2d, uint32_t LayerIdx);
-	virtual HAL_StatusTypeDef HAL_DMA2D_Start(DMA2D_HandleTypeDef *hdma2d, uint32_t pdata, uint32_t DstAddress,
-											  uint32_t Width, uint32_t Height);
-	virtual HAL_StatusTypeDef HAL_DMA2D_PollForTransfer(DMA2D_HandleTypeDef *hdma2d, uint32_t Timeout);
+	void HAL_RCC_GPIOJ_CLK_ENABLE(void) final;
+
+	void HAL_GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init) final;
+	void HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) final;
+
+	HAL_StatusTypeDef HAL_DMA2D_Init(DMA2D_HandleTypeDef *hdma2d) final;
+	HAL_StatusTypeDef HAL_DMA2D_ConfigLayer(DMA2D_HandleTypeDef *hdma2d, uint32_t LayerIdx) final;
+	HAL_StatusTypeDef HAL_DMA2D_Start(DMA2D_HandleTypeDef *hdma2d, uint32_t pdata, uint32_t DstAddress, uint32_t Width,
+									  uint32_t Height) final;
+	HAL_StatusTypeDef HAL_DMA2D_PollForTransfer(DMA2D_HandleTypeDef *hdma2d, uint32_t Timeout) final;
+
+	HAL_StatusTypeDef HAL_DSI_Init(DSI_HandleTypeDef *hdsi, DSI_PLLInitTypeDef *PLLInit) final;
+	HAL_StatusTypeDef HAL_DSI_DeInit(DSI_HandleTypeDef *hdsi) final;
+	HAL_StatusTypeDef HAL_DSI_ConfigVideoMode(DSI_HandleTypeDef *hdsi, DSI_VidCfgTypeDef *VidCfg) final;
+	HAL_StatusTypeDef HAL_DSI_Start(DSI_HandleTypeDef *hdsi) final;
+	HAL_StatusTypeDef HAL_DSI_ShortWrite(DSI_HandleTypeDef *hdsi, uint32_t ChannelID, uint32_t Mode, uint32_t Param1,
+										 uint32_t Param2) final;
+	HAL_StatusTypeDef HAL_DSI_LongWrite(DSI_HandleTypeDef *hdsi, uint32_t ChannelID, uint32_t Mode, uint32_t NbParams,
+										uint32_t Param1, uint8_t *ParametersTable) final;
 
   private:
 };
