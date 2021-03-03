04473439ad8ebe9d007ac6df4b66bd21d34dfa9c
Ladislas de Toldi <ladislas@detoldi.me>
2021-03-03 14:53:00 +0100

    :construction_worker: (markdown lint): Add markdown lint checks

---
+++ .mdlrc_style.rb
@@ -0,0 +1,11 @@
+# Activate all rules
+all
+
+# Customize rules
+rule 'MD002', :level => 1
+rule 'MD007', :indent => 4
+
+# Exclude rules
+exclude_rule 'MD013'
+exclude_rule 'MD033'
+exclude_rule 'MD041'
