00b1b3c12625185d45c443000cfd9a01156af2fc
Yann Locatelli <yann.locatelli2@gmail.com>
2021-03-12 18:53:29 +0100

    🤡(ltdc): Add LTDC mock

---
+++ drivers/LKCoreVideo/include/internal/LKCoreLTDCBase.h
@@ -0,0 +1,31 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_LTDC_BASE_H_
+#define _LEKA_OS_LIB_LTDC_BASE_H_
+
+#include "stm32f7xx_hal.h"
+
+namespace leka {
+
+class LKCoreLTDCBase
+{
+  public:
+	virtual ~LKCoreLTDCBase() = default;
+
+	virtual void initialize() = 0;
+
+	virtual void setupLayerConfig(void)		 = 0;
+	virtual void setupTimingConfig(void)	 = 0;
+	virtual void setupBackgroundConfig(void) = 0;
+
+	virtual void configureLayer(void) = 0;
+
+	virtual LTDC_HandleTypeDef getHandle(void)		  = 0;
+	virtual LTDC_LayerCfgTypeDef getLayerConfig(void) = 0;
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_LIB_LTDC_BASE_H_
