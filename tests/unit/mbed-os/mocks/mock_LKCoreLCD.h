8669e18a1bd2c5fa93c0d3755e75fd0420fdc0bb
Yann Locatelli <yann.locatelli2@gmail.com>
2021-03-13 09:21:47 +0100

    🤡(lcd): Add LCD mock

---
+++ tests/unit/mbed-os/mocks/mock_LKCoreLCD.h
@@ -0,0 +1,24 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LCD_MOCK_H_
+#define _LEKA_OS_LCD_MOCK_H_
+
+#include "LKCoreLCDBase.h"
+#include "gmock/gmock.h"
+
+namespace leka {
+
+class LKCoreLCDMock : public LKCoreLCDBase
+{
+  public:
+	MOCK_METHOD(void, initialize, (), (override));
+	MOCK_METHOD(void, turnOn, (), (override));
+	MOCK_METHOD(void, turnOff, (), (override));
+	MOCK_METHOD(void, setBrightness, (float value), (override));
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_LCD_MOCK_H_
