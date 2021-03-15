2fb76f4928ad100a8d9d8dec86a306ecbed03216
Yann Locatelli <yann.locatelli2@gmail.com>
2021-03-15 11:57:53 +0100

    :building_construction: (dma2d): Add LKCoreDMA2DBase

---
+++ drivers/LKCoreVideo/include/internal/LKCoreDMA2DBase.h
@@ -0,0 +1,29 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_DMA2D_BASE_H_
+#define _LEKA_OS_LIB_DMA2D_BASE_H_
+
+#include "LKCoreSTM32HalBase.h"
+
+namespace leka {
+
+class LKCoreDMA2DBase
+{
+  public:
+	virtual ~LKCoreDMA2DBase() = default;
+
+	virtual void initialize() = 0;
+
+	virtual void transferData(uintptr_t input_data_address, uintptr_t output_data_address, uint16_t width,
+							  uint16_t height)																	 = 0;
+	virtual void transferImage(uint16_t width, uint16_t height, uint16_t width_offset)							 = 0;
+	virtual void transferDrawing(uintptr_t first_pixel_address, uint16_t width, uint16_t height, uint32_t color) = 0;
+
+	virtual DMA2D_HandleTypeDef getHandle(void) = 0;
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_LIB_DMA2D_BASE_H_
