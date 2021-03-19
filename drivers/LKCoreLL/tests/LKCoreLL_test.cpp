2ba1c13fe0acb5fa7777d9a0f360f93d0982903d
Yann Locatelli <yann.locatelli2@gmail.com>
2021-03-19 17:39:11 +0100

    ✅ Add LKCoreLL unit test

---
+++ drivers/LKCoreLL/tests/LKCoreLL_test.cpp
@@ -0,0 +1,29 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "LKCoreLL.h"
+
+#include "gtest/gtest.h"
+
+using namespace leka;
+
+TEST(LKUtilsTest, rawMemoryWriteToBuffer)
+{
+	LKCoreLL ll;
+
+	uint32_t buffer[5] {0};
+	uintptr_t buffer_address = reinterpret_cast<uintptr_t>(&buffer);
+
+	ll.rawMemoryWrite(buffer_address + 0 * sizeof(buffer[0]), 42);
+	ll.rawMemoryWrite(buffer_address + 1 * sizeof(buffer[0]), 43);
+	ll.rawMemoryWrite(buffer_address + 2 * sizeof(buffer[0]), 44);
+	ll.rawMemoryWrite(buffer_address + 3 * sizeof(buffer[0]), 45);
+	ll.rawMemoryWrite(buffer_address + 4 * sizeof(buffer[0]), 46);
+
+	ASSERT_EQ(buffer[0], 42);
+	ASSERT_EQ(buffer[1], 43);
+	ASSERT_EQ(buffer[2], 44);
+	ASSERT_EQ(buffer[3], 45);
+	ASSERT_EQ(buffer[4], 46);
+}
