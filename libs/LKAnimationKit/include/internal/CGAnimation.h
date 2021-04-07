c7e447be3f2b67b82bb84977cb83a11c14a15bb9
Yann Locatelli <yann.locatelli2@gmail.com>
2021-04-07 21:44:15 +0200

    :sparkles: Add CGAnimation

---
+++ libs/LKAnimationKit/include/internal/CGAnimation.h
@@ -0,0 +1,24 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_CGANIMATION_H_
+#define _LEKA_OS_LIB_CGANIMATION_H_
+
+#include <cstdint>
+
+namespace leka::animation::interface {
+
+class CGAnimation
+{
+  public:
+	virtual ~CGAnimation() = default;
+
+	virtual void start() = 0;
+	virtual void run()	 = 0;
+	virtual void stop()	 = 0;
+};
+
+}	// namespace leka::animation::interface
+
+#endif
