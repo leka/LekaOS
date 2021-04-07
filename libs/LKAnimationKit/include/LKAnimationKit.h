156a05a44ab098d5ed751859e70869ed3075f921
Yann Locatelli <yann.locatelli2@gmail.com>
2021-04-07 21:47:15 +0200

    :sparkles: Add LKAnimationKit

---
+++ libs/LKAnimationKit/include/LKAnimationKit.h
@@ -0,0 +1,36 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_LKANIMATIONKIT_H_
+#define _LEKA_OS_LIB_LKANIMATIONKIT_H_
+
+#include "events/EventQueue.h"
+#include "rtos/Thread.h"
+
+#include "CGAnimation.h"
+
+namespace leka {
+
+class LKAnimationKit
+{
+  public:
+	LKAnimationKit(rtos::Thread &thread, events::EventQueue &event_queue, animation::interface::CGAnimation &animation);
+
+	void start(animation::interface::CGAnimation &animation);
+	void stop();
+
+	void setRefreshRate(events::EventQueue::duration refresh_rate);
+
+  private:
+	rtos::Thread &_thread;
+	events::EventQueue &_event_queue;
+	animation::interface::CGAnimation &_animation;
+
+	int _animation_id;
+	std::chrono::milliseconds _refresh_rate = std::chrono::milliseconds(1);
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_LIB_LKANIMATIONKIT_H_
---
+++ libs/LKAnimationKit/source/LKAnimationKit.cpp
@@ -0,0 +1,36 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "LKAnimationKit.h"
+
+using namespace leka;
+using namespace animation;
+
+LKAnimationKit::LKAnimationKit(rtos::Thread &thread, events::EventQueue &event_queue, interface::CGAnimation &animation)
+	: _thread(thread), _event_queue(event_queue), _animation(animation)
+{
+	_thread.start({&_event_queue, &events::EventQueue::dispatch_forever});
+}
+
+void LKAnimationKit::start(interface::CGAnimation &animation)
+{
+	stop();	  // Avoid speed up of animation
+
+	_animation = animation;
+	animation.start();
+
+	_animation_id = _event_queue.call_every(_refresh_rate, &animation, &interface::CGAnimation::run);
+}
+
+void LKAnimationKit::stop()
+{
+	_event_queue.cancel(_animation_id);
+
+	_animation.stop();
+}
+
+void LKAnimationKit::setRefreshRate(events::EventQueue::duration refresh_rate)
+{
+	_refresh_rate = refresh_rate;
+}
