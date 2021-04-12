edbe4532f95ab1a76feb8a40c05022a60414770b
Yann Locatelli <yann.locatelli2@gmail.com>
2021-04-12 14:49:35 +0200

    :white_check_mark: (animations): Add LKAnimationKit tests

---
+++ tests/unit/mbed-os/stubs/stub_extension_EventQueue.cpp
@@ -0,0 +1,10 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "events/EventQueue.h"
+
+void events::EventQueue::dispatch_forever()
+{
+	// nothing to do
+}
