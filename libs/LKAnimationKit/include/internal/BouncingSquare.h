4f0b7d6f8b1b18e7125569b775d3fadd60040517
Yann Locatelli <yann.locatelli2@gmail.com>
2021-04-12 15:17:09 +0200

    :sparkles: (animations): Add BouncingSquare

---
+++ libs/LKAnimationKit/include/internal/BouncingSquare.h
@@ -0,0 +1,50 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_ANIMATION_BOUNCINGSQUARE_H_
+#define _LEKA_OS_LIB_ANIMATION_BOUNCINGSQUARE_H_
+
+// ? LCOV_EXCL_START - Exclude from coverage report
+
+#include <cstdint>
+
+#include "CGAnimation.h"
+#include "LKCoreGraphicsBase.h"
+
+namespace leka::animation {
+
+class BouncingSquare : public interface::CGAnimation
+{
+  public:
+	explicit BouncingSquare(LKCoreGraphicsBase &coregraphics);
+
+	void start(void) final;
+	void run(void) final;
+	void stop(void) final;
+
+  private:
+	bool squareWillBeOutOfBoundVertical() const;
+	bool squareWillBeOutOfBoundHorizontal() const;
+	void updateSquareDirection();
+	void updateSquarePosition();
+	void updateSquareColor();
+	void drawSquare();
+
+	LKCoreGraphicsBase &_coregraphics;
+
+	LKCoreGraphicsBase::FilledRectangle _square {{0, 0}, 100, 100};
+	CGColor _color {.red = 0xFF, .green = 0x00, .blue = 0x00};
+
+	struct Shift {
+		int32_t horizontal;
+		int32_t vertical;
+	};
+	Shift _shift {.horizontal = 1, .vertical = 1};
+};
+
+}	// namespace leka::animation
+
+// ? LCOV_EXCL_STOP - Exclude from coverage report
+
+#endif
---
+++ libs/LKAnimationKit/source/BouncingSquare.cpp
@@ -0,0 +1,94 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// ? LCOV_EXCL_START - Exclude from coverage report
+
+#include "BouncingSquare.h"
+
+#include "corevideo_config.h"
+
+namespace leka::animation {
+
+BouncingSquare::BouncingSquare(LKCoreGraphicsBase &coregraphics) : _coregraphics(coregraphics) {}
+
+void BouncingSquare::start(void)
+{
+	_coregraphics.clearScreen(CGColor::yellow);
+}
+
+void BouncingSquare::stop(void)
+{
+	_coregraphics.clearScreen(CGColor::black);
+}
+
+void BouncingSquare::run(void)
+{
+	updateSquareDirection();
+	updateSquarePosition();
+	updateSquareColor();
+	drawSquare();
+}
+
+void BouncingSquare::updateSquareDirection()
+{
+	if (squareWillBeOutOfBoundHorizontal()) {
+		_shift.horizontal = -_shift.horizontal;
+	}
+
+	if (squareWillBeOutOfBoundVertical()) {
+		_shift.vertical = -_shift.vertical;
+	}
+}
+
+bool BouncingSquare::squareWillBeOutOfBoundHorizontal() const
+{
+	if (_square.origin.x + _shift.horizontal <= 0) {
+		return true;
+	}
+	if (_square.origin.x + _square.width + _shift.horizontal > static_cast<uint32_t>(lcd::dimension.width - 1)) {
+		return true;
+	}
+	return false;
+}
+
+bool BouncingSquare::squareWillBeOutOfBoundVertical() const
+{
+	if (_square.origin.y + _shift.vertical <= 0) {
+		return true;
+	}
+	if (_square.origin.y + _square.height + _shift.vertical > static_cast<uint32_t>(lcd::dimension.height - 1)) {
+		return true;
+	}
+	return false;
+}
+
+void BouncingSquare::updateSquarePosition()
+{
+	_square.origin.x += _shift.horizontal;
+	_square.origin.y += _shift.vertical;
+}
+
+void BouncingSquare::updateSquareColor()
+{
+	if (_color.red == 0x00) {
+		_color.green++;
+		_color.blue--;
+	}
+	if (_color.green == 0x00) {
+		_color.blue++;
+		_color.red--;
+	}
+	if (_color.blue == 0x00) {
+		_color.red++;
+		_color.green--;
+	}
+}
+
+void BouncingSquare::drawSquare()
+{
+	_coregraphics.drawRectangle(_square, _color);
+}
+}	// namespace leka::animation
+
+// ? LCOV_EXCL_STOP - Exclude from coverage report
