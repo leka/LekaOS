c85ed7bd802cc6af49cc20b35a9b8c33127d5385
Ladislas de Toldi <ladislas@detoldi.me>
2021-05-03 15:59:58 +0200

    :recycle: (modernize): Libs/PrettyPrinter - Modernize code and use LogKit

--- libs/PrettyPrinter/include/PrettyPrinter.h
+++ libs/PrettyPrinter/include/PrettyPrinter.h
@@ -12,7 +12,7 @@ namespace leka::ble {
 void printError(ble_error_t error, const char *msg);
 void printAddress(const ::ble::address_t &addr);
 void printMacAddress();
-const char *phy_to_string(::ble::phy_t phy);
+auto phy_to_string(::ble::phy_t phy) -> const char *;
 
 }	// namespace leka::ble
 
--- libs/PrettyPrinter/source/PrettyPrinter.cpp
+++ libs/PrettyPrinter/source/PrettyPrinter.cpp
@@ -4,66 +4,67 @@
 
 #include "PrettyPrinter.h"
 
+#include "LogKit.h"
+
 void leka::ble::printError(ble_error_t error, const char *msg)
 {
 	printf("%s: ", msg);
 	switch (error) {
 		case BLE_ERROR_NONE:
-			printf("BLE_ERROR_NONE: No error");
+			log_error("BLE_ERROR_NONE: No error");
 			break;
 		case BLE_ERROR_BUFFER_OVERFLOW:
-			printf(
+			log_error(
 				"BLE_ERROR_BUFFER_OVERFLOW: The requested action would cause a buffer overflow and has been "
 				"aborted");
 			break;
 		case BLE_ERROR_NOT_IMPLEMENTED:
-			printf(
+			log_error(
 				"BLE_ERROR_NOT_IMPLEMENTED: Requested a feature that isn't yet implement or isn't supported by the "
 				"target HW");
 			break;
 		case BLE_ERROR_PARAM_OUT_OF_RANGE:
-			printf("BLE_ERROR_PARAM_OUT_OF_RANGE: One of the supplied parameters is outside the valid range");
+			log_error("BLE_ERROR_PARAM_OUT_OF_RANGE: One of the supplied parameters is outside the valid range");
 			break;
 		case BLE_ERROR_INVALID_PARAM:
-			printf("BLE_ERROR_INVALID_PARAM: One of the supplied parameters is invalid");
+			log_error("BLE_ERROR_INVALID_PARAM: One of the supplied parameters is invalid");
 			break;
 		case BLE_STACK_BUSY:
-			printf("BLE_STACK_BUSY: The stack is busy");
+			log_error("BLE_STACK_BUSY: The stack is busy");
 			break;
 		case BLE_ERROR_INVALID_STATE:
-			printf("BLE_ERROR_INVALID_STATE: Invalid state");
+			log_error("BLE_ERROR_INVALID_STATE: Invalid state");
 			break;
 		case BLE_ERROR_NO_MEM:
-			printf("BLE_ERROR_NO_MEM: Out of Memory");
+			log_error("BLE_ERROR_NO_MEM: Out of Memory");
 			break;
 		case BLE_ERROR_OPERATION_NOT_PERMITTED:
-			printf("BLE_ERROR_OPERATION_NOT_PERMITTED");
+			log_error("BLE_ERROR_OPERATION_NOT_PERMITTED");
 			break;
 		case BLE_ERROR_INITIALIZATION_INCOMPLETE:
-			printf("BLE_ERROR_INITIALIZATION_INCOMPLETE");
+			log_error("BLE_ERROR_INITIALIZATION_INCOMPLETE");
 			break;
 		case BLE_ERROR_ALREADY_INITIALIZED:
-			printf("BLE_ERROR_ALREADY_INITIALIZED");
+			log_error("BLE_ERROR_ALREADY_INITIALIZED");
 			break;
 		case BLE_ERROR_UNSPECIFIED:
-			printf("BLE_ERROR_UNSPECIFIED: Unknown error");
+			log_error("BLE_ERROR_UNSPECIFIED: Unknown error");
 			break;
 		case BLE_ERROR_INTERNAL_STACK_FAILURE:
-			printf("BLE_ERROR_INTERNAL_STACK_FAILURE: internal stack failure");
+			log_error("BLE_ERROR_INTERNAL_STACK_FAILURE: internal stack failure");
 			break;
 		case BLE_ERROR_NOT_FOUND:
-			printf("BLE_ERROR_NOT_FOUND");
+			log_error("BLE_ERROR_NOT_FOUND");
 			break;
 		default:
-			printf("Unknown error");
+			log_error("Unknown error");
 	}
-	printf("\r\n");
 }
 
 /** print device address to the terminal */
 void leka::ble::printAddress(const ::ble::address_t &addr)
 {
-	printf("%02x:%02x:%02x:%02x:%02x:%02x\r\n", addr[5], addr[4], addr[3], addr[2], addr[1], addr[0]);
+	log_info("%02x:%02x:%02x:%02x:%02x:%02x\r\n", addr[5], addr[4], addr[3], addr[2], addr[1], addr[0]);
 }
 
 void leka::ble::printMacAddress()
@@ -72,11 +73,11 @@ void leka::ble::printMacAddress()
 	::ble::own_address_type_t addr_type;
 	::ble::address_t address;
 	BLE::Instance().gap().getAddress(addr_type, address);
-	printf("DEVICE MAC ADDRESS: ");
+	log_info("DEVICE MAC ADDRESS: ");
 	leka::ble::printAddress(address);
 }
 
-const char *phy_to_string(::ble::phy_t phy)
+auto phy_to_string(::ble::phy_t phy) -> const char *
 {
 	switch (phy.value()) {
 		case ::ble::phy_t::LE_1M:
