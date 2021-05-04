c164d8c2b4b5f9a7ed76e76dbf8ee5c5076fbf55
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-05-04 12:46:37 +0200

    :construction:(core) Add BufferedSerial mock

---
+++ tests/unit/mbed-os/mocks/mock_CoreBufferedSerial.h
@@ -0,0 +1,22 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_BUFFERED_SERIAL_MOCK_H_
+#define _LEKA_OS_BUFFERED_SERIAL_MOCK_H_
+
+#include "CoreBufferedSerial.h"
+#include "gmock/gmock.h"
+
+namespace leka {
+
+class LKCoreBufferedSerialMock : public interface::BufferedSerial
+{
+  public:
+	MOCK_METHOD(int, read, (uint8_t *, ssize_t), (override));
+	MOCK_METHOD(int, write, (const uint8_t *, ssize_t), (override));
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_BUFFERED_SERIAL_MOCK_H_
