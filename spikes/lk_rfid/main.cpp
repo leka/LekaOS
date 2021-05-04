18684b1726adbecda36758b9c7317ae230814e18
Ladislas de Toldi <ladislas@detoldi.me>
2021-05-04 13:10:04 +0200

    :recycle: (modernize): Spikes/lk_rfid - Modernize code and use LogKit

--- spikes/lk_rfid/main.cpp
+++ spikes/lk_rfid/main.cpp
@@ -2,40 +2,40 @@
 // Copyright 2020 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-#include "mbed.h"
+#include "drivers/BufferedSerial.h"
+#include "rtos/ThisThread.h"
+#include "rtos/Thread.h"
 
 #include "HelloWorld.h"
 #include "LekaRFID.h"
+#include "LogKit.h"
 
 using namespace leka;
+using namespace std::chrono;
 
-HelloWorld hello;
-RFID leka_rfid;
-
-static BufferedSerial serial(USBTX, USBRX, 9600);
-
-constexpr uint8_t buff_size = 128;
-char buff[buff_size] {};
-
-Thread rfid_thread;
-
-int main(void)
+auto main() -> int
 {
-	auto start = Kernel::Clock::now();
+	static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
+	leka::logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
 
-	printf("\nHello, Investigation Day!\n\n");
+	auto start = rtos::Kernel::Clock::now();
+
+	log_info("Hello, World!\n\n");
 
 	rtos::ThisThread::sleep_for(2s);
 
+	RFID leka_rfid;
+	rtos::Thread rfid_thread;
 	rfid_thread.start({&leka_rfid, &RFID::start});
+
+	HelloWorld hello;
 	hello.start();
 
 	while (true) {
-		auto t = Kernel::Clock::now() - start;
+		auto t = rtos::Kernel::Clock::now() - start;
+		log_info("A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME, hello.world,
+				 int(t.count() / 1000));
 
-		int length = sprintf(buff, "A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME,
-							 hello.world, int(t.count() / 1000));
-		serial.write(buff, length);
 		rtos::ThisThread::sleep_for(1s);
 	}
 }
