10389a6f2af36060078629e17a76d6c7cf83b08a
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-05-04 12:37:37 +0200

    :construction:(core) Add BufferedSerial interface

---
+++ drivers/CoreBufferedSerial/include/CoreBufferedSerial.h
@@ -0,0 +1,39 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_DRIVER_LK_CORE_BUFFERED_SERIAL_H_
+#define _LEKA_OS_DRIVER_LK_CORE_BUFFERED_SERIAL_H_
+
+#include "drivers/BufferedSerial.h"
+
+namespace leka::interface {
+
+class BufferedSerial
+{
+  public:
+	virtual ~BufferedSerial() = default;
+
+	virtual auto read(void *buffer, ssize_t length) -> ssize_t		  = 0;
+	virtual auto write(const void *buffer, ssize_t length) -> ssize_t = 0;
+};
+
+}	// namespace leka::interface
+
+namespace leka {
+
+class CoreBufferedSerial : public interface::BufferedSerial
+{
+  public:
+	explicit CoreBufferedSerial(mbed::BufferedSerial &interface) : _interface {interface} {};
+
+	auto read(void *buffer, ssize_t length) -> ssize_t final;
+	auto write(const void *buffer, ssize_t length) -> ssize_t final;
+
+  private:
+	mbed::BufferedSerial &_interface;
+};
+
+}	// namespace leka
+
+#endif	 //_LEKA_OS_DRIVER_LK_CORE_BUFFERED_SERIAL_H_
---
+++ drivers/CoreBufferedSerial/source/CoreBufferedSerial.cpp
@@ -0,0 +1,17 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "CoreBufferedSerial.h"
+
+using namespace leka;
+
+auto CoreBufferedSerial::read(void *buffer, ssize_t length) -> ssize_t
+{
+	return _interface.read(buffer, length);
+}
+
+auto CoreBufferedSerial::write(const void *buffer, ssize_t length) -> ssize_t
+{
+	return _interface.write(buffer, length);
+}
