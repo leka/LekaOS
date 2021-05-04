31146580f5722ffe23d503419eca034bed8685a1
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-05-04 15:03:36 +0200

    :construction:(driver) Add writeProtocol and checkProtocol functions

--- drivers/LKCoreRFID/source/LKCoreRFID.cpp
+++ drivers/LKCoreRFID/source/LKCoreRFID.cpp
@@ -8,38 +8,23 @@ namespace leka {
 
 LKCoreRFID::LKCoreRFID(interface::BufferedSerial &interface) : _interface(interface) {}
 
-RFIDTag LKCoreRFID::RFIDMessageIntoStruct(uint8_t *tag_value)
+auto LKCoreRFID::writeProtocol() -> void
 {
-	RFIDTag decompose_value;
-
-	uint8_t length = tag_value[1];
-	if (!isDataLengthOk(length)) {
-		return {0, 0, 0};
-	}
-
-	decompose_value.command = tag_value[0];
-	decompose_value.length	= length;
-
-	for (int i = 0; i < decompose_value.length; ++i) {
-		decompose_value.data[i] = tag_value[i + 2];
-	}
-
-	return decompose_value;
+	const char iec_14443_cmd[4]		   = {0x02, 0x02, 0x02, 0x00};
+	const uint8_t iec_14443_cmd_length = 4;
+	_interface.write(iec_14443_cmd, iec_14443_cmd_length);
 }
 
-// auto setProtocol() -> void
-// {
-// 	const uint8_t iec_14443_cmd[4]	   = {0x02, 0x02, 0x02, 0x00};
-// 	const uint8_t iec_14443_cmd_length = 4;
-// 	_interface.write(iec_14443_cmd, iec_14443_cmd_length);
-// }
-
-auto LKCoreRFID::isDataLengthOk(uint8_t length) -> bool
+auto LKCoreRFID::checkProtocol() -> bool
 {
-	if (length > _MAXIMUM_BUFFER_SIZE) {
-		return false;
+	char buffer[2];
+	_interface.read(buffer, 2);
+
+	if (buffer[0] == 0 && buffer[1] == 0) {
+		return true;
 	}
-	return true;
+
+	return false;
 }
 
 }	// namespace leka
