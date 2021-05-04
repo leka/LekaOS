31aff63cb99939a77bc1b15ac7b8af360cc2c358
Ladislas de Toldi <ladislas@detoldi.me>
2021-05-04 15:47:29 +0200

    :recycle: (modernize): Spikes/lk_sensors_battery - Modernize code and use LogKit

--- spikes/lk_sensors_battery/Utils.h
+++ spikes/lk_sensors_battery/Utils.h
@@ -6,51 +6,55 @@
 #define _LEKA_OS_SPIKE_UTILS_H_
 
 #include <cstdint>
+#include <memory>
 
 #include "FileManager.h"
+#include "LogKit.h"
 
 auto readSdFile(const char *fileName, char *buffer, uint32_t buffer_size) -> int
 {
-	FIL file;
+	auto file = std::make_unique<FIL>();
 	UINT nbBytesRead {0};
 
-	if (f_open(&file, fileName, FA_READ | FA_OPEN_ALWAYS) != FR_OK) {
-		printf("Failed to open.\n");
+	if (f_open(file.get(), fileName, FA_READ | FA_OPEN_ALWAYS) != FR_OK) {
+		log_error("Failed to open");
 		return -1;
 	}
-	f_lseek(&file, 0);	 // Set the pointer of the file at the beginning. Otherwise it will appear blank to read since
-						 // the pointer is at the end of the file.
+	f_lseek(file.get(), 0);	  // Set the pointer of the file at the beginning. Otherwise it will appear blank to read
+							  // since the pointer is at the end of the file.
 
-	if (f_read(&file, reinterpret_cast<BYTE *>(buffer), buffer_size, &nbBytesRead) != FR_OK) {
-		printf("Failed to read\n");
+	if (f_read(file.get(), reinterpret_cast<BYTE *>(buffer), buffer_size, &nbBytesRead) != FR_OK) {
+		log_error("Failed to read");
 		return -1;
 	}
-	if (f_close(&file) != FR_OK) {
-		printf("Failed to close.\n");
+	if (f_close(file.get()) != FR_OK) {
+		log_error("Failed to close");
 		return -1;
 	}
+
 	return static_cast<int>(nbBytesRead);
 }
 
 auto writeSDFile(const char *fileName, const char *data, int nbByteToWrite) -> int
 {
-	FIL file;
+	auto file = std::make_unique<FIL>();
 	UINT nbBytesWritten {0};
 
-	if (f_open(&file, fileName, FA_READ | FA_WRITE | FA_OPEN_APPEND) != FR_OK) {
-		printf("Failed to open.\n");
+	if (f_open(file.get(), fileName, FA_READ | FA_WRITE | FA_OPEN_APPEND) != FR_OK) {
+		log_error("Failed to open");
 		return -1;
 	}
 
-	if (f_write(&file, data, nbByteToWrite, &nbBytesWritten) != FR_OK) {
-		printf("Failed to Write.\n");
+	if (f_write(file.get(), data, nbByteToWrite, &nbBytesWritten) != FR_OK) {
+		log_error("Failed to Write");
 		return -1;
 	}
 
-	if (f_close(&file) != FR_OK) {
-		printf("Failed to close.\n");
+	if (f_close(file.get()) != FR_OK) {
+		log_error("Failed to close");
 		return -1;
 	}
+
 	return static_cast<int>(nbBytesWritten);
 }
 
--- spikes/lk_sensors_battery/main.cpp
+++ spikes/lk_sensors_battery/main.cpp
@@ -2,88 +2,95 @@
 // Copyright 2021 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-#include "mbed.h"
+#include <array>
+#include <cstdint>
 
 #include "PinNames.h"
 
-#include "./Utils.h"
+#include "drivers/BufferedSerial.h"
+#include "rtos/ThisThread.h"
+#include "rtos/Thread.h"
+
 #include "FATFileSystem.h"
 #include "HelloWorld.h"
 #include "LKCoreBattery.h"
 #include "LKCoreMotor.h"
+#include "LogKit.h"
 #include "SDBlockDevice.h"
-
-// Setup important variables
-#define PRINT_BATTERY_VOLTAGE_LOG false									 // True to read a file, false to enable log
-#define BATTERY_LOG_FILENAME	  "2021_03_30-Battery-Voltage-Log.csv"	 // YYYY_MM_DD-Battery-Voltage-Log.csv
+#include "Utils.h"
 
 using namespace leka;
+using namespace std::chrono;
 
-HelloWorld hello;
-FileManager sd;
+constexpr auto PRINT_BATTERY_VOLTAGE_LOG = false;
+constexpr auto fileName =
+	std::array<char, 36> {"2021_03_30-Battery-Voltage-Log.csv"};   // YYYY_MM_DD-Battery-Voltage-Log.csv
 
-LKCoreBattery battery(PinName::BATTERY_VOLTAGE);
+const FileManager sd;
 
-LKCoreMotor motor_right(MOTOR_RIGHT_DIRECTION_1, MOTOR_RIGHT_DIRECTION_2, MOTOR_RIGHT_PWM);
-LKCoreMotor motor_left(MOTOR_LEFT_DIRECTION_1, MOTOR_LEFT_DIRECTION_2, MOTOR_LEFT_PWM);
-
-const char fileName[] = BATTERY_LOG_FILENAME;
-
-Thread battery_logger_thread;
-Thread motor_thread;
-
-void batteryLoggerThread()
+auto main() -> int
 {
-	char buffer[64] {0};
+	static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
+	leka::logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
 
-	auto now	 = [] { return static_cast<int>(Kernel::Clock::now().time_since_epoch().count()); };
-	auto voltage = [] { return battery.readVoltage(); };
+	log_info("Hello, World!\n\n");
 
-	while (true) {
-		auto length = sprintf(buffer, "%i, %f\n", now(), voltage());
-		writeSDFile(fileName, buffer, length);
-
-		rtos::ThisThread::sleep_for(30s);
-	}
-}
-
-void motorThread()
-{
-	while (true) {
-		motor_right.spin(Rotation::counterClockwise, 1.0f);
-		motor_left.spin(Rotation::counterClockwise, 1.0f);
-
-		rtos::ThisThread::sleep_for(50s);
-
-		motor_right.stop();
-		motor_left.stop();
-
-		rtos::ThisThread::sleep_for(10s);
-	}
-}
-
-int main(void)
-{
-	rtos::ThisThread::sleep_for(2s);
-
+	HelloWorld hello;
 	hello.start();
 
+	auto battery = LKCoreBattery {PinName::BATTERY_VOLTAGE};
+
+	auto battery_thread = rtos::Thread {};
+	auto battery_lambda = [&battery] {
+		auto now	 = [] { return static_cast<int>(rtos::Kernel::Clock::now().time_since_epoch().count()); };
+		auto voltage = [&] { return battery.readVoltage(); };
+
+		auto buffer = std::array<char, 64> {};
+
+		while (true) {
+			auto length = snprintf(buffer.data(), std::size(buffer), "%i, %f\n", now(), voltage());
+			writeSDFile(fileName.data(), buffer.data(), length);
+			rtos::ThisThread::sleep_for(30s);
+		}
+	};
+
+	auto motor_right = LKCoreMotor {MOTOR_RIGHT_DIRECTION_1, MOTOR_RIGHT_DIRECTION_2, MOTOR_RIGHT_PWM};
+	auto motor_left	 = LKCoreMotor {MOTOR_LEFT_DIRECTION_1, MOTOR_LEFT_DIRECTION_2, MOTOR_LEFT_PWM};
+
+	auto motors_thread = rtos::Thread {};
+	auto motors_lambda = [&motor_left, &motor_right] {
+		while (true) {
+			motor_left.spin(Rotation::counterClockwise, 1.0f);
+			motor_right.spin(Rotation::counterClockwise, 1.0f);
+
+			rtos::ThisThread::sleep_for(50s);
+
+			motor_left.stop();
+			motor_right.stop();
+
+			rtos::ThisThread::sleep_for(10s);
+		}
+	};
+
 	rtos::ThisThread::sleep_for(2s);
 
-	if (PRINT_BATTERY_VOLTAGE_LOG) {
-		constexpr uint16_t buffeur_size = 2048;
-		char buffer[buffeur_size];
+	log_info("Starting...");
 
-		auto length = readSdFile(fileName, buffer, buffeur_size);
-		for (auto i = 0; i < length; ++i) {
-			printf("%c", buffer[i]);
-		}
+	rtos::ThisThread::sleep_for(1s);
+
+	if (PRINT_BATTERY_VOLTAGE_LOG) {
+		auto buffer = std::array<char, 2048> {};
+		auto length = readSdFile(fileName.data(), buffer.data(), std::size(buffer));
+
+		log_info("Battery data:\n");
+		serial.write(buffer.data(), length);
 	} else {
-		battery_logger_thread.start(batteryLoggerThread);
-		motor_thread.start(motorThread);
+		battery_thread.start(battery_lambda);
+		motors_thread.start(motors_lambda);
 	}
 
 	while (true) {
-		rtos::ThisThread::sleep_for(10s);
+		log_info("Main thread is running...");
+		rtos::ThisThread::sleep_for(5s);
 	}
 }
