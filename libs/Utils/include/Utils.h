b451b3e01c49dec0272bfe7c6a21b4de2a948ce4
Ladislas de Toldi <ladislas@detoldi.me>
2021-05-05 17:48:06 +0200

    :recycle: (utils): Move map() function to math namespace

--- libs/Utils/include/Utils.h
+++ libs/Utils/include/Utils.h
@@ -5,7 +5,7 @@
 #ifndef _LEKA_OS_LIB_LKUTILS_H_
 #define _LEKA_OS_LIB_LKUTILS_H_
 
-namespace leka::utils {
+namespace leka::utils::math {
 
 template <typename OutputType, typename IntputType>
 auto map(IntputType x, IntputType in_min, IntputType in_max, OutputType out_min, OutputType out_max) -> OutputType
@@ -13,22 +13,18 @@ auto map(IntputType x, IntputType in_min, IntputType in_max, OutputType out_min,
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-namespace math {
+struct Point {
+	float x {0};
+	float y {0};
+};
 
-	struct Point {
-		float x {0};
-		float y {0};
-	};
+struct LinearCoefficients {
+	float slope {0};
+	float y_intercept {0};
+};
 
-	struct LinearCoefficients {
-		float slope {0};
-		float y_intercept {0};
-	};
+auto computeLinearCoefficients(Point p1, Point p2) -> LinearCoefficients;
 
-	auto computeLinearCoefficients(Point p1, Point p2) -> LinearCoefficients;
-
-}	// namespace math
-
-}	// namespace leka::utils
+}	// namespace leka::utils::math
 
 #endif	 // _LEKA_OS_LIB_LKUTILS_H_
