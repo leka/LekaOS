963f8c8d0978d72750763a2e6097add5637dc1ef
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-05-19 17:43:55 +0200

    :clown_face: (stubs): Add stub for mbed::BufferedSerial

---
+++ tests/unit/mbed-os/stubs/stub_BufferedSerial.cpp
@@ -0,0 +1,66 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "stub_BufferedSerial.h"
+
+namespace mbed {
+
+BufferedSerial::BufferedSerial(PinName tx, PinName rx, int baud) : SerialBase(tx, rx, baud)
+{
+	return;
+}
+
+BufferedSerial::~BufferedSerial()
+{
+	return;
+}
+
+auto BufferedSerial::poll(short events) const -> short
+{
+	return 0;
+}
+
+auto BufferedSerial::write(const void *buffer, size_t length) -> ssize_t
+{
+	return length;
+}
+
+auto BufferedSerial::read(void *buffer, size_t length) -> ssize_t
+{
+	return length;
+}
+
+auto BufferedSerial::close() -> int
+{
+	return 0;
+}
+
+auto BufferedSerial::isatty() -> int
+{
+	return 0;
+}
+
+auto BufferedSerial::seek(off_t offset, int whence) -> off_t
+{
+	return 0;
+}
+
+auto BufferedSerial::sync() -> int
+{
+	return 0;
+}
+
+auto BufferedSerial::enable_input(bool enabled) -> int
+{
+	return 0;
+}
+
+auto BufferedSerial::enable_output(bool enabled) -> int
+{
+	return 0;
+}
+
+void BufferedSerial::sigio(Callback<void()> func) {}
+
+}	// namespace mbed
