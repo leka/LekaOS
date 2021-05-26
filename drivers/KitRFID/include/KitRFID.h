33d21ee12600ba2dcb62217c7907044dd275ae00
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-05-26 08:43:03 +0200

    :construction: (drivers): Add KitRFID structure with RFID and ISO14443 interfaces

---
+++ drivers/KitRFID/include/KitRFID.h
@@ -0,0 +1,10 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_DRIVER_LK_KIT_RFID_H_
+#define _LEKA_OS_DRIVER_LK_KIT_RFID_H_
+
+#include "RfidInterface.h"
+
+#endif	 //_LEKA_OS_DRIVER_LK_KIT_RFID_H_
---
+++ drivers/KitRFID/include/RfidInterface.h
@@ -0,0 +1,36 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_DRIVER_LK_KIT_RFID_INTERFACE_H_
+#define _LEKA_OS_DRIVER_LK_KIT_RFID_INTERFACE_H_
+
+#include <array>
+#include <cstddef>
+#include <stdint.h>
+
+namespace leka::interface {
+
+class RFID
+{
+  public:
+	virtual ~RFID() = default;
+
+	virtual void send(void *data, size_t size)	  = 0;
+	virtual void receive(void *data, size_t size) = 0;
+	virtual void tagIsNearReader()				  = 0;
+
+	class ISO14443
+	{
+	  public:
+		virtual void requestA();
+		virtual void read();
+
+		static constexpr uint8_t commandRequestA {0x26};
+		std::array<uint8_t, 16> data {0};
+	};
+};
+
+}	// namespace leka::interface
+
+#endif	 //_LEKA_OS_DRIVER_LK_KIT_RFID_INTERFACE_H_
