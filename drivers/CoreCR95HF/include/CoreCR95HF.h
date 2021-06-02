41bd01cb1ffb2419f76519be069e60a592413790
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-06-02 15:36:42 +0200

    :construction: (REVIEW): Return of gain_modulation_values in uint8_t

--- drivers/CoreCR95HF/include/CoreCR95HF.h
+++ drivers/CoreCR95HF/include/CoreCR95HF.h
@@ -18,7 +18,10 @@ namespace cr95hf {
 		const uint8_t id;
 		const std::byte gain;
 		const std::byte modulation;
-		constexpr auto gain_modulation_values() const -> std::byte { return gain | modulation; }
+		constexpr auto gain_modulation_values() const -> std::uint8_t
+		{
+			return std::to_integer<uint8_t>(gain | modulation);
+		}
 	};
 
 	constexpr size_t max_tx_length = 16;
