018d0bf5d415ca4dd5f85a27d227ddc06d4fd963
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-06-08 13:15:47 +0200

    :clown_face: (mocks): Add RFID mock

---
+++ tests/unit/mocks/mocks/leka/CoreRFID.h
@@ -0,0 +1,25 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_RFID_MOCK_H_
+#define _LEKA_OS_RFID_MOCK_H_
+
+#include <cstddef>
+
+#include "gmock/gmock.h"
+#include "interface/drivers/RFID.h"
+
+namespace leka {
+
+class CoreRFIDMock : public interface::RFID
+{
+  public:
+	MOCK_METHOD(bool, init, (), (override));
+	MOCK_METHOD(void, send, (const lstd::span<uint8_t> &), (override));
+	MOCK_METHOD(size_t, receive, (const lstd::span<uint8_t> &), (override));
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_RFID_MOCK_H_
