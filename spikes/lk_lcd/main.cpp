55a5a5bc873afeb09b132d1bfba511964a4ddadc
Madour <modar1999@gmail.com>
2021-06-24 15:24:55 +0200

    filenames arrays in main

--- spikes/lk_lcd/main.cpp
+++ spikes/lk_lcd/main.cpp
@@ -48,9 +48,17 @@ LKCoreLCD corelcd(coreotm);
 LKCoreJPEG corejpeg(hal, coredma2d, corefatfs, std::make_unique<LKCoreJPEG::ST_DMAMode>());
 LKCoreVideo corevideo(hal, coresdram, coredma2d, coredsi, coreltdc, corelcd, coregraphics, corefont, corejpeg);
 
-const char filename1[] = "assets/images/Leka/logo.jpg";
-const char filename2[] = "assets/images/Leka/emotion-happy.jpg";
-const char filename3[] = "assets/video/BirdsAndFeeder.avi";
+std::array<std::string, 2> images = {
+	"assets/images/Leka/logo.jpg",
+	"assets/images/Leka/emotion-happy.jpg"
+};
+
+std::array<std::string, 4> videos = {
+	"assets/video/JoieV4.avi",
+	"assets/video/JoieV4_3.avi",
+	"assets/video/JoieV4_low.avi",
+	"assets/video/JoieV4_smol.avi"
+};
 
 extern "C" {
 	void JPEG_IRQHandler(void)
@@ -110,7 +118,7 @@ auto main() -> int
 		foreground = (i % 2 == 0) ? CGColor::black : CGColor::pure_red;
 		line	   = i * 2;
 		log_info("Line #%i", i);
-		rtos::ThisThread::sleep_for(200ms);
+		//rtos::ThisThread::sleep_for(200ms);
 	}
 
 	leka::logger::set_print_function([](const char *str, size_t size) {
@@ -121,7 +129,7 @@ auto main() -> int
 		"This sentence is supposed to be on multiple lines because it is too long to be displayed on "
 		"only one line of the screen.");
 
-	rtos::ThisThread::sleep_for(2s);
+	rtos::ThisThread::sleep_for(1s);
 
 	leka::logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
 
@@ -132,34 +140,27 @@ auto main() -> int
 		log_info("A message from your board %s --> \"%s\" at %is", MBED_CONF_APP_TARGET_NAME, hello.world,
 				 int(t.count() / 1000));
 
-		rtos::ThisThread::sleep_for(1s);
-
 		corevideo.turnOn();
-		if (corefatfs.open(filename1) == FR_OK) {
-			log_info("Displaying image 1 : %s", filename1);
-			corevideo.displayImage(JPEG_File.get());
-			corevideo.setBrightness(0.2f);
+		corevideo.setBrightness(0.6f);
 
-
-			corefatfs.close();
-			rtos::ThisThread::sleep_for(2s);
+		for (const auto& image : images) {
+			if (corefatfs.open(image.c_str()) == FR_OK) {
+				corevideo.displayImage(JPEG_File.get());
+				corefatfs.close();
+				log_info("Displaying image : %s", image.c_str());
+				rtos::ThisThread::sleep_for(2s);
+			}
 		}
 
-		if (corefatfs.open(filename2) == FR_OK) {
-			log_info("Displaying image 2 : %s", filename2);
-			corevideo.displayImage(JPEG_File.get());
-			corevideo.setBrightness(0.9f);
-
-			corefatfs.close();
-			rtos::ThisThread::sleep_for(2s);
-
+		for (const auto& video : videos) {
+			if (corefatfs.open(video.c_str()) == FR_OK) {
+				corejpeg.playVideo();
+				corefatfs.close();
+				log_info("Displaying video : %s", video.c_str());
+				rtos::ThisThread::sleep_for(200ms);
+			}
 		}
 
-		if (corefatfs.open(filename3) == FR_OK) {
-			log_info("Displaying video : %s", filename3);
-			corejpeg.playVideo();
-			corefatfs.close();
-		}
 		corevideo.turnOff();
 		rtos::ThisThread::sleep_for(1s);
 	}
