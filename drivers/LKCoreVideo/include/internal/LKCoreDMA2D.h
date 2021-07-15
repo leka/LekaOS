c02f4354f6e4d7cd44c378035b631393252511be
Madour <modar1999@gmail.com>
2021-07-15 16:21:25 +0200

    :sparkles: (dma2d) add fillRect

--- drivers/LKCoreVideo/include/internal/LKCoreDMA2D.h
+++ drivers/LKCoreVideo/include/internal/LKCoreDMA2D.h
@@ -19,11 +19,12 @@ class LKCoreDMA2D : public interface::LKCoreDMA2D
 	void initialize() final;
 
 	void setFrameBufferAddress(uintptr_t address) final;
+	auto getPositionAddress(uint32_t x, uint32_t y) -> uintptr_t;
 
-	void transferData(uintptr_t input_data_address, uintptr_t output_data_address, uint32_t width,
-					  uint32_t height) final;
+	void transferData(uintptr_t src, uintptr_t dst_address, uint32_t width, uint32_t height) final;
 	void transferImage(uint32_t width, uint32_t height, uint32_t width_offset) final;
 	void transferDrawing(uintptr_t first_pixel_address, uint32_t width, uint32_t height, uint32_t color) final;
+	void fillRect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color) final;
 
   private:
 	LKCoreSTM32HalBase &_hal;
