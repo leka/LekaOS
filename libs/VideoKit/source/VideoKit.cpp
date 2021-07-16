064ce8b53c7696453e3390f8f55f29ea52b6e4b4
Madour <modar1999@gmail.com>
2021-07-16 11:27:31 +0200

    :sparkles: (video) VideoKit::tick and refresh to update screen

--- libs/VideoKit/source/VideoKit.cpp
+++ libs/VideoKit/source/VideoKit.cpp
@@ -3,9 +3,8 @@
 #include "LogKit.h"
 
 using namespace leka;
-using namespace std::chrono_literals;
 
-LKVideoKit::LKVideoKit()
+VideoKit::VideoKit()
 	: _coresdram(_hal),
 	  // peripherals
 	  _corejpeg(_hal, std::make_unique<CoreJPEGDMAMode>()),
@@ -18,7 +17,7 @@ LKVideoKit::LKVideoKit()
 {
 }
 
-void LKVideoKit::initialize()
+void VideoKit::initialize()
 {
 	__HAL_RCC_LTDC_CLK_ENABLE();
 
@@ -59,52 +58,109 @@ void LKVideoKit::initialize()
 	_coredsi.enableTearingEffectReporting();
 }
 
-auto LKVideoKit::getDSI() -> CoreDSI &
+auto VideoKit::getDSI() -> CoreDSI &
 {
 	return _coredsi;
 }
 
-auto LKVideoKit::getLTDC() -> CoreLTDC &
+auto VideoKit::getLTDC() -> CoreLTDC &
 {
 	return _coreltdc;
 }
 
-auto LKVideoKit::getDMA2D() -> CoreDMA2D &
+auto VideoKit::getDMA2D() -> CoreDMA2D &
 {
 	return _coredma2d;
 }
 
-auto LKVideoKit::getJPEG() -> CoreJPEG &
+auto VideoKit::getJPEG() -> CoreJPEG &
 {
 	return _corejpeg;
 }
 
-void LKVideoKit::clear(gfx::Color color)
+void VideoKit::setFrameRateLimit(unsigned framerate)
+{
+	_frametime = (1000ms / framerate);
+}
+
+void VideoKit::clear(gfx::Color color)
 {
 	_coredma2d.fillRect(0, 0, lcd::dimension.width, lcd::dimension.height, color.toARGB8888());
 }
 
-void LKVideoKit::drawRectangle(gfx::Rectangle rect, uint32_t x, uint32_t y)
+void VideoKit::drawRectangle(gfx::Rectangle rect, uint32_t x, uint32_t y)
 {
 	_coredma2d.fillRect(x, y, rect.width, rect.height, rect.color.toARGB8888());
 }
 
-void LKVideoKit::display()
+void VideoKit::drawImage(LKCoreFatFsBase &file)
+{
+	file.seek(0);
+	_corejpeg.decodeImage(file);
+
+	auto config = _corejpeg.getConfig();
+
+	_coredma2d.transferImage(config.ImageWidth, config.ImageHeight, CoreJPEG::getWidthOffset(config));
+}
+
+void VideoKit::drawVideo(LKCoreFatFsBase &file)
+{
+	uint32_t frame_index  = 0;
+	uint32_t frame_size	  = 0;
+	uint32_t frame_offset = CoreJPEG::findFrameOffset(file, 0);
+	JPEG_ConfTypeDef config;
+
+	while (frame_offset != 0) {
+		file.seek(frame_offset);
+
+		frame_size = _corejpeg.decodeImage(file);
+
+		// if first frame, get file info
+		if (frame_index == 0) {
+			config = _corejpeg.getConfig();
+		}
+
+		frame_index += 1;
+
+		_coredma2d.transferImage(config.ImageWidth, config.ImageHeight, CoreJPEG::getWidthOffset(config));
+
+		// get next frame offset
+		frame_offset = CoreJPEG::findFrameOffset(file, frame_offset + frame_size + 4);
+
+		display();
+	}
+	log_info("%d frames", frame_index);
+}
+
+void VideoKit::display()
 {
 	// wait for DMA2D to finish transfer
 	while (_coredma2d.getHandle().State != HAL_DMA2D_STATE_READY)
 		;
-	// refresh screen
-	_coredsi.refresh();
-	// wait for DSI to finish refresh
+
+	refresh();
+	tick();
+
+	// wait for DSI to finish refresh before starting the new frame
 	while (_coredsi.isBusy())
 		;
+}
 
+void VideoKit::refresh()
+{
+	_coredsi.refresh();
+}
+
+void VideoKit::tick()
+{
 	auto dt = rtos::Kernel::Clock::now() - _last_time;
-	if (dt < 40ms) {
-		rtos::ThisThread::sleep_for(40ms - dt);
+
+	if (dt < _frametime) {
+		rtos::ThisThread::sleep_for(_frametime - dt);
 	}
 
-	log_info("(%ld) %ld ms = %f fps", dt, 1000.f / dt.count());
+	dt = rtos::Kernel::Clock::now() - _last_time;
+	log_info("(%ld) %ld ms = %f fps", dt.count(), 1000.f / dt.count());
+
 	_last_time = rtos::Kernel::Clock::now();
 }
