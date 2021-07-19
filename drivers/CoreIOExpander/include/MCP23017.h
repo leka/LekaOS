3cd683b0dd2ca8d017530a22009a72f4c943627f
Yann Locatelli <yann.locatelli2@gmail.com>
2021-07-19 20:12:12 +0200

    :construction: Add initial structure

---
+++ drivers/CoreIOExpander/include/MCP23017.h
@@ -0,0 +1,24 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_DRIVER_MCP23017_H_
+#define _LEKA_OS_DRIVER_MCP23017_H_
+
+#include "drivers/DigitalIn.h"
+#include "drivers/interfaces/InterfaceDigitalIn.h"
+
+#include "CoreI2C.h"
+
+// ? MCP23017 driver datasheet available at:
+// ? https://ww1.microchip.com/downloads/en/DeviceDoc/20001952C.pdf
+
+namespace leka {
+
+class MCP23017
+{
+};
+
+}	// namespace leka
+
+#endif	 //_LEKA_OS_DRIVER_MCP23017_H_
