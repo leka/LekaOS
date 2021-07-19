9f1037fc26c5fda84e59c188330d68a57cbb8a23
Aermanio <max92290.blanc@gmail.com>
2021-07-19 17:03:22 +0200

    Create vibration patterns and implement them in a test to run on the robot

--- spikes/lk_vibrations/VibrationsUtils.h
+++ spikes/lk_vibrations/VibrationsUtils.h
@@ -8,22 +8,29 @@
 
 #define SIN_PERIOD_SIZE 16
 #define SIN_FREQUENCY	128
-#define OUT_BUFF_SIZE	256
+#define OUT_BUFF_SIZE	512
 #define SAMPLING_RATE	2048
-#define DAC_MAX_VAL		0xFFF
+#define DAC_MAX_VAL		0x3FF
 
 #include <array>
 #include <cmath>
 #include <cstdint>
 
+#include "rtos/ThisThread.h"
+
+#include "CoreDAC.h"
+#include "CoreDACTimer.h"
+
+using namespace leka;
+
 enum EnvelopType
 {
 	Window,
 	Sawtooth,
-	Click,	 // small fast
-	Ramp,	 // Up 2/3 of the time and down 1/3
-	Buzz,	 // round
-	Pulse	 // multiple maximums
+	Click,		  // small fast
+	Triangular,	  // Up 2/3 of the time and down 1/3
+	Buzz,		  // round
+	Pulse		  // multiple maximums
 };
 
 void fillBufferWithSinWave(uint16_t *buffer, uint32_t bufferSize, uint32_t frequency, uint32_t samplingRate = 44100,
@@ -32,5 +39,8 @@ void fillBufferWithSinWave(uint16_t *buffer, uint32_t bufferSize, uint32_t frequ
 void fillBufferWithEnvelop(uint16_t *buffer, uint32_t bufferSize, EnvelopType eType, uint16_t maxVal = 0xFFFF);
 
 void fillEnvelop(uint16_t *buffer, uint32_t bufferSize, uint16_t envelopMaxVal);
+
+void playPattern(uint16_t *buffer, uint32_t bufferSize, EnvelopType eType, uint16_t numTimes, CoreDACTimer &dtimer,
+				 CoreDAC &dac);
 
 #endif	 // _LEKA_OS_SPIKE_VIBRATION_UTILS_H_
--- spikes/lk_vibrations/main.cpp
+++ spikes/lk_vibrations/main.cpp
@@ -3,9 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "drivers/BufferedSerial.h"
-#include "rtos/ThisThread.h"
 
-#include "CoreAudio.h"
 #include "HelloWorld.h"
 #include "LKCoreSTM32Hal.h"
 #include "VibrationsUtils.h"
@@ -15,13 +13,13 @@ using namespace std::chrono;
 
 // uint16_t _outBuffer[OUT_BUFF_SIZE] = {0};
 
+mbed::DigitalOut audio_enable(SOUND_ENABLE, 1);
 std::array<uint16_t, OUT_BUFF_SIZE> _outBuffer;
 auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
 
 LKCoreSTM32Hal hal;
 CoreDACTimer coreDACTimer(hal);
 CoreDAC coreDAC(hal);
-CoreAudio coreAudio(hal, coreDAC, coreDACTimer);
 
 /**
  * @brief This function handles DMA1 stream5 global interrupt.
@@ -35,6 +33,45 @@ void DMA1_Stream5_IRQHandler()
 
 auto main() -> int
 {
-	while (true) {
+	printf("\n\nStarting vibration process, hold your kids\n\n\n");
+
+	coreDACTimer.initialize(SAMPLING_RATE);
+	coreDAC.initialize();
+
+	// fillBufferWithEnvelop(_outBuffer.data(), OUT_BUFF_SIZE, Buzz, 0xFFFF);
+	// fillEnvelop(_outBuffer.data(), OUT_BUFF_SIZE, 0xFFFF);
+
+	// coreDAC.start(_outBuffer.data(), OUT_BUFF_SIZE);
+	// coreDACTimer.start();
+
+	// int count = 0;
+	// printf("Start of loop\n");
+	// while (count < 20) {
+	// 	if (coreDAC.dmaFlag() == CoreDAC::Cpt) {
+	// 		count++;
+	// 		coreDACTimer.stop();
+	// 		wait_us(500000);
+	// 		coreDACTimer.start();
+	// 		printf("count : %d\n", count);
+	// 		coreDAC.dmaFlag() = CoreDAC::None;
+	// 	}
+	// }
+	// printf("End of loop\n");
+	// coreDACTimer.stop();
+	// coreDAC.stop();
+
+	std::array<EnvelopType, 4> types = {Window, Sawtooth, Triangular, Buzz};
+	int numTimes					 = 10;
+	for (auto t: types) {
+		playPattern(_outBuffer.data(), OUT_BUFF_SIZE / 2, t, numTimes, coreDACTimer, coreDAC);
+		rtos::ThisThread::sleep_for(2s);
 	}
+	rtos::ThisThread::sleep_for(4s);
+	for (auto t: types) {
+		playPattern(_outBuffer.data(), OUT_BUFF_SIZE, t, numTimes, coreDACTimer, coreDAC);
+		rtos::ThisThread::sleep_for(2s);
+	}
+
+	coreDACTimer.deInitialize();
+	coreDAC.deInitialize();
 }
