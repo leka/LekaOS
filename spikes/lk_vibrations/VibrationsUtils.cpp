250d7489ab015289c528297dcf0236205ed40e10
Aermanio <max92290.blanc@gmail.com>
2021-07-19 17:03:22 +0200

    Create vibration patterns and implement them in a test to run on the robot

--- spikes/lk_vibrations/VibrationsUtils.cpp
+++ spikes/lk_vibrations/VibrationsUtils.cpp
@@ -1,5 +1,34 @@
 #include "VibrationsUtils.h"
 
+using namespace leka;
+using namespace std::chrono;
+
+void playPattern(uint16_t *buffer, uint32_t bufferSize, EnvelopType eType, uint16_t numTimes, CoreDACTimer &dtimer,
+				 CoreDAC &dac)
+{
+	fillBufferWithEnvelop(buffer, bufferSize, eType, 0xFFFF);
+	fillEnvelop(buffer, bufferSize, 0xFFFF);
+
+	dac.start(buffer, bufferSize);
+	dtimer.start();
+
+	int count = 0;
+	// printf("Start of loop\n");
+	while (count < numTimes) {
+		if (dac.dmaFlag() == CoreDAC::Cpt) {
+			dtimer.stop();
+			count++;
+			dac.dmaFlag() = CoreDAC::None;
+			rtos::ThisThread::sleep_for(500ms);
+			dtimer.start();
+			// printf("count : %d\n", count);
+		}
+	}
+	// printf("End of loop\n");
+	dtimer.stop();
+	dac.stop();
+}
+
 // Fill buffer with sin wave at given frequency and sampling rate
 // Values can be limited between maxValue and minValue
 void fillBufferWithSinWave(uint16_t *buffer, uint32_t bufferSize, uint32_t frequency, uint32_t samplingRate,
@@ -18,8 +47,16 @@ void fillBufferWithEnvelop(uint16_t *buffer, uint32_t bufferSize, EnvelopType eT
 {
 	switch (eType) {
 		case Click:	  // TODO : code this option
-		case Buzz:	  // TODO : code this option
+
+		case Buzz:	 // TODO : code this option
+			for (uint32_t i = 0; i < bufferSize; ++i) {
+				float tmp = sin(i * M_PI / bufferSize);
+				buffer[i] = tmp * maxVal;
+			}
+			break;
 		case Pulse:	  // TODO : code this option
+
+			break;
 		case Window:
 			for (uint32_t i = 0; i < bufferSize; ++i) {
 				buffer[i] = maxVal;
@@ -32,10 +69,10 @@ void fillBufferWithEnvelop(uint16_t *buffer, uint32_t bufferSize, EnvelopType eT
 			}
 			break;
 
-		case Ramp:
+		case Triangular:
 			uint32_t riseTime = 2 * bufferSize / 3;
 			for (uint32_t i = 0; i < riseTime; ++i) {
-				buffer[i] = i * ((double)maxVal / bufferSize);
+				buffer[i] = i * ((double)maxVal / riseTime);
 			}
 			for (uint32_t i = riseTime; i < bufferSize; ++i) {
 				buffer[i] = maxVal - i * ((double)maxVal / (bufferSize - riseTime));
@@ -48,7 +85,7 @@ void fillEnvelop(uint16_t *buffer, uint32_t bufferSize, uint16_t envelopMaxVal)
 {
 	std::array<uint16_t, SIN_PERIOD_SIZE> sinPeriod = {0};
 
-	fillBufferWithSinWave(sinPeriod.begin(), SIN_PERIOD_SIZE, SIN_FREQUENCY, SAMPLING_RATE, DAC_MAX_VAL, 0x0);
+	fillBufferWithSinWave(sinPeriod.data(), SIN_PERIOD_SIZE, SIN_FREQUENCY, SAMPLING_RATE, DAC_MAX_VAL, 0x0);
 
 	uint32_t i = 0;
 	while (i < bufferSize) {
