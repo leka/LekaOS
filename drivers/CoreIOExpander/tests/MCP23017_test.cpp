6fa8d21862166c02886911bf90d311d5eb7b521f
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-07-26 10:25:30 +0200

    :construction: (drivers): Clean existing tests

--- drivers/CoreIOExpander/tests/MCP23017_test.cpp
+++ drivers/CoreIOExpander/tests/MCP23017_test.cpp
@@ -28,6 +28,20 @@ class CoreMCP23017Test : public ::testing::Test
 
 	MCP23017 coreIOExpander;
 	LKCoreI2CMock i2cMock;
+
+	void readRegister(uint16_t pin, std::array<uint8_t, 2> &values)
+	{
+		const auto expected_register = ElementsAre(pin);
+		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_register));
+
+		EXPECT_CALL(i2cMock, read).WillOnce(DoAll(SetArrayArgument<1>(begin(values), end(values)), Return(0)));
+	}
+
+	void writeRegister(uint16_t pin, std::array<uint8_t, 2> values)
+	{
+		const auto data = ElementsAre(pin, values[0], values[1]);
+		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(data));
+	}
 };
 
 TEST_F(CoreMCP23017Test, instantiation)
@@ -37,11 +51,16 @@ TEST_F(CoreMCP23017Test, instantiation)
 
 TEST_F(CoreMCP23017Test, setRegisterMapping)
 {
-	const auto expected_IODIR_values = ElementsAre(mcp23017::registers::IOCON, 0x00, 0x00);
-	EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_IODIR_values));
+	std::array<uint8_t, 2> expected_IODIR_values = {0x00, 0x00};
+	writeRegister(mcp23017::registers::IOCON, expected_IODIR_values);
 
 	coreIOExpander.setRegisterMapping();
 }
+
+// std::array<uint8_t, 2> expected_INTCON_values = {0x00, 0x00};
+// 		std::array<uint8_t, 2> actual_INTCON_values	  = {0x00, 0x00};
+// 		readRegister(mcp23017::registers::INTCON, expected_INTCON_values);
+// 		writeRegister(mcp23017::registers::INTCON, actual_INTCON_values);
 
 TEST_F(CoreMCP23017Test, reset)
 {
@@ -62,15 +81,11 @@ TEST_F(CoreMCP23017Test, setInputPin)
 {
 	{
 		InSequence seq;
-		const auto expected_register = ElementsAre(mcp23017::registers::IODIR);
-		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_register));
 
-		std::array<uint8_t, 2> returned_values = {0x00, 0x0A};
-		EXPECT_CALL(i2cMock, read)
-			.WillOnce(DoAll(SetArrayArgument<1>(begin(returned_values), end(returned_values)), Return(0)));
-
-		const auto expected_IODIR_values = ElementsAre(mcp23017::registers::IODIR, 0x01, 0x0A);
-		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_IODIR_values));
+		std::array<uint8_t, 2> expected_IODIR_values = {0x00, 0x0A};
+		std::array<uint8_t, 2> actual_IODIR_values	 = {0x01, 0x0A};
+		readRegister(mcp23017::registers::IODIR, expected_IODIR_values);
+		writeRegister(mcp23017::registers::IODIR, actual_IODIR_values);
 	}
 
 	coreIOExpander.setInputPin(MCP23017::Pin::Pin_PA0);
@@ -81,15 +96,10 @@ TEST_F(CoreMCP23017Test, setOutputPin)
 	coreIOExpander.setInputPin(MCP23017::Pin::Pin_PA0);
 	{
 		InSequence seq;
-		const auto expected_register = ElementsAre(mcp23017::registers::IODIR);
-		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_register));
-
-		std::array<uint8_t, 2> returned_values = {0x01, 0x0A};
-		EXPECT_CALL(i2cMock, read)
-			.WillOnce(DoAll(SetArrayArgument<1>(begin(returned_values), end(returned_values)), Return(0)));
-
-		const auto expected_IODIR_values = ElementsAre(mcp23017::registers::IODIR, 0x00, 0x0A);
-		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_IODIR_values));
+		std::array<uint8_t, 2> expected_IODIR_values = {0x01, 0x0A};
+		std::array<uint8_t, 2> actual_IODIR_values	 = {0x00, 0x0A};
+		readRegister(mcp23017::registers::IODIR, expected_IODIR_values);
+		writeRegister(mcp23017::registers::IODIR, actual_IODIR_values);
 	}
 
 	coreIOExpander.setOutputPin(MCP23017::Pin::Pin_PA0);
@@ -100,12 +110,9 @@ TEST_F(CoreMCP23017Test, readInputs)
 	coreIOExpander.setInputPin(MCP23017::Pin::Pin_PA0);
 	{
 		InSequence seq;
-		const auto expected_register = ElementsAre(mcp23017::registers::GPIO);
-		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_register));
 
-		std::array<uint8_t, 2> returned_values = {0x00, 0xff};
-		EXPECT_CALL(i2cMock, read)
-			.WillOnce(DoAll(SetArrayArgument<1>(begin(returned_values), end(returned_values)), Return(0)));
+		std::array<uint8_t, 2> expected_GPIO_values = {0x00, 0xff};
+		readRegister(mcp23017::registers::GPIO, expected_GPIO_values);
 	}
 
 	auto GPOI_values = coreIOExpander.readInputs();
@@ -116,9 +123,8 @@ TEST_F(CoreMCP23017Test, setInputPolarity)
 {
 	uint16_t polarity_value = MCP23017::Pin::Pin_PA0 | MCP23017::Pin::Pin_PB5;
 
-	const auto expected_pin_values =
-		ElementsAre(mcp23017::registers::IPOL, MCP23017::Pin::Pin_PA0, MCP23017::Pin::Pin_PB5 >> 8);
-	EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_pin_values));
+	std::array<uint8_t, 2> actual_IPOL_values = {MCP23017::Pin::Pin_PA0, MCP23017::Pin::Pin_PB5 >> 8};
+	writeRegister(mcp23017::registers::IPOL, actual_IPOL_values);
 
 	coreIOExpander.setInputPolarity(polarity_value);
 }
@@ -130,12 +136,9 @@ TEST_F(CoreMCP23017Test, getInputPolarity)
 
 	{
 		InSequence seq;
-		const auto expected_pin_values = ElementsAre(mcp23017::registers::IPOL);
-		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_pin_values));
 
-		std::array<uint8_t, 2> returned_values = {0x01, 0x20};
-		EXPECT_CALL(i2cMock, read)
-			.WillOnce(DoAll(SetArrayArgument<1>(begin(returned_values), end(returned_values)), Return(0)));
+		std::array<uint8_t, 2> expected_IPOL_values = {0x01, 0x20};
+		readRegister(mcp23017::registers::IPOL, expected_IPOL_values);
 	}
 
 	auto actual_polarity_values = coreIOExpander.getInputPolarity();
@@ -146,9 +149,8 @@ TEST_F(CoreMCP23017Test, setPullups)
 {
 	uint16_t polarity_value = MCP23017::Pin::Pin_PA0 | MCP23017::Pin::Pin_PB5;
 
-	const auto expected_pin_values =
-		ElementsAre(mcp23017::registers::GPPU, MCP23017::Pin::Pin_PA0, MCP23017::Pin::Pin_PB5 >> 8);
-	EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_pin_values));
+	std::array<uint8_t, 2> actual_GPPU_values = {MCP23017::Pin::Pin_PA0, MCP23017::Pin::Pin_PB5 >> 8};
+	writeRegister(mcp23017::registers::GPPU, actual_GPPU_values);
 
 	coreIOExpander.setPullups(polarity_value);
 }
@@ -160,12 +162,9 @@ TEST_F(CoreMCP23017Test, getPullups)
 
 	{
 		InSequence seq;
-		const auto expected_pin_values = ElementsAre(mcp23017::registers::GPPU);
-		EXPECT_CALL(i2cMock, write).With(Args<1, 2>(expected_pin_values));
 
-		std::array<uint8_t, 2> returned_values = {0x01, 0x20};
-		EXPECT_CALL(i2cMock, read)
-			.WillOnce(DoAll(SetArrayArgument<1>(begin(returned_values), end(returned_values)), Return(0)));
+		std::array<uint8_t, 2> expected_GPPU_values = {0x01, 0x20};
+		readRegister(mcp23017::registers::GPPU, expected_GPPU_values);
 	}
 
 	auto actual_pullups_values = coreIOExpander.getPullups();
