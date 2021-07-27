e1425c7a76ebdda713c695ef650974719ec38e74
Aermanio <max92290.blanc@gmail.com>
2021-07-27 18:00:28 +0200

    :construction: Continue progress on vibration demos

--- spikes/lk_vibrations/main.cpp
+++ spikes/lk_vibrations/main.cpp
@@ -37,43 +37,75 @@ void DMA1_Stream5_IRQHandler()
 }
 }
 
+void cardiacCoherenceDemo()
+{
+	printf("\n\nCardiac coherence demo!!\n");
+	VibrationTemplate vib(5s, 128, 0.1, false);
+	coreVib.playPeriodically(vib, 5s, 6);
+	while (coreVib.isPlayingPeriodically()) {
+		ThisThread::sleep_for(50ms);
+	}
+}
+
+void countVibsDemo()
+{
+	printf("\n\nCount vibrations demo!!\n");
+	VibrationTemplate vib(1s, 128, 0.1, false);
+	for (int i = 0; i < 5; i++) {
+		int reps = rand() % 9 + 1;
+		coreVib.playPeriodically(vib, 500ms, reps);
+
+		while (coreVib.isPlayingPeriodically()) {
+			ThisThread::sleep_for(50ms);
+		}
+		printf("The number of times to guess was: %d\n", reps);
+		ThisThread::sleep_for(2s);
+	}
+}
+
 auto main() -> int
 {
 	printf("\n\nStarting vibration process, hold your kids\n\n\n");
 	coreVib.initialize(2048);
 
-	float duration	   = 2.0;
-	uint32_t frequency = 128;
-	float amplitude	   = 0.1;
+	// float duration	   = 2.0;
+	// uint32_t frequency = 128;
+	// float amplitude	   = 0.1;
 
-	VibrationTemplate vib1(duration, frequency, amplitude, false);
-	VibrationTemplate vib2(1, 200, 0.3, false);
-	VibrationTemplate vib3(0.5, 128, 0.1, false);
-	VibrationTemplate pause_1s(1, 1, 0, false);
-	VibrationTemplate pause_2s(2, 1, 0, false);
-	VibrationTemplate pause_500ms(0.5, 1, 0, false);
+	// VibrationTemplate vib1(duration, frequency, amplitude, false);
+	// VibrationTemplate vib2(1, 200, 0.3, false);
+	// VibrationTemplate vib3(0.5, 128, 0.1, false);
+	// VibrationTemplate pause_1s(1, 1, 0, false);
+	// VibrationTemplate pause_2s(2, 1, 0, false);
+	// VibrationTemplate pause_500ms(0.5, 1, 0, false);
 
-	vector<VibrationTemplate> sequence;
-	sequence.push_back(vib1);
-	sequence.push_back(pause_2s);
+	// vector<VibrationTemplate> sequence;
+	// sequence.push_back(vib1);
+	// sequence.push_back(pause_2s);
 
-	sequence.push_back(vib2);
-	sequence.push_back(pause_1s);
-	sequence.push_back(vib2);
-	sequence.push_back(pause_1s);
+	// sequence.push_back(vib2);
+	// sequence.push_back(pause_1s);
+	// sequence.push_back(vib2);
+	// sequence.push_back(pause_1s);
 
-	sequence.push_back(vib3);
-	sequence.push_back(pause_500ms);
-	sequence.push_back(vib3);
-	sequence.push_back(pause_500ms);
-	sequence.push_back(vib3);
-	sequence.push_back(pause_500ms);
-	sequence.push_back(vib3);
-	sequence.push_back(pause_500ms);
+	// sequence.push_back(vib3);
+	// sequence.push_back(pause_500ms);
+	// sequence.push_back(vib3);
+	// sequence.push_back(pause_500ms);
+	// sequence.push_back(vib3);
+	// sequence.push_back(pause_500ms);
+	// sequence.push_back(vib3);
+	// sequence.push_back(pause_500ms);
 
+	// coreVib.play(vib1);
+
+	cardiacCoherenceDemo();
+	countVibsDemo();
+
+	printf("\n\nEnd of demos\n\n");
 	while (true) {
-		printf("Playing vibrations !\n");
-		coreVib.playVibrationSequence(sequence);
+		// printf("Playing vibrations !\n");
+		// coreVib.playSequence(sequence);
 		rtos::ThisThread::sleep_for(1s);
 	}
 
