5dd890ed3686409cf2943d534351704af7ce2179
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-02 15:17:42 +0200

    :construction: (interface): Create interfaces for Dac and Timer

---
+++ include/interface/drivers/DacTimer.h
@@ -0,0 +1,34 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_INTERFACE_DRIVER_DAC_TIMER_H_
+#define _LEKA_OS_INTERFACE_DRIVER_DAC_TIMER_H_
+
+#include "stm32f7xx_hal.h"
+
+namespace leka::interface {
+
+class DacTimer
+{
+  public:
+	~DacTimer() = default;
+
+	virtual void initialize(float frequency) = 0;
+	virtual void deInitialize()				 = 0;
+	virtual void start()					 = 0;
+	virtual void stop()						 = 0;
+
+	virtual auto getHandle() -> TIM_HandleTypeDef = 0;
+
+  protected:
+	virtual auto _calculatePeriod(float freq) -> uint32_t = 0;
+
+	virtual void _registerMspCallbacks() = 0;
+	virtual void _mspInitCallback()		 = 0;
+	virtual void _mspDeInitCallback()	 = 0;
+};
+
+}	// namespace leka::interface
+
+#endif	 // _LEKA_OS_INTERFACE_DRIVER_DAC_TIMER_H_
