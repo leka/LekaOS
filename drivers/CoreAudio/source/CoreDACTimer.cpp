47f6a1fbd77dada65f9477683e1b3ba624cb8fcc
Aermanio <max92290.blanc@gmail.com>
2021-08-02 16:57:20 +0200

    :construction: (drivers): Add init and deInit methods

--- drivers/CoreAudio/source/CoreDACTimer.cpp
+++ drivers/CoreAudio/source/CoreDACTimer.cpp
@@ -11,6 +11,29 @@ CoreDACTimer::CoreDACTimer(LKCoreSTM32HalBase &hal) : _hal(hal)
 	_htim.Instance = TIM6;
 }
 
+void CoreDACTimer::initialize(float frequency)
+{
+	_htim.Init.Prescaler		 = 0;	// no need of prescaler for high frequencies
+	_htim.Init.CounterMode		 = TIM_COUNTERMODE_UP;
+	_htim.Init.Period			 = _calculatePeriod(frequency);
+	_htim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;	 // no need to change counter period while working
+
+	_registerMspCallbacks();
+
+	_hal.HAL_TIM_Base_Init(&_htim);
+
+	TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;			   // trigger used for DAC
+	sMasterConfig.MasterSlaveMode	  = TIM_MASTERSLAVEMODE_DISABLE;   // TODO(): verify utility of this parameter
+	_hal.HAL_TIMEx_MasterConfigSynchronization(&_htim, &sMasterConfig);
+}
+
+void CoreDACTimer::deInitialize()
+{
+	_hal.HAL_TIM_Base_DeInit(&_htim);
+}
+
 void CoreDACTimer::_registerMspCallbacks()
 {
 	static auto *self = this;
