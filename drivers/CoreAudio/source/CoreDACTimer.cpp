1ae4f51f495886597add7915be1e18e000be8226
Aermanio <max92290.blanc@gmail.com>
2021-08-02 16:54:15 +0200

    :construction: (drivers): Add MSP callbacks management

--- drivers/CoreAudio/source/CoreDACTimer.cpp
+++ drivers/CoreAudio/source/CoreDACTimer.cpp
@@ -11,4 +11,27 @@ CoreDACTimer::CoreDACTimer(LKCoreSTM32HalBase &hal) : _hal(hal)
 	_htim.Instance = TIM6;
 }
 
+void CoreDACTimer::_registerMspCallbacks()
+{
+	static auto *self = this;
+	_hal.HAL_TIM_RegisterCallback(&_htim, HAL_TIM_BASE_MSPINIT_CB_ID,
+								  [](TIM_HandleTypeDef *htim) { self->_mspInitCallback(); });
+	_hal.HAL_TIM_RegisterCallback(&_htim, HAL_TIM_BASE_MSPDEINIT_CB_ID,
+								  [](TIM_HandleTypeDef *htim) { self->_mspDeInitCallback(); });
+}
+
+void CoreDACTimer::_mspInitCallback()
+{
+	if (_htim.Instance == TIM6) {
+		_hal.HAL_RCC_TIM6_CLK_ENABLE();
+	}
+}
+
+void CoreDACTimer::_mspDeInitCallback()
+{
+	if (_htim.Instance == TIM6) {
+		_hal.HAL_RCC_TIM6_CLK_DISABLE();
+	}
+}
+
 }	// namespace leka
