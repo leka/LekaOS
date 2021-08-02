b32a35bbd0e28e341896d57801d6c424a578f992
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-02 16:08:50 +0200

    :sparkles: (drivers): Update Hal class to wrap DAC and DacTimer calls to HAL functions

--- drivers/LKCoreSTM32Hal/include/LKCoreSTM32HalBase.h
+++ drivers/LKCoreSTM32Hal/include/LKCoreSTM32HalBase.h
@@ -6,6 +6,7 @@
 #define _LEKA_OS_STM32_HAL_BASE_H_
 
 #include "stm32f7xx_hal.h"
+#include "stm32f7xx_hal_tim.h"
 
 namespace leka {
 
@@ -14,6 +15,7 @@ class LKCoreSTM32HalBase
   public:
 	virtual ~LKCoreSTM32HalBase() = default;
 
+	virtual void HAL_RCC_GPIOA_CLK_ENABLE(void) = 0;
 	virtual void HAL_RCC_GPIOD_CLK_ENABLE(void) = 0;
 	virtual void HAL_RCC_GPIOE_CLK_ENABLE(void) = 0;
 	virtual void HAL_RCC_GPIOF_CLK_ENABLE(void) = 0;
@@ -22,7 +24,10 @@ class LKCoreSTM32HalBase
 	virtual void HAL_RCC_GPIOI_CLK_ENABLE(void) = 0;
 	virtual void HAL_RCC_GPIOJ_CLK_ENABLE(void) = 0;
 
+	virtual void HAL_RCC_TIM6_CLK_ENABLE(void)													 = 0;
+	virtual void HAL_RCC_TIM6_CLK_DISABLE(void)													 = 0;
 	virtual void HAL_RCC_FMC_CLK_ENABLE(void)													 = 0;
+	virtual void HAL_RCC_DMA1_CLK_ENABLE(void)													 = 0;
 	virtual void HAL_RCC_DMA2_CLK_ENABLE(void)													 = 0;
 	virtual void HAL_RCC_JPEG_CLK_ENABLE(void)													 = 0;
 	virtual void HAL_RCC_LTDC_CLK_ENABLE(void)													 = 0;
@@ -34,12 +39,15 @@ class LKCoreSTM32HalBase
 	virtual void HAL_RCC_DSI_CLK_ENABLE(void)													 = 0;
 	virtual void HAL_RCC_DSI_FORCE_RESET(void)													 = 0;
 	virtual void HAL_RCC_DSI_RELEASE_RESET(void)												 = 0;
+	virtual void HAL_RCC_DAC_CLK_ENABLE(void)													 = 0;
+	virtual void HAL_RCC_DAC_CLK_DISABLE(void)													 = 0;
 	virtual HAL_StatusTypeDef HAL_RCCEx_PeriphCLKConfig(RCC_PeriphCLKInitTypeDef *PeriphClkInit) = 0;
 
 	virtual void HAL_LINKDMA(SDRAM_HandleTypeDef *hsdram, DMA_HandleTypeDef *hdma, DMA_HandleTypeDef dma_handle) = 0;
 
 	virtual void HAL_GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init)				   = 0;
 	virtual void HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) = 0;
+	virtual void HAL_GPIO_DeInit(GPIO_TypeDef *GPIOx, uint32_t GPIO_Pin)						   = 0;
 
 	virtual void HAL_NVIC_SetPriority(IRQn_Type IRQn, uint32_t PreemptPriority, uint32_t SubPriority) = 0;
 	virtual void HAL_NVIC_EnableIRQ(IRQn_Type IRQn)													  = 0;
@@ -81,6 +89,27 @@ class LKCoreSTM32HalBase
 											uint32_t InDataLength)											  = 0;
 	virtual void HAL_JPEG_ConfigOutputBuffer(JPEG_HandleTypeDef *hjpeg, uint8_t *pNewOutputBuffer,
 											 uint32_t OutDataLength)										  = 0;
+
+	virtual HAL_StatusTypeDef HAL_TIM_Base_Init(TIM_HandleTypeDef *htim)									= 0;
+	virtual HAL_StatusTypeDef HAL_TIMEx_MasterConfigSynchronization(TIM_HandleTypeDef *htim,
+																	TIM_MasterConfigTypeDef *sMasterConfig) = 0;
+	virtual HAL_StatusTypeDef HAL_TIM_RegisterCallback(TIM_HandleTypeDef *htim, HAL_TIM_CallbackIDTypeDef CallbackID,
+													   pTIM_CallbackTypeDef pCallback)						= 0;
+	virtual HAL_StatusTypeDef HAL_TIM_Base_Start(TIM_HandleTypeDef *htim)									= 0;
+	virtual HAL_StatusTypeDef HAL_TIM_Base_Stop(TIM_HandleTypeDef *htim)									= 0;
+	virtual HAL_StatusTypeDef HAL_TIM_Base_DeInit(TIM_HandleTypeDef *htim)									= 0;
+
+	virtual HAL_StatusTypeDef HAL_DAC_Init(DAC_HandleTypeDef *hdac)						  = 0;
+	virtual HAL_StatusTypeDef HAL_DAC_ConfigChannel(DAC_HandleTypeDef *hdac, DAC_ChannelConfTypeDef *sConfig,
+													uint32_t Channel)					  = 0;
+	virtual HAL_StatusTypeDef HAL_DAC_RegisterCallback(DAC_HandleTypeDef *hdac, HAL_DAC_CallbackIDTypeDef CallbackID,
+													   pDAC_CallbackTypeDef pCallback)	  = 0;
+	virtual HAL_StatusTypeDef HAL_DAC_Start(DAC_HandleTypeDef *hdac, uint32_t Channel)	  = 0;
+	virtual HAL_StatusTypeDef HAL_DAC_Start_DMA(DAC_HandleTypeDef *hdac, uint32_t Channel, uint32_t *pData,
+												uint32_t Length, uint32_t Alignment)	  = 0;
+	virtual HAL_StatusTypeDef HAL_DAC_Stop_DMA(DAC_HandleTypeDef *hdac, uint32_t Channel) = 0;
+	virtual HAL_StatusTypeDef HAL_DAC_DeInit(DAC_HandleTypeDef *hdac)					  = 0;
+	virtual uint32_t HAL_RCC_GetPCLK1Freq(void)											  = 0;
 };
 
 }	// namespace leka
