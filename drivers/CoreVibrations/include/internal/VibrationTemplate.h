890547ad082d74fc97d3b2d8cbd7bcebf7be7f79
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-03 17:43:59 +0200

    :sparkles: (drivers): Add VibrationTemplate.h

---
+++ drivers/CoreVibrations/include/internal/VibrationTemplate.h
@@ -0,0 +1,54 @@
+// Leka - LekaOS
+// Copyright 2020 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_VIBRATION_TEMPLATE_H_
+#define _LEKA_OS_LIB_VIBRATION_TEMPLATE_H_
+
+#include <chrono>
+#include <cstdio>
+#include <memory>
+
+#include "VibrationEnvelope.h"
+
+namespace leka {
+
+// TODO(): replace these redefines somewhere else
+using fmilliseconds = std::chrono::duration<float, std::milli>;
+using fseconds		= std::chrono::duration<float>;
+using fminutes		= std::chrono::duration<float, std::ratio<60>>;
+/*Redefines taken at : https://philippegroarke.com/posts/2018/chrono_for_humans/
+Allow to easily work with durations
+*/
+
+class VibrationTemplate
+{
+  public:
+	VibrationTemplate(fseconds duration, uint32_t frequency, float amplitude, VibrationEnvelope::EnvelopType eType,
+					  float triangDutyCycle = 1.F);
+
+	[[nodiscard]] auto getDuration() const -> const fseconds &;
+	[[nodiscard]] auto getFrequency() const -> const uint32_t &;
+	[[nodiscard]] auto getAmplitude() const -> const float &;
+
+	[[nodiscard]] auto getCurrentSample() const -> const uint32_t &;
+	[[nodiscard]] auto getTotalSamples() const -> const uint32_t &;
+
+	void setCurrentSample(uint32_t currentSample);
+	void setTotalSamples(uint32_t totalSamples);
+
+	void applyCurrentEnvelopeSlice(float *buffer, uint32_t nbSamples) const;
+
+  private:
+	fseconds _duration;
+	uint32_t _frequency;
+	float _amplitude;
+	std::shared_ptr<VibrationEnvelope> _envelope;
+
+	uint32_t _currentSample;
+	uint32_t _totalSamples;
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_LIB_VIBRATION_TEMPLATE_H_
