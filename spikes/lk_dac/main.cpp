cce0e32a9b3ce6dce18b55d3b5d92dcf270923c9
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-09 18:13:55 +0200

    :poop::construction: (spikes): Update DAC and Timer spike to test TIM7 use

--- spikes/lk_dac/main.cpp
+++ spikes/lk_dac/main.cpp
@@ -15,10 +15,14 @@ mbed::DigitalOut audio_enable(SOUND_ENABLE, 1);
 static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
 
 LKCoreSTM32Hal hal;
-CoreDACTimer coreTimer(hal);
+
+CoreDACTimer coreTimer_6(hal, CoreDACTimer::HardWareBasicTimer::BasicTimer6);
+CoreDACTimer coreTimer_7(hal, CoreDACTimer::HardWareBasicTimer::BasicTimer7);
 CoreDAC coreDac(hal);
 
-bool ended = false;
+uint32_t _count								 = 0;
+bool _ended									 = false;
+CoreDACTimer::HardWareBasicTimer _currentTim = CoreDACTimer::HardWareBasicTimer::BasicTimer6;
 
 /**
  * @brief This function handles DMA1 stream5 global interrupt.
@@ -44,42 +48,63 @@ void fillBufferWithSinWave(uint16_t *buffer, uint32_t bufferSize, uint32_t frequ
 
 void callbackTest(DAC_HandleTypeDef *hdac)
 {
-	static uint32_t count	 = 0;
 	static const int numSecs = 3;
-	count++;
-	if (count == (numSecs * 44100 / 256)) {
-		coreTimer.stop();
-		coreDac.stop();
-		ended = true;
+	if (!_ended) {
+		_count++;
+	} else {
+		_count = 0;
+	}
+	if (_count == (numSecs * 44100 / 256)) {
+		// if (_count == 5) {
+		if (_currentTim == CoreDACTimer::HardWareBasicTimer::BasicTimer6) {
+			coreTimer_6.stop();
+		} else {
+			coreTimer_7.stop();
+		}
+		_ended = true;
 	}
 }
 
 auto main() -> int
 {
-	std::array<uint16_t, 512> outBuff;
+	std::array<uint16_t, 512> outBuff {};
+	fillBufferWithSinWave(outBuff.data(), 512, 512, 44100, 0x999, 0x666);
 
 	printf("\n\nHello, investigation day!\n\n");
+
+	//
 	coreDac.setOnHalfBufferReadPtr(&callbackTest);
 	coreDac.setOnFullBufferReadPtr(&callbackTest);
-	// Init
-	coreTimer.initialize(44100);
-	coreDac.initialize();
 
-	// generate data
-	fillBufferWithSinWave(outBuff.data(), 512, 512, 44100, 0x999, 0x666);
+	// Init
+	coreTimer_6.initialize(44100);
+	coreTimer_7.initialize(22050);
+	coreDac.initialize(coreTimer_6);
 
 	// start
 	auto outSpan = lstd::span {outBuff.data(), outBuff.size()};
-	coreTimer.start();
+
+	printf("First timer\n");
+	coreTimer_6.start();
 	coreDac.start(outSpan);
 
 	// wait preset period
-	while (!ended) {
+	while (!_ended) {
 		rtos::ThisThread::sleep_for(50ms);
 	}
 
-	printf("End of audio test!\n\n");
+	rtos::ThisThread::sleep_for(2s);
+	printf("Second timer\n");
 
-	coreDac.terminate();
-	coreTimer.terminate();
+	coreDac.stop();
+	// coreDac.configTimer(coreTimer_7);
+	coreTimer_6.start();
+	// coreDac.start(outSpan);
+
+	_ended = false;
+	while (!_ended) {
+		rtos::ThisThread::sleep_for(50ms);
+	}
+
+	printf("End of DAC and Timer test!\n\n");
 }
