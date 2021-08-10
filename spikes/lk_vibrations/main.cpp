799c122f436d695535af907abaee6f9dc86ec1fd
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-10 16:02:27 +0200

    :bug: (drivers): Fix CoreVibration rebase

--- spikes/lk_vibrations/main.cpp
+++ spikes/lk_vibrations/main.cpp
@@ -19,7 +19,7 @@ mbed::DigitalOut audio_enable(SOUND_ENABLE, 1);
 auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
 
 LKCoreSTM32Hal hal;
-CoreDACTimer coreDACTimer(hal);
+CoreDACTimer coreDACTimer(hal, CoreDACTimer::HardWareBasicTimer::BasicTimer6);
 CoreDAC coreDAC(hal);
 
 rtos::Thread vibrationsThread;
