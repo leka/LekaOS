7c677926e5782797d29588b342bad3e6a75e6d51
Aermanio <max92290.blanc@gmail.com>
2021-08-10 17:37:24 +0200

    :construction::poop: (spikes): Test using motor in pair with the imu sensor for stabilization

--- spikes/lk_sensors_imu/main.cpp
+++ spikes/lk_sensors_imu/main.cpp
@@ -1,10 +1,23 @@
-#include "mbed.h"
+// #include "mbed.h"
 
+#include "PinNames.h"
+
+#include "drivers/BufferedSerial.h"
+#include "drivers/DigitalOut.h"
+#include "drivers/PwmOut.h"
+#include "platform/Callback.h"
+#include "rtos/ThisThread.h"
+#include "rtos/Thread.h"
+
+#include "CoreMotor.h"
+#include "CorePwm.h"
 #include "DigitalOut.h"
+#include "HelloWorld.h"
 #include "LSM6DSOX_CommunicationI2C.h"
 #include "LSM6DSOX_ComponentAccelerometer.h"
 #include "LSM6DSOX_ComponentGyroscope.h"
 #include "LSM6DSOX_DataGatherer.h"
+#include "LogKit.h"
 
 //##################################################################################################
 // Defines
@@ -14,19 +27,11 @@
 #define PIN_I2C1_SCL	  (SENSOR_IMU_TH_I2C_SCL)
 #define PIN_LSM6DSOX_INT1 (SENSOR_IMU_IRQ)
 
-//##################################################################################################
-// Structs
-//##################################################################################################
-
-//##################################################################################################
-// Prototypes
-//##################################################################################################
+using namespace leka;
+using namespace std::chrono;
 
 void tickerCB();
 
-//##################################################################################################
-// Global Variables
-//##################################################################################################
 I2C i2c1(PIN_I2C1_SDA, PIN_I2C1_SCL);
 Communication::LSM6DSOX_I2C lsm6dsox_i2c(i2c1);
 
@@ -40,12 +45,53 @@ Component::LSM6DSOX_DataGatherer dataGatherer(lsm6dsox_i2c, PIN_LSM6DSOX_INT1, 1
 DigitalIn INT_1_LSM6DSOX(PIN_LSM6DSOX_INT1, PullNone);
 
 // UnbufferedSerial serial(USBTX, USBRX);
-//##################################################################################################
-// Main
-//##################################################################################################
+
+void spinLeft(CoreMotorBase &left, CoreMotorBase &right, float speed)
+{
+	left.spin(Rotation::clockwise, speed);
+	right.spin(Rotation::clockwise, speed);
+}
+
+void spinRight(CoreMotorBase &left, CoreMotorBase &right, float speed)
+{
+	left.spin(Rotation::counterClockwise, speed);
+	right.spin(Rotation::counterClockwise, speed);
+}
+
+void goForward(CoreMotorBase &left, CoreMotorBase &right, float speed)
+{
+	left.spin(Rotation::counterClockwise, speed);
+	right.spin(Rotation::clockwise, speed);
+}
+
+void goBackward(CoreMotorBase &left, CoreMotorBase &right, float speed)
+{
+	left.spin(Rotation::clockwise, speed);
+	right.spin(Rotation::counterClockwise, speed);
+}
+
+void stop(CoreMotorBase &left, CoreMotorBase &right)
+{
+	left.stop();
+	right.stop();
+}
 
 auto main() -> int
 {
+	static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
+	leka::logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
+
+	auto motor_left_dir_1 = mbed::DigitalOut {MOTOR_LEFT_DIRECTION_1};
+	auto motor_left_dir_2 = mbed::DigitalOut {MOTOR_LEFT_DIRECTION_2};
+	auto motor_left_speed = CorePwm {MOTOR_LEFT_PWM};
+
+	auto motor_right_dir_1 = mbed::DigitalOut {MOTOR_RIGHT_DIRECTION_1};
+	auto motor_right_dir_2 = mbed::DigitalOut {MOTOR_RIGHT_DIRECTION_2};
+	auto motor_right_speed = CorePwm {MOTOR_RIGHT_PWM};
+
+	auto motor_left	 = CoreMotor {motor_left_dir_1, motor_left_dir_2, motor_left_speed};
+	auto motor_right = CoreMotor {motor_right_dir_1, motor_right_dir_2, motor_right_speed};
+
 	// Time for sensor init
 	ThisThread::sleep_for(10ms);   // waiting for sensor startup
 
@@ -74,11 +120,58 @@ auto main() -> int
 		"data\n");
 	printf("\n\n");
 
-	while (true) {
-		dataGatherer.onTick();
-		ThisThread::sleep_for(500ms);
-	}
+	std::array<float, 6> data;
 
+	while (true) {
+		//	dataGatherer.onTick();
+		dataGatherer.getData(data);
+
+		// for (int i = 0; i < 5; i++) {
+		// 	printf("%f ", data.at(i));
+		// }
+		// printf("\n");
+
+		// rtos::ThisThread::sleep_for(50ms);
+
+		while (abs(data[1]) > 100) {
+			if (data[1] < 0) {		 // tilted on the right
+				if (data[1] < 0) {	 // tilted forward
+					motor_right.spin(Rotation::counterClockwise, 0.5);
+					motor_left.spin(Rotation::clockwise, 0.5);
+				} else {   // tilted backward
+					motor_right.spin(Rotation::clockwise, 0.5);
+					motor_left.spin(Rotation::counterClockwise, 0.5);
+				}
+			} else {				 // tilted on the left
+				if (data[1] > 0) {	 // tilted forward
+					motor_left.spin(Rotation::clockwise, 0.5);
+					motor_right.spin(Rotation::counterClockwise, 0.5);
+				} else {   // tilted backward
+					motor_left.spin(Rotation::counterClockwise, 0.5);
+					motor_right.spin(Rotation::clockwise, 0.5);
+				}
+			}
+
+			dataGatherer.getData(data);
+			rtos::ThisThread::sleep_for(50ms);
+		}
+
+		if (abs(data[0]) > 100) {
+			while (abs(data[0]) > 80) {
+				dataGatherer.getData(data);
+				// printf("speed : %f\n", abs(data[0]) / 1000.f + 0.2 * (1000 - abs(data[0])) / 1000);
+				rtos::ThisThread::sleep_for(50ms);
+				float speed = abs(data[0]) / 1000.f;
+				if (data[0] < 0) {
+					goForward(motor_left, motor_right, abs(data[0]) / 1000.f);
+				} else {
+					goBackward(motor_left, motor_right, abs(data[0]) / 1000.f);
+				}
+			}
+		}
+
+		stop(motor_left, motor_right);
+	}
 	return 0;
 }
 
