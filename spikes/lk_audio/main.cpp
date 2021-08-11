a2c204d13c2ede4ef6d85bcd422ab664c52c4b20
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-11 17:33:02 +0200

    :art: (spike): Code smells and clean up

--- spikes/lk_audio/main.cpp
+++ spikes/lk_audio/main.cpp
@@ -16,18 +16,8 @@
 
 using namespace leka;
 
-leka::FileManager sd_card;
-
-mbed::DigitalOut audio_enable(SOUND_ENABLE, 1);
-static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
-
-rtos::Thread audioThread;
-events::EventQueue audioEventQueue;
-
 LKCoreSTM32Hal hal;
-CoreDACTimer coreDACTimer(hal, CoreDACTimer::HardWareBasicTimer::BasicTimer6);
 CoreDAC coreDAC(hal);
-CoreAudio coreAudio(hal, coreDAC, coreDACTimer, audioThread, audioEventQueue);
 
 /**
  * @brief This function handles DMA1 stream5 global interrupt.
@@ -41,7 +31,19 @@ void DMA1_Stream5_IRQHandler()
 
 auto main() -> int
 {
-	printf("\n\nHello, investigation day!\n\n");
+	leka::FileManager sd_card;
+
+	mbed::DigitalOut audio_enable(SOUND_ENABLE, 1);
+	static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
+
+	rtos::Thread audioThread;
+	events::EventQueue audioEventQueue;
+
+	CoreDACTimer coreDACTimer(hal, CoreDACTimer::HardWareBasicTimer::BasicTimer6);
+	CoreAudio coreAudio(hal, coreDAC, coreDACTimer, audioThread, audioEventQueue);
+
+	leka::logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
+	log_info("\n\tHello, investigation day!");
 
 	FIL file;
 	std::string filename = "test-voix.wav";	  // sawtooth10_44ksamp_mono_5sec.wav
@@ -50,10 +52,10 @@ auto main() -> int
 		coreAudio.setVolume(30);
 		filename = "test-voix-100Hzmin.wav";
 		if (f_open(&file, filename.c_str(), FA_READ) == FR_OK) {
-			printf("\n\nFile %s did open!!\n", filename.c_str());
+			log_info("\n\tFile %s did open!!", filename.c_str());
 			coreAudio.playFile(&file);
 		} else {
-			printf("Could not open file\n");
+			log_info("\n\tCould not open file");
 		}
 
 		while (coreAudio.isPlaying()) {
@@ -66,10 +68,10 @@ auto main() -> int
 		filename = "fur-elise-100Hzmin.wav";   // sawtooth10_44ksamp_mono_5sec.wav
 
 		if (f_open(&file, filename.c_str(), FA_READ) == FR_OK) {
-			printf("\n\nFile %s did open!!\n", filename.c_str());
+			log_info("\n\tFile %s did open!!", filename.c_str());
 			coreAudio.playFile(&file);
 		} else {
-			printf("Could not open file\n");
+			log_info("\n\tCould not open file");
 		}
 
 		while (coreAudio.isPlaying()) {
