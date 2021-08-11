5298e1a00ad60fb423347ce680ad6681d4f23c14
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-11 13:19:20 +0200

    :recycle: (drivers): Use logger and replace defines with constexpr

--- drivers/CoreAudio/include/internal/WavFile.h
+++ drivers/CoreAudio/include/internal/WavFile.h
@@ -11,19 +11,21 @@
 #include "LKCoreFatFs.h"
 #include "cstdio"
 
-#define FILE_TYPE_ID 0x52494646	  // correspond to the letters 'RIFF'
-#define FILE_FORMAT	 0x57415645	  // correspond to the letters 'WAVE'
-#define FORMAT_ID	 0x666D7420	  // correspond to the letters 'fmt '
-#define DATA_ID		 0x64617461	  // correspond to the letters 'data'
-
-#define HEADER_SIZE 44
+// #define FILE_TYPE_ID 0x52494646	  // correspond to the letters 'RIFF'
+// #define FILE_FORMAT	 0x57415645	  // correspond to the letters 'WAVE'
+// #define FORMAT_ID	 0x666D7420	  // correspond to the letters 'fmt '
+// #define DATA_ID		 0x64617461	  // correspond to the letters 'data'
 
 namespace leka {
 
 class WavFile
 {
   private:
-	using Endianness = enum { LittleEndian, BigEndian };
+	using Endianness							 = enum { LittleEndian, BigEndian };
+	constexpr static uint32_t blockID_fileType	 = 0x52494646;	 // correspond to the letters 'RIFF'
+	constexpr static uint32_t blockID_fileFormat = 0x57415645;	 // correspond to the letters 'WAVE'
+	constexpr static uint32_t blockID_format	 = 0x666D7420;	 // correspond to the letters 'fmt '
+	constexpr static uint32_t blockID_data		 = 0x64617461;	 // correspond to the letters 'data'
 
   public:
 	using WavHeader = struct wavHeader {
@@ -51,7 +53,8 @@ class WavFile
 	WavHeader _header {};
 
 	void _readHeader();
-	auto _readHeaderDataUnit(uint8_t *buffer, uint8_t idx, uint8_t numOfBytes, Endianness bytesFormat) -> uint32_t;
+	auto _readHeaderDataUnit(const uint8_t *buffer, uint8_t idx, uint8_t numOfBytes, Endianness bytesFormat)
+		-> uint32_t;
 	auto _reverseEndianness(uint32_t val, uint8_t numBytes) -> uint32_t;
 };
 
--- drivers/CoreAudio/source/WavFile.cpp
+++ drivers/CoreAudio/source/WavFile.cpp
@@ -4,6 +4,8 @@
 
 #include "WavFile.h"
 
+#include "LogKit.h"
+
 namespace leka {
 
 WavFile::WavFile(FIL *file) : _file(file)
@@ -16,95 +18,84 @@ void WavFile::_readHeader()
 	uint8_t bytesRead	  = 0;
 	uint8_t indexInBuffer = 0;
 
-	uint8_t buffer[HEADER_SIZE] = {0};
+	std::array<uint8_t, sizeof(WavHeader)> buffer = {};
 
 	f_lseek(_file, 0);	 // move the file pointer to start of file
 
-	if (f_read(_file, buffer, HEADER_SIZE, (UINT *)&bytesRead) != FR_OK) {
-		// TODO(samhadjes) : handle error
+	if (f_read(_file, buffer.data(), sizeof(WavHeader), (UINT *)&bytesRead) != FR_OK) {
+		log_error("Error while reading wav file header");
 	}
 
-	// f_lseek(_file, 0);	// reset position
-	// TODO(samhadjes) : check if starting after header is problematic
-
-	_header.FileTypeBlockID = _readHeaderDataUnit(buffer, indexInBuffer, 4, BigEndian);
-	if (_header.FileTypeBlockID != FILE_TYPE_ID) {
-		// printf("File is not RIFF\n");
-
-		// return 1;
-		// TODO(samhadjes) : handle error
+	_header.FileTypeBlockID = _readHeaderDataUnit(buffer.data(), indexInBuffer, 4, BigEndian);
+	if (_header.FileTypeBlockID != blockID_fileType) {
+		log_error("File is not RIFF");
 	}
 	indexInBuffer += 4;
 
-	_header.FileSize = _readHeaderDataUnit(buffer, indexInBuffer, 4, LittleEndian);	  // Total size - 8 bytes
+	_header.FileSize =
+		_readHeaderDataUnit(buffer.data(), indexInBuffer, 4, LittleEndian);	  // Nb bytes in file -8 bytes
 	indexInBuffer += 4;
 
-	_header.FileFormatID = _readHeaderDataUnit(buffer, indexInBuffer, 4, BigEndian);
-	if (_header.FileFormatID != FILE_FORMAT) {
-		// printf("File is not WAVE\n");
-		// return 1;
-		// TODO(samhadjes) : handle error
+	_header.FileFormatID = _readHeaderDataUnit(buffer.data(), indexInBuffer, 4, BigEndian);
+	if (_header.FileFormatID != blockID_fileFormat) {
+		log_error("File is not WAVE");
 	}
 	indexInBuffer += 4;
 
-	_header.FormatBlockID = _readHeaderDataUnit(buffer, indexInBuffer, 4, BigEndian);
-	if (_header.FormatBlockID != FORMAT_ID) {
-		// printf("Format definition block not found\n");
-		// return 1;
-		// TODO(samhadjes) : handle error
+	_header.FormatBlockID = _readHeaderDataUnit(buffer.data(), indexInBuffer, 4, BigEndian);
+	if (_header.FormatBlockID != blockID_format) {
+		log_error("Format definition block not found");
 	}
 	indexInBuffer += 4;
 
-	_header.FormatBlockSize = _readHeaderDataUnit(buffer, indexInBuffer, 4, LittleEndian);
+	_header.FormatBlockSize =
+		_readHeaderDataUnit(buffer.data(), indexInBuffer, 4, LittleEndian);	  // Nb bytes in format block -16 bytes
 	indexInBuffer += 4;
 
-	_header.AudioFormat = _readHeaderDataUnit(buffer, indexInBuffer, 2, LittleEndian);
+	_header.AudioFormat = _readHeaderDataUnit(buffer.data(), indexInBuffer, 2, LittleEndian);
 	if (_header.AudioFormat != 1) {
-		// printf("Only integer PCM format is supported\n");
-		// return 1;
-		// TODO(samhadjes) : handle error
+		log_error("Only integer PCM format is supported");
 	}
 	indexInBuffer += 2;
 
-	_header.NumChannels = _readHeaderDataUnit(buffer, indexInBuffer, 2, LittleEndian);
+	_header.NumChannels = _readHeaderDataUnit(buffer.data(), indexInBuffer, 2, LittleEndian);
 	if (_header.NumChannels != 1) {
-		// printf("Only mono (1 channel) sounds are supported\n");
-		// return 1;
-		// TODO(samhadjes) : handle error
+		log_error("Only mono (1 channel) sounds are supported");
 	}
 	indexInBuffer += 2;
 
-	_header.SamplingRate = _readHeaderDataUnit(buffer, indexInBuffer, 4, LittleEndian);
+	_header.SamplingRate = _readHeaderDataUnit(buffer.data(), indexInBuffer, 4, LittleEndian);
 	indexInBuffer += 4;
 
-	_header.BytePerSec = _readHeaderDataUnit(buffer, indexInBuffer, 4, LittleEndian);
+	_header.BytePerSec = _readHeaderDataUnit(buffer.data(), indexInBuffer, 4, LittleEndian);
 	indexInBuffer += 4;
 
-	_header.BytePerSampleBlock = _readHeaderDataUnit(buffer, indexInBuffer, 2, LittleEndian);
+	_header.BytePerSampleBlock = _readHeaderDataUnit(buffer.data(), indexInBuffer, 2, LittleEndian);
 	indexInBuffer += 2;
 
-	_header.BitsPerSample = _readHeaderDataUnit(buffer, indexInBuffer, 2, LittleEndian);
+	_header.BitsPerSample = _readHeaderDataUnit(buffer.data(), indexInBuffer, 2, LittleEndian);
 	if (_header.BitsPerSample != 16) {
-		// printf("Only 16 bits sampling is supported\n");
-		// return 1;
+		log_error("Only 16 bits sampling is supported");
 	}
 	indexInBuffer += 2;
 
-	_header.DataBlockID = _readHeaderDataUnit(buffer, indexInBuffer, 4, BigEndian);
-	if (_header.DataBlockID != DATA_ID) {
-		// printf("Data block not found\n");
-		// return 1;
+	_header.DataBlockID = _readHeaderDataUnit(buffer.data(), indexInBuffer, 4, BigEndian);
+	if (_header.DataBlockID != blockID_data) {
+		log_error("Data block not found");
 	}
 	indexInBuffer += 4;
 
-	_header.DataSize = _readHeaderDataUnit(buffer, indexInBuffer, 4, LittleEndian);
+	_header.DataSize = _readHeaderDataUnit(buffer.data(), indexInBuffer, 4, LittleEndian);
 	indexInBuffer += 4;
 }
 
-// read bytes from buffer and reorganize them in the right order
 // max uint32_t
-auto WavFile::_readHeaderDataUnit(uint8_t *buffer, uint8_t idx, uint8_t numOfBytes, Endianness bytesFormat) -> uint32_t
+auto WavFile::_readHeaderDataUnit(const uint8_t *buffer, uint8_t idx, uint8_t numOfBytes, Endianness bytesFormat)
+	-> uint32_t
 {
+	if (numOfBytes > 4) {
+		log_error("Can't reverse more than 4 bytes");
+	}
 	uint32_t index = 0;
 	uint32_t temp  = 0;
 
