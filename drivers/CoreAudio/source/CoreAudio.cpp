b8a73584d3a8c18408b856dca388d847df644558
Samuel Hadjes <samuel.hadjes@gmail.com>
2021-08-11 17:23:27 +0200

    :recycle: (drivers): Code smells and cleanup for CoreAudio

--- drivers/CoreAudio/source/CoreAudio.cpp
+++ drivers/CoreAudio/source/CoreAudio.cpp
@@ -6,46 +6,39 @@
 
 namespace leka {
 
-uint16_t CoreAudio::_waveBuffer[512];
+std::array<uint16_t, CoreAudio::outBufferSize_Samples> CoreAudio::_waveBuffer;
 
 CoreAudio::CoreAudio(LKCoreSTM32HalBase &hal, CoreDAC &dac, CoreDACTimer &timer, rtos::Thread &thread,
 					 events::EventQueue &eventQueue)
-	: playing(false),
-	  _hal(hal),
-	  _coreDac(dac),
-	  _coreTimer(timer),
-	  _thread(thread),
-	  _eventQueue(eventQueue),
-	  _volume(100),
-	  _eofFlag(EndOfFileState::NotFinished)
+	: _hal(hal), _coreDac(dac), _coreTimer(timer), _thread(thread), _eventQueue(eventQueue)
 {
 	_thread.start({&_eventQueue, &events::EventQueue::dispatch_forever});
 }
 
 void CoreAudio::playFile(FIL *file)
 {
-	playing					= true;
+	_playing				= true;
 	_eofFlag				= NotFinished;
-	_wavFile				= new WavFile(file);
-	uint16_t *_waveBuffer_2 = &_waveBuffer[256];
+	_wavFile				= make_unique<WavFile>(file);
+	uint16_t *_waveBuffer_2 = _waveBuffer.data() + (outBufferSize_Samples / 2);
 
 	_initialize(_wavFile->getHeader().SamplingRate);
 
-	_handleNextSector(_waveBuffer);
+	_handleNextSector(_waveBuffer.data());
 	_handleNextSector(_waveBuffer_2);
 
-	auto outSpan = lstd::span {_waveBuffer, 512};	// TODO() : check this
+	auto outSpan = lstd::span {_waveBuffer.data(), outBufferSize_Samples};
 
 	_coreTimer.start();
-	_coreDac.start(outSpan);
+	_coreDac.start(outSpan);   // TODO() : check if span shouldn't be copied instead of given by ref
 }
 
-void CoreAudio::pause()
+void CoreAudio::pause() const
 {
 	_coreTimer.stop();
 }
 
-void CoreAudio::resume()
+void CoreAudio::resume() const
 {
 	_coreTimer.start();
 }
@@ -54,44 +47,46 @@ void CoreAudio::stop()
 {
 	_coreTimer.stop();
 	_coreDac.stop();
-	playing = false;
+	_playing = false;
 }
 
-void CoreAudio::_initialize(float frequency)
+void CoreAudio::_initialize(uint32_t frequency)
 {
 	static auto *self = this;
 	auto halfBuffCb =
-		static_cast<pDAC_CallbackTypeDef>([]([[maybe_unused]] DAC_HandleTypeDef *hdac) { self->_halfCptCallback(); });
+		static_cast<pDAC_CallbackTypeDef>([]([[maybe_unused]] DAC_HandleTypeDef *hdac) { self->_onHalfBuffRead(); });
 	auto fullBuffCb =
-		static_cast<pDAC_CallbackTypeDef>([]([[maybe_unused]] DAC_HandleTypeDef *hdac) { self->_cptCallback(); });
+		static_cast<pDAC_CallbackTypeDef>([]([[maybe_unused]] DAC_HandleTypeDef *hdac) { self->_onFullBuffRead(); });
 
 	_coreTimer.initialize(frequency);
 	_coreDac.initialize(_coreTimer, halfBuffCb, fullBuffCb);
 }
 
-void CoreAudio::_align12bR(uint16_t *buffer, uint16_t length)
+void CoreAudio::_align12bR(uint16_t *buffer, uint16_t nbSamples) const
 {
-	for (int i = 0; i < length; ++i) {
+	for (int i = 0; i < nbSamples; ++i) {
 		buffer[i] = buffer[i] >> 4;
 	}
 }
 
-void CoreAudio::_scaleToVolume(uint16_t *buffer, uint16_t length)
+void CoreAudio::_scaleToVolume(uint16_t *buffer, uint16_t nbSamples) const
 {
-	for (int i = 0; i < length; ++i) {
-		buffer[i] = static_cast<double>(buffer[i]) * (_volume / 100.F);
-		buffer[i] += 0x7FFF * (1.F - _volume / 100.F);
+	static const uint16_t offset = 0x7FFF;
+	for (int i = 0; i < nbSamples; ++i) {
+		buffer[i] = static_cast<uint16_t>((static_cast<float>(buffer[i]) * _volume) / 100);
+		buffer[i] += static_cast<uint16_t>(offset * (1.F - _volume / 100.F));
 	}
 }
 
-void CoreAudio::_halfCptCallback()
+void CoreAudio::_onHalfBuffRead()
 {
-	_eventQueue.call(this, &CoreAudio::_handleCallback, _waveBuffer);
+	_eventQueue.call(this, &CoreAudio::_handleCallback, _waveBuffer.data());
 }
 
-void CoreAudio::_cptCallback()
+void CoreAudio::_onFullBuffRead()
 {
-	_eventQueue.call(this, &CoreAudio::_handleCallback, &(_waveBuffer[256]));
+	uint16_t *_waveBuffer_2 = _waveBuffer.data() + (outBufferSize_Samples / 2);
+	_eventQueue.call(this, &CoreAudio::_handleCallback, _waveBuffer_2);
 }
 
 void CoreAudio::_handleCallback(uint16_t *buffer)
@@ -108,9 +103,9 @@ void CoreAudio::_handleCallback(uint16_t *buffer)
 
 void CoreAudio::_handleNextSector(uint16_t *buffer)
 {
-	bool eof = WavReader::loadSector(*_wavFile, buffer, 512);
-	_scaleToVolume(buffer, 256);
-	_align12bR(buffer, 256);
+	bool eof = WavReader::loadSector(*_wavFile, buffer, sectorSize_Bytes);
+	_scaleToVolume(buffer, sectorSize_Samples);
+	_align12bR(buffer, sectorSize_Samples);
 	if (eof) {
 		_eofFlag = LastBuffer;
 	}
