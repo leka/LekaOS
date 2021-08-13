e93814d3f3d096124eee1ea043d839b412ca90a4
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-08-13 15:57:44 +0200

    :construction: (Drivers): Update CR95HF mock

--- tests/unit/mocks/mocks/leka/CoreRFID.h
+++ tests/unit/mocks/mocks/leka/CoreRFID.h
@@ -15,9 +15,16 @@ namespace leka {
 class CoreRFIDMock : public interface::RFID
 {
   public:
-	MOCK_METHOD(bool, init, (), (override));
-	MOCK_METHOD(void, send, (const lstd::span<uint8_t> &), (override));
-	MOCK_METHOD(size_t, receive, (const lstd::span<uint8_t> &), (override));
+	MOCK_METHOD(void, init, (), (override));
+	MOCK_METHOD(void, registerTagAvailableCallback, (tagAvailableCallback), (override));
+	MOCK_METHOD(void, onTagAvailable, (), (override));
+	MOCK_METHOD((std::array<uint8_t, 17>), getIDN, (), (override));
+	MOCK_METHOD(bool, setBaudrate, (uint8_t), (override));
+	MOCK_METHOD(bool, setCommunicationProtocol, (rfid::Protocol), (override));
+	MOCK_METHOD(void, sendCommandToTag, (lstd::span<uint8_t>), (override));
+	MOCK_METHOD(size_t, receiveDataFromTag, (lstd::span<uint8_t> *), (override));
+	MOCK_METHOD(void, setModeTagDetection, (), (override));
+	MOCK_METHOD(bool, checkForTagDetection, (), (override));
 };
 
 }	// namespace leka
