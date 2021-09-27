54ede5be6f86f5bee593f024aa41696c811f9e06
Yann Locatelli <yann.locatelli2@gmail.com>
2021-09-27 12:28:06 +0200

    :truck: (file): Add FileReception lib

---
+++ libs/FileReception/tests/FileReception_test.cpp
@@ -0,0 +1,18 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "FileReception.h"
+
+#include "gtest/gtest.h"
+
+// using namespace leka;
+
+class FileReceptionTest : public ::testing::Test
+{
+  protected:
+	FileReceptionTest() {}
+
+	// void SetUp() override {}
+	// void TearDown() override {}
+};
---
+++ spikes/lk_file_reception/main.cpp
@@ -0,0 +1,36 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "drivers/BufferedSerial.h"
+#include "rtos/ThisThread.h"
+
+#include "FileReception.h"
+#include "HelloWorld.h"
+#include "LogKit.h"
+
+using namespace leka;
+using namespace std::chrono;
+
+auto main() -> int
+{
+	static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
+	leka::logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
+
+	log_info("Hello, World!\n\n");
+
+	auto start = rtos::Kernel::Clock::now();
+
+	auto hello = HelloWorld();
+
+	rtos::ThisThread::sleep_for(1s);
+
+	hello.start();
+
+	while (true) {
+		auto t = rtos::Kernel::Clock::now() - start;
+		log_info("A message from your board %s --> \"%s\" at %i s", MBED_CONF_APP_TARGET_NAME, hello.world,
+				 int(t.count() / 1000));
+		rtos::ThisThread::sleep_for(10s);
+	}
+}
