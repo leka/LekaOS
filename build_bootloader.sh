7e53119012102646f7e43e47b2ec552a78e688f6
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-04 14:22:41 +0200

    :hammer: (firmware): Script to generate firmware
    Build bootloader, Build and sign application, Merge them to get firmware
    
    :truck: (boot): Generated files move to _tmp folder and/or _release
    folder
    :children_crossing: (boot): Use .bin for updates
    :hammer: (firmware): Split in 3 scripts: bootloader, application and firmware
    :hammer: (boot): Add recompile all flag and mute mkdir warning

---
+++ build_bootloader.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir _tmp > /dev/null 2> /dev/null
+
+# Variables
+BOOTLOADER_HEX_SOURCE="$1"
+BOOTLOADER_HEX="$2"
+
+if [ -z "$BOOTLOADER_HEX_SOURCE" ]; then
+	echo "BOOTLOADER_HEX_SOURCE is unset"
+	BOOTLOADER_HEX_SOURCE="_build/LEKA_V1_2_DEV/bootloader/bootloader.hex"
+fi
+
+if [ -z "$BOOTLOADER_HEX" ]; then
+	echo "BOOTLOADER_HEX is unset"
+	BOOTLOADER_HEX="_tmp/bootloader.hex"
+fi
+
+# Compile bootloader
+make deep_clean
+make config
+make
+
+# Get bootloader binary
+cp $BOOTLOADER_HEX_SOURCE $BOOTLOADER_HEX
---
+++ generate_firmware.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir _tmp > /dev/null 2> /dev/null
+mkdir _release > /dev/null 2> /dev/null
+
+# Variables
+RECOMPILE_BOOTLOADER="false"
+while getopts r flag
+do
+    case "${flag}" in
+        r) RECOMPILE_BOOTLOADER="true";;
+    esac
+done
+
+BOOTLOADER_HEX_SOURCE="_build/LEKA_V1_2_DEV/bootloader/bootloader.hex"
+BOOTLOADER_HEX="_tmp/bootloader.hex"
+
+APPLICATION_HEX_SOURCE="_build/LEKA_V1_2_DEV/spikes/lk_update_process_app_base/spike_lk_update_process_app_base.hex"
+APPLICATION_VERSION="1.2.3+4"
+APPLICATION_SIGNED_HEX="_tmp/application-signed.hex"
+
+FIRMWARE_HEX="_tmp/firmwware.hex"
+FIRMWARE_RELEASE="_release/firmware.bin"
+
+# Get bootloader
+if [ "$RECOMPILE_BOOTLOADER" = "true" ];
+then
+	./build_bootloader.sh $BOOTLOADER_HEX_SOURCE $BOOTLOADER_HEX
+fi;
+
+# Get application
+./build_update.sh $RECOMPILE_BOOTLOADER $APPLICATION_HEX_SOURCE $APPLICATION_VERSION $APPLICATION_SIGNED_HEX
+
+# Merge bootloader and application
+hexmerge.py -o $FIRMWARE_HEX --no-start-addr $BOOTLOADER_HEX $APPLICATION_SIGNED_HEX
+arm-none-eabi-objcopy -I ihex -O binary $FIRMWARE_HEX $FIRMWARE_RELEASE
