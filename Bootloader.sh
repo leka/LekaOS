e76b7df54e9beba2853adc76ba5c649b19cb181f
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-05 15:51:26 +0200

    :truck: (boot): Generated files move to _tmp folder

--- Bootloader.sh
+++ Bootloader.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+mkdir _tmp
+mkdir release
+
 # Compile bootloader
 make deep_clean && make config ENABLE_BOOTLOADER=OFF && make
 
 # Get bootloader binary
-cp _build/LEKA_V1_2_DEV/bootloader/bootloader.hex bootloader/bootloader.hex
+cp _build/LEKA_V1_2_DEV/bootloader/bootloader.hex _tmp/bootloader.hex
 
 # Compile applications
 make deep_clean && make config ENABLE_BOOTLOADER=ON && make
 
 # Get application binary
-cp _build/LEKA_V1_2_DEV/src/LekaOS.hex src/LekaOS.hex
+cp _build/LEKA_V1_2_DEV/src/LekaOS.hex _tmp/application.hex
 
 # Sign application with private key
-imgtool sign -k signing-keys.pem --align 4 -v 1.2.3+4 --header-size 4096 --pad-header -S 0x180000 src/LekaOS.hex src/LekaOS-signed.hex
+imgtool sign -k signing-keys.pem --align 4 -v 1.2.3+4 --header-size 4096 --pad-header -S 0x180000 _tmp/application.hex _tmp/application-signed.hex
 
 # Merge bootloader and application
-hexmerge.py -o firmware.hex --no-start-addr bootloader/bootloader.hex src/LekaOS-signed.hex
-arm-none-eabi-objcopy -I ihex -O binary firmware.hex firmware.bin
+hexmerge.py -o _tmp/firmware.hex --no-start-addr _tmp/bootloader.hex _tmp/application-signed.hex
+arm-none-eabi-objcopy -I ihex -O binary _tmp/firmware.hex release/firmware.bin
 
 # Get update binary
-cp _build/LEKA_V1_2_DEV/spikes/lk_ble/spike_lk_ble.hex update.hex
+cp _build/LEKA_V1_2_DEV/spikes/lk_ble/spike_lk_ble.hex _tmp/update.hex
 
 # Sign update with private key
-imgtool sign -k signing-keys.pem --align 4 -v 1.2.3+5 --header-size 4096 --pad-header -S 0x180000 update.hex update-signed.hex
-arm-none-eabi-objcopy -I ihex -O binary update-signed.hex update.bin
+imgtool sign -k signing-keys.pem --align 4 -v 1.2.3+5 --header-size 4096 --pad-header -S 0x180000 _tmp/update.hex _tmp/update-signed.hex
+arm-none-eabi-objcopy -I ihex -O binary _tmp/update-signed.hex release/update.bin
+
+rm -rf _tmp
