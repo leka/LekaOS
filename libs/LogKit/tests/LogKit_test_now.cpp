171ecb743ec087f46019be202f307b2e34d6fe76
Ladislas de Toldi <ladislas@detoldi.me>
2021-10-09 16:02:45 +0200

    :recycle: (tests): Use MockFunction with std::function

--- libs/LogKit/tests/LogKit_test_now.cpp
+++ libs/LogKit/tests/LogKit_test_now.cpp
@@ -11,28 +11,13 @@
 
 using namespace leka;
 
-using ::testing::HasSubstr;
-using ::testing::MatchesRegex;
+using ::testing::MockFunction;
 
 class LogKitNowTest : public ::testing::Test
 {
   protected:
-	void SetUp() override
-	{
-		spy_string = "";
-		spy_now	   = 0;
-	}
-
+	// void SetUp() override {}
 	void TearDown() override { logger::set_now_function(logger::default_now); }
-
-	static auto dynamic_now() -> int64_t
-	{
-		spy_now = std::chrono::system_clock::now().time_since_epoch().count();
-		return spy_now;
-	}
-
-	static inline auto spy_string = std::string {};
-	static inline auto spy_now	  = int64_t {};
 };
 
 TEST_F(LogKitNowTest, useDefaultNowFunction)
@@ -40,20 +25,13 @@ TEST_F(LogKitNowTest, useDefaultNowFunction)
 	ASSERT_EQ(spy_kernel_tick_count, logger::now());
 }
 
-TEST_F(LogKitNowTest, setConstNowFunction)
+TEST_F(LogKitNowTest, useCustomNowFunction)
 {
-	auto const_now = []() { return int64_t {1619354279231635}; };
+	MockFunction<int64_t()> mock_now;
 
-	logger::set_now_function(const_now);
+	EXPECT_CALL(mock_now, Call()).Times(1);
 
-	ASSERT_EQ(1619354279231635, leka::logger::now());
-}
+	logger::set_now_function(mock_now.AsStdFunction());
 
-TEST_F(LogKitNowTest, setDynamicNowFunction)
-{
-	logger::set_now_function(dynamic_now);
-
-	auto expected = logger::now();
-
-	ASSERT_EQ(expected, spy_now);
+	logger::now();
 }
