d3d96547dfbef1ee2c9b6de0f0a5849d8a624536
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-11 15:33:27 +0200

    :see_no_evil: (boot): Ignore _tmp and _release folders

--- bootloader.sh
+++ bootloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir _tmp
-mkdir release
+mkdir _release
 
 # Compile bootloader
 make deep_clean
@@ -24,12 +24,12 @@ imgtool sign -k signing-keys.pem --align 4 -v 1.2.3+4 --header-size 4096 --pad-h
 
 # Merge bootloader and application
 hexmerge.py -o _tmp/firmware.hex --no-start-addr _tmp/bootloader.hex _tmp/application-signed.hex
-arm-none-eabi-objcopy -I ihex -O binary _tmp/firmware.hex release/firmware.bin
+arm-none-eabi-objcopy -I ihex -O binary _tmp/firmware.hex _release/firmware.bin
 
 # Get update binary
 cp _build/LEKA_V1_2_DEV/spikes/lk_ble/spike_lk_ble.bin _tmp/update.bin
 
 # Sign update with private key
-imgtool sign -k signing-keys.pem --align 4 -v 1.2.3+5 --header-size 4096 --pad-header -S 0x180000 _tmp/update.bin release/update-signed.bin
+imgtool sign -k signing-keys.pem --align 4 -v 1.2.3+5 --header-size 4096 --pad-header -S 0x180000 _tmp/update.bin _release/update-signed.bin
 
 rm -rf _tmp
