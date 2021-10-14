32cee71039875b6503005ec44a63ca64951fa827
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-14 18:05:29 +0200

    :sparkles: (firmware): Add FirmwareKit
    :sparkles: (firmware): loadUpdate into external flash via QSPI
    :zap: (firmware): Check file is open prior loading it
    :white_check_mark: (firmware): Add unit test for FirmwareKit
    
    Co-authored-by: ladislas <ladislas@detoldi.me>

---
+++ libs/FirmwareKit/source/FirmwareKit.cpp
@@ -0,0 +1,24 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "FirmwareKit.h"
+
+using namespace leka;
+
+void FirmwareKit::loadUpdate(interface::File &file)
+{
+	if (file.is_open()) {
+		auto address			   = uint32_t {0x0};
+		constexpr auto packet_size = std::size_t {256};
+
+		std::array<uint8_t, packet_size> buffer {};
+
+		while (auto packet_read = file.read(buffer.data(), std::size(buffer))) {
+			_update_container.write(address, buffer, packet_read);
+			address += packet_read;
+		}
+
+		file.close();
+	}
+}
---
+++ libs/FirmwareKit/tests/FirmwareKit_test.cpp
@@ -0,0 +1,95 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "FirmwareKit.h"
+
+#include "gtest/gtest.h"
+#include "mocks/leka/File.h"
+#include "mocks/leka/FlashMemory.h"
+
+using namespace leka;
+
+using ::testing::_;
+using ::testing::InSequence;
+using ::testing::Return;
+
+class FirmwareKitTest : public ::testing::Test
+{
+  protected:
+	FirmwareKitTest() : firmwarekit(flash_memory) {}
+
+	// void SetUp() override {}
+	// void TearDown() override {}
+
+	mock::FlashMemory flash_memory;
+	mock::File file;
+	FirmwareKit firmwarekit;
+
+	void MOCK_FUNCTION_end_of_file_reached() { EXPECT_CALL(file, read(_, _)).WillOnce(Return(0)); }
+};
+
+TEST_F(FirmwareKitTest, instantiation)
+{
+	ASSERT_NE(&firmwarekit, nullptr);
+}
+
+TEST_F(FirmwareKitTest, loadUpdateCheckPackets)
+{
+	auto expected_packet_size	 = 256;
+	auto actual_some_packet_read = 200;
+
+	{
+		InSequence seq;
+
+		EXPECT_CALL(file, is_open).WillOnce(Return(true));
+
+		EXPECT_CALL(file, read(_, expected_packet_size)).WillOnce(Return(actual_some_packet_read));
+		EXPECT_CALL(flash_memory, write(_, _, actual_some_packet_read)).Times(1);
+
+		MOCK_FUNCTION_end_of_file_reached();
+
+		EXPECT_CALL(file, close).Times(1);
+	}
+
+	firmwarekit.loadUpdate(file);
+}
+
+TEST_F(FirmwareKitTest, loadUpdateCheckAddress)
+{
+	auto expected_adress		 = 0x0;
+	auto actual_some_packet_read = 200;
+
+	{
+		InSequence seq;
+
+		EXPECT_CALL(file, is_open).WillOnce(Return(true));
+
+		EXPECT_CALL(file, read(_, _)).WillOnce(Return(actual_some_packet_read));
+		EXPECT_CALL(flash_memory, write(expected_adress, _, _)).Times(1);
+		expected_adress += actual_some_packet_read;
+
+		EXPECT_CALL(file, read(_, _)).WillOnce(Return(actual_some_packet_read));
+		EXPECT_CALL(flash_memory, write(expected_adress, _, _)).Times(1);
+
+		MOCK_FUNCTION_end_of_file_reached();
+
+		EXPECT_CALL(file, close).Times(1);
+	}
+
+	firmwarekit.loadUpdate(file);
+}
+
+TEST_F(FirmwareKitTest, loadUpdateFileNotOpened)
+{
+	{
+		InSequence seq;
+
+		EXPECT_CALL(file, is_open).WillOnce(Return(false));
+
+		EXPECT_CALL(file, read(_, _)).Times(0);
+		EXPECT_CALL(flash_memory, write).Times(0);
+	}
+
+	firmwarekit.loadUpdate(file);
+}
