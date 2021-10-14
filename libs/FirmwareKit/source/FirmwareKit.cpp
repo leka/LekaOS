cc38d3fc194121b0c3f7e872374a925b4de84d95
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-14 19:04:30 +0200

    :zap: (firmware): Check file is open prior loading it

--- libs/FirmwareKit/source/FirmwareKit.cpp
+++ libs/FirmwareKit/source/FirmwareKit.cpp
@@ -9,17 +9,19 @@ using namespace leka;
 
 void FirmwareKit::loadUpdate(interface::File &update_file)
 {
-	uint32_t address		 = 0x0;
-	const size_t packet_size = 0x100;
-	auto packet_read		 = packet_size;
-	std::vector<uint8_t> buffer {};
-	buffer.resize(packet_size);
+	if (update_file.is_open()) {
+		uint32_t address		 = 0x0;
+		const size_t packet_size = 0x100;
+		auto packet_read		 = packet_size;
+		std::vector<uint8_t> buffer {};
+		buffer.resize(packet_size);
 
-	while (packet_read != 0) {
-		packet_read = update_file.read(buffer.data(), packet_size);
-		_update_container.write(address, buffer, packet_read);
-		address += packet_read;
+		while (packet_read != 0) {
+			packet_read = update_file.read(buffer.data(), packet_size);
+			_update_container.write(address, buffer, packet_read);
+			address += packet_read;
+		}
+
+		update_file.close();
 	}
-
-	update_file.close();
 }
