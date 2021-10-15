875768d7a708cc6aa93f107f63b0101ec1bc10fb
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-15 17:57:50 +0200

    :recycle: (firmware): Update spike to load file for update

--- spikes/lk_update_process_app_base/main.cpp
+++ spikes/lk_update_process_app_base/main.cpp
@@ -9,6 +9,7 @@
 #include "CoreQSPI.h"
 #include "FATFileSystem.h"
 #include "FileSystemKit.h"
+#include "FirmwareKit.h"
 #include "HelloWorld.h"
 #include "LogKit.h"
 #include "QSPIFBlockDevice.h"
@@ -29,6 +30,7 @@ FATFileSystem fatfs("fs");
 auto coreqspi		   = CoreQSPI();
 auto coremanageris25lp = CoreFlashManagerIS25LP016D(coreqspi);
 auto coreis25lp		   = CoreFlashIS25LP016D(coreqspi, coremanageris25lp);
+auto firmwwarekit	   = FirmwareKit(coreis25lp);
 
 auto get_secondary_bd() -> mbed::BlockDevice *
 {
@@ -62,25 +64,10 @@ auto main() -> int
 	coreqspi.setFrequency(flash::is25lp016d::max_clock_frequency_in_hz);
 	coreis25lp.erase();
 
-	// Open file and initialize tools
-	uint32_t address		 = 0x0;
-	const size_t packet_size = 0x100;
-	auto packet_read		 = packet_size;
-	std::array<uint8_t, packet_size> buffer {};
-
-	fflush(stdout);
+	// Load file
 	auto update_file = FileSystemKit::File("/fs/update.bin");
 
-	// Transfer update file into external flash memory
-	while (packet_read != 0) {
-		packet_read = update_file.read(buffer.data(), packet_size);
-		coreis25lp.write(address, buffer, packet_read);
-		address += packet_read;
-	}
-
-	// Close the file which also flushes any cached writes
-	fflush(stdout);
-	update_file.close();
+	firmwwarekit.loadUpdate(update_file);
 
 	// Set ready for reboot
 	if (int ret = boot_set_pending(0); ret == 0) {
