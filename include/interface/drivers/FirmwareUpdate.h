3dd5956072d08be10247cb32a0eb517bbd4bf5b9
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-18 13:41:35 +0200

    :construction: (firmware): Simplify use of FirmwareKit
    * Use loadUpdateLatest instead of giving file and path
    * interface::FirmwareUpdate for API
    * File used is from FileSystemKit
    * Set default path to latests update
    * Rebase to develop

---
+++ include/interface/drivers/FirmwareUpdate.h
@@ -0,0 +1,20 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_INTERFACE_DRIVERS_FIRMWARE_UPDATE_H_
+#define _LEKA_OS_INTERFACE_DRIVERS_FIRMWARE_UPDATE_H_
+
+namespace leka::interface {
+
+class FirmwareUpdate
+{
+  public:
+	virtual ~FirmwareUpdate() = default;
+
+	virtual void loadUpdateLatest() = 0;
+};
+
+}	// namespace leka::interface
+
+#endif	 // _LEKA_OS_INTERFACE_DRIVERS_FIRMWARE_UPDATE_H_
