9b2a674802fadd2dcf28d3fe2c3d4bb8b0f00b20
Yann Locatelli <yann.locatelli2@gmail.com>
2021-10-25 16:20:44 +0200

    :sparkles: (firmware): Add FirmwareUpdate and FirmwareVersion interfaces

---
+++ include/interface/drivers/FirmwareVersion.h
@@ -0,0 +1,20 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_DRIVERS_FIRMWARE_VERSION_H_
+#define _LEKA_OS_DRIVERS_FIRMWARE_VERSION_H_
+
+#include <cinttypes>
+
+namespace leka {
+
+struct FirmwareVersion {
+	uint8_t major;
+	uint8_t minor;
+	uint16_t revision;
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_DRIVERS_FIRMWARE_VERSION_H_
