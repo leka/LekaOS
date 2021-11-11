9097129df8f633c3d031652249c0362fb099e9a1
Yann Locatelli <yann.locatelli2@gmail.com>
2021-11-11 23:12:17 +0100

    :white_check_mark: (battery): Set voltage in each Capacity and near expected value

--- drivers/CoreBattery/tests/CoreBattery_test.cpp
+++ drivers/CoreBattery/tests/CoreBattery_test.cpp
@@ -5,43 +5,105 @@
 #include "CoreBattery.h"
 
 #include "gtest/gtest.h"
+#include "mocks/mbed/DigitalIn.h"
 #include "stubs/mbed/AnalogIn.h"
 
 using namespace leka;
 
-CoreBattery corebattery(PinName::BATTERY_VOLTAGE);
+using ::testing::Return;
 
-TEST(CoreBatteryTest, initialization)
+class CoreBatteryTest : public ::testing::Test
 {
-	ASSERT_NE(&corebattery, nullptr);
+  protected:
+	CoreBatteryTest() : battery(PinName::BATTERY_VOLTAGE, charge_input) {}
+
+	// void SetUp() override {}
+	// void TearDown() override {}
+
+	mbed::mock::DigitalIn charge_input = {};
+
+	CoreBattery battery;
+};
+
+TEST_F(CoreBatteryTest, initialization)
+{
+	ASSERT_NE(&battery, nullptr);
 }
 
-TEST(CoreBatteryTest, getVoltage)
+TEST_F(CoreBatteryTest, getVoltageAboveFull)
 {
-	auto AnalogIn_read_voltage_value = float {1.2};
-	spy_AnalogIn_setVoltageValue(AnalogIn_read_voltage_value);
+	auto AnalogIn_voltage_value	  = float {2.208};
+	auto expected_battery_voltage = float {12.6};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
 
-	auto expected = AnalogIn_read_voltage_value / CoreBattery::voltage::divider;
-
-	ASSERT_FLOAT_EQ(expected, corebattery.getVoltage());
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::full);
 }
 
-TEST(CoreBatteryTest, getVoltageLowest)
+TEST_F(CoreBatteryTest, getVoltageBetweenThreeQuarterAndFull)
 {
-	auto AnalogIn_read_voltage_value = float {0};
-	spy_AnalogIn_setVoltageValue(AnalogIn_read_voltage_value);
+	auto AnalogIn_voltage_value	  = float {2.183};
+	auto expected_battery_voltage = float {12.1};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
 
-	auto expected = AnalogIn_read_voltage_value / CoreBattery::voltage::divider;
-
-	ASSERT_FLOAT_EQ(expected, corebattery.getVoltage());
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::full);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::three_quarter);
 }
 
-TEST(CoreBatteryTest, getVoltageHighest)
+TEST_F(CoreBatteryTest, getVoltageBetweenHalfAndThreeQuarter)
 {
-	auto AnalogIn_read_voltage_value = float {1.65};
-	spy_AnalogIn_setVoltageValue(AnalogIn_read_voltage_value);
+	auto AnalogIn_voltage_value	  = float {2.144};
+	auto expected_battery_voltage = float {11.4};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
 
-	auto expected = AnalogIn_read_voltage_value / CoreBattery::voltage::divider;
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::three_quarter);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::half);
+}
 
-	ASSERT_FLOAT_EQ(expected, corebattery.getVoltage());
+TEST_F(CoreBatteryTest, getVoltageBetweenQuarterAndHalf)
+{
+	auto AnalogIn_voltage_value	  = float {2.108};
+	auto expected_battery_voltage = float {10.8};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
+
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::half);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::quarter);
+}
+
+TEST_F(CoreBatteryTest, getVoltageBetweenEmptyAndQuarter)
+{
+	auto AnalogIn_voltage_value	  = float {2.033};
+	auto expected_battery_voltage = float {9.7};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
+
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::quarter);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::empty);
+}
+
+TEST_F(CoreBatteryTest, getVoltageBelowEmpty)
+{
+	auto AnalogIn_voltage_value	  = float {1.82};
+	auto expected_battery_voltage = float {7.5};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
+
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::empty);
+}
+
+TEST_F(CoreBatteryTest, isCharging)
+{
+	EXPECT_CALL(charge_input, read).WillOnce(Return(1));
+
+	ASSERT_TRUE(battery.isCharging());
+}
+
+TEST_F(CoreBatteryTest, isNotCharging)
+{
+	EXPECT_CALL(charge_input, read).WillOnce(Return(0));
+
+	ASSERT_FALSE(battery.isCharging());
 }
