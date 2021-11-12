d006be090223881757a58d346a982f0ebbc44f54
Yann Locatelli <yann.locatelli2@gmail.com>
2021-11-12 23:58:28 +0100

    :sparkles: (ble): Add BLEGap

---
+++ libs/BLEKit/include/BLEGap.h
@@ -0,0 +1,22 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_BLE_GAP_H_
+#define _LEKA_OS_LIB_BLE_GAP_H_
+
+namespace leka {
+
+class BLEGap
+{
+  public:
+	explicit BLEGap() = default;
+
+	~BLEGap() = default;
+
+  private:
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_LIB_BLE_GAP_H_
