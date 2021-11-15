8b67a8c7445c9a02416d91ef1683bd12e6272a55
Yann Locatelli <yann.locatelli2@gmail.com>
2021-11-15 11:31:01 +0100

    :sparkles: (ble): Start BLE process

--- libs/BLEKit/include/BLEGap.h
+++ libs/BLEKit/include/BLEGap.h
@@ -15,7 +15,17 @@ class BLEGap : public ble::Gap::EventHandler
   public:
 	explicit BLEGap(BLE &ble) : _ble(ble), _ble_gap(ble.gap()) {};
 
+	void start();
+	void stop();
+
   private:
+	void onInitComplete(BLE::InitializationCompleteCallbackContext *params);
+	void onConnectionComplete(const ble::ConnectionCompleteEvent &event) override;
+	void onDisconnectionComplete(const ble::DisconnectionCompleteEvent &event) override;
+	void onAdvertisingEnd(const ble::AdvertisingEndEvent &event) override;
+
+	void startAdvertising();
+
 	BLE &_ble;
 	ble::Gap &_ble_gap;
 };
