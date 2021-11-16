6ddf622ad9d78f03037aa477b2a53226ca4ad369
Yann Locatelli <yann.locatelli2@gmail.com>
2021-11-17 00:21:25 +0100

    :white_check_mark: (battery): Set voltage in each Capacity and near expected value

--- drivers/CoreBattery/tests/CoreBattery_test.cpp
+++ drivers/CoreBattery/tests/CoreBattery_test.cpp
@@ -12,48 +12,98 @@ using namespace leka;
 
 using ::testing::Return;
 
-CoreBattery corebattery(PinName::BATTERY_VOLTAGE);
-
-class CoreChargeTest : public ::testing::Test
+class CoreBatteryTest : public ::testing::Test
 {
   protected:
-	CoreChargeTest() : charge(charge_input) {}
+	CoreBatteryTest() : battery(PinName::BATTERY_VOLTAGE, charge_input) {}
 
 	// void SetUp() override {}
 	// void TearDown() override {}
 
 	mbed::mock::DigitalIn charge_input = {};
 
-	CoreCharge charge;
+	CoreBattery battery;
 };
 
-TEST(CoreBatteryTest, initialization)
+TEST_F(CoreBatteryTest, initialization)
 {
-	ASSERT_NE(&corebattery, nullptr);
+	ASSERT_NE(&battery, nullptr);
 }
 
-TEST(CoreBatteryTest, getVoltage)
+TEST_F(CoreBatteryTest, getVoltageAboveFull)
 {
-	auto AnalogIn_voltage_value = float {1.2};
+	auto AnalogIn_voltage_value	  = float {2.208};
+	auto expected_battery_voltage = float {12.6};
 	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
 
-	auto expected_voltage = CoreBattery::polynomial::degree_0 +
-							CoreBattery::polynomial::degree_1 * AnalogIn_voltage_value +
-							CoreBattery::polynomial::degree_2 * AnalogIn_voltage_value * AnalogIn_voltage_value;
-
-	ASSERT_FLOAT_EQ(expected_voltage, corebattery.getVoltage());
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::full);
 }
 
-// TEST_F(CoreChargeTest, isOn)
-// {
-// 	EXPECT_CALL(charge_input, read).WillOnce(Return(1));
+TEST_F(CoreBatteryTest, getVoltageBetweenThreeQuarterAndFull)
+{
+	auto AnalogIn_voltage_value	  = float {2.183};
+	auto expected_battery_voltage = float {12.1};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
 
-// 	ASSERT_TRUE(charge.isOn());
-// }
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::full);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::three_quarter);
+}
 
-// TEST_F(CoreChargeTest, isOff)
-// {
-// 	EXPECT_CALL(charge_input, read).WillOnce(Return(0));
+TEST_F(CoreBatteryTest, getVoltageBetweenHalfAndThreeQuarter)
+{
+	auto AnalogIn_voltage_value	  = float {2.144};
+	auto expected_battery_voltage = float {11.4};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
 
-// 	ASSERT_FALSE(charge.isOn());
-// }
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::three_quarter);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::half);
+}
+
+TEST_F(CoreBatteryTest, getVoltageBetweenQuarterAndHalf)
+{
+	auto AnalogIn_voltage_value	  = float {2.108};
+	auto expected_battery_voltage = float {10.8};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
+
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::half);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::quarter);
+}
+
+TEST_F(CoreBatteryTest, getVoltageBetweenEmptyAndQuarter)
+{
+	auto AnalogIn_voltage_value	  = float {2.033};
+	auto expected_battery_voltage = float {9.7};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
+
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::quarter);
+	ASSERT_GT(battery.getVoltage(), CoreBattery::Capacity::empty);
+}
+
+TEST_F(CoreBatteryTest, getVoltageBelowEmpty)
+{
+	auto AnalogIn_voltage_value	  = float {1.82};
+	auto expected_battery_voltage = float {7.5};
+	spy_AnalogIn_setVoltageValue(AnalogIn_voltage_value);
+
+	ASSERT_NEAR(battery.getVoltage(), expected_battery_voltage, 0.1);
+	ASSERT_LT(battery.getVoltage(), CoreBattery::Capacity::empty);
+}
+
+TEST_F(CoreBatteryTest, isInCharge)
+{
+	EXPECT_CALL(charge_input, read).WillOnce(Return(1));
+
+	ASSERT_TRUE(battery.isInCharge());
+}
+
+TEST_F(CoreBatteryTest, isNotInCharge)
+{
+	EXPECT_CALL(charge_input, read).WillOnce(Return(0));
+
+	ASSERT_FALSE(battery.isInCharge());
+}
