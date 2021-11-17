84d358da1491a10c0d35d1a7e39ea95ddd4783cc
Yann Locatelli <yann.locatelli2@gmail.com>
2021-11-17 12:16:29 +0100

    :sparkles: (demo): Base for every demo

---
+++ spikes/lk_demo_base/main.cpp
@@ -0,0 +1,33 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "drivers/BufferedSerial.h"
+#include "rtos/ThisThread.h"
+
+#include "HelloWorld.h"
+#include "LogKit.h"
+
+using namespace leka;
+using namespace std::chrono_literals;
+
+auto hello = HelloWorld {};
+
+auto main() -> int
+{
+	static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
+	logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
+
+	auto start = rtos::Kernel::Clock::now();
+
+	log_info("Hello, World!\n\n");
+	hello.start();
+
+	while (true) {
+		auto t = rtos::Kernel::Clock::now() - start;
+		log_info("A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME, hello.world,
+				 int(t.count() / 1000));
+
+		rtos::ThisThread::sleep_for(1s);
+	}
+}
