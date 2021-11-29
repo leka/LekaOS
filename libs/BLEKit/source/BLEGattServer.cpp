bea6dd9f52b3b0a6042087807e462a53eb9ddf0c
Yann Locatelli <yann.locatelli2@gmail.com>
2021-11-29 23:03:09 +0100

    :sparkles: (blekit): Robot write in BLE Gatt server

--- libs/BLEKit/source/BLEGattServer.cpp
+++ libs/BLEKit/source/BLEGattServer.cpp
@@ -5,6 +5,7 @@
 #include "BLEGattServer.h"
 
 using namespace leka;
+using namespace std::chrono_literals;
 
 void BLEGattServer::addServices(lstd::span<interface::BLEService *> services)
 {
@@ -18,6 +19,8 @@ void BLEGattServer::addServices(lstd::span<interface::BLEService *> services)
 void BLEGattServer::start(BLE &ble, events::EventQueue &event_queue)
 {
 	_server.setEventHandler(this);
+
+	event_queue.call_every(1s, this, &BLEGattServer::updateData);
 }
 
 void BLEGattServer::onDataWritten(const GattWriteCallbackParams &params)
@@ -26,8 +29,23 @@ void BLEGattServer::onDataWritten(const GattWriteCallbackParams &params)
 		auto characteristics_count = service->getCharacteristicCount();
 		for (int index = 0; index < characteristics_count; index++) {
 			if (params.handle == service->getCharacteristic(index)->getValueHandle()) {
-				service->onDataUpdated(params);
+				service->onDataWritten(params);
 			}
+		}
+	}
+}
+
+void BLEGattServer::updateData()
+{
+	updateServiceFunction update_server_data = [&](GattAttribute::Handle_t characteristic_updated, const uint8_t *data,
+												   uint16_t n_data_bytes) {
+		_server.write(characteristic_updated, data, n_data_bytes);
+	};
+
+	for (auto &service: _services) {
+		auto characteristics_count = service->getCharacteristicCount();
+		for (int index = 0; index < characteristics_count; index++) {
+			service->updateData(update_server_data);
 		}
 	}
 }
