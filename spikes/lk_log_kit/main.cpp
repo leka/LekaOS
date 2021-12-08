215ffc567f06f504d49b349e595c405100c13bb0
Ladislas de Toldi <ladislas@detoldi.me>
2021-12-08 16:19:02 +0100

    :pencil2: (spikes): Fix typos log messages

--- spikes/lk_log_kit/main.cpp
+++ spikes/lk_log_kit/main.cpp
@@ -20,16 +20,16 @@ using namespace std::chrono;
 		auto start = rtos::Kernel::Clock::now();
 		for (auto i = 1; i <= 10; ++i) {
 			auto start_run = rtos::Kernel::Clock::now();
-			log_debug("Run number: %i", i);
+			log_debug("Run debug number: %i", i);
 			log_debug("%s debug message", "First:");
 			log_info("%s information message", "Second:");
 			log_error("%s error message", "Third:");
 			auto stop_run = rtos::Kernel::Clock::now();
-			log_info("Total time to log the %i message --> %ims\n", 4, int((stop_run - start_run).count()));
+			log_info("Total time to log the %i messages --> %ims\n", 4, int((stop_run - start_run).count()));
 		}
 		auto stop = rtos::Kernel::Clock::now();
 
-		log_info("Total time to log the for loop --> %ims\n", int((stop - start).count()));
+		log_info("Total time to LOG the for loop --> %ims\n", int((stop - start).count()));
 
 		rtos::ThisThread::sleep_for(3s);
 	}
@@ -43,16 +43,16 @@ using namespace std::chrono;
 		auto start = rtos::Kernel::Clock::now();
 		for (auto i = 1; i <= 10; ++i) {
 			auto start_run = rtos::Kernel::Clock::now();
-			log_printf("Run number: %i\n", i);
+			log_printf("Run printf number: %i\n", i);
 			log_printf("%s debug message\n", "First:");
 			log_printf("%s information message\n", "Second:");
 			log_printf("%s error message\n", "Third:");
 			auto stop_run = rtos::Kernel::Clock::now();
-			log_printf("Total time to log the %i message --> %ims\n\n", 4, int((stop_run - start_run).count()));
+			log_printf("Total time to log the %i messages --> %ims\n\n", 4, int((stop_run - start_run).count()));
 		}
 		auto stop = rtos::Kernel::Clock::now();
 
-		log_printf("Total time to log the for loop --> %ims\n\n", int((stop - start).count()));
+		log_printf("Total time to PRINTF the for loop --> %ims\n\n", int((stop - start).count()));
 
 		rtos::ThisThread::sleep_for(3s);
 	}
