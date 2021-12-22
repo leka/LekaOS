52d97b69c1c23ee98eb3b72a2eb1d4bd43f23d18
Ladislas de Toldi <ladislas@detoldi.me>
2021-12-22 15:39:09 +0100

    :test_tube: (libs): Add failing functional test for LogKit

---
+++ spikes/lk_log_kit/include/utils.h
@@ -0,0 +1,11 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _INCLUDE_UTILS_H_
+#define _INCLUDE_UTILS_H_
+
+[[noreturn]] void log_thread_debug();
+[[noreturn]] void log_thread_printf();
+
+#endif	 // _INCLUDE_UTILS_H_
