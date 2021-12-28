4a972c77fd12663061ea1fdf6f029ac749696081
Ladislas de Toldi <ladislas@detoldi.me>
2021-12-28 11:10:39 +0100

    :bug: (analysis): Fix flash/ram size calculation
    Memap.py has a bug regarding flash/ram usage,
    see https://github.com/ARMmbed/mbed-os/issues/12541
    
    Using workaround from https://interrupt.memfault.com/blog/best-firmware-size-tools
    
    Also:
    
    - improve pr message output
    - refactor action and scripts using functions
    - fix several issues

--- .github/actions/compare_files/compare_files_map.sh
+++ .github/actions/compare_files/compare_files_map.sh
@@ -9,17 +9,25 @@ for target in "${all_targets[@]}"; do
 target_name=$target
 
 	if ! [[ " ${added_targets[*]} " =~ " $target " ]] && ! [[ " ${deleted_targets[*]} " =~ " $target " ]]; then
+
 		echo "$target not deleted nor new, running diff"
 
-		if ! diff_output=$(diff --unified=150 _build_tmp/$BASE_SHA/$target_name.txt _build_tmp/$HEAD_SHA/$target_name.txt); then
-			echo $diff_output
+		if ! diff_map_output=$(diff --unified=150 _build_tmp/$BASE_SHA/$target_name-map.txt _build_tmp/$HEAD_SHA/$target_name-map.txt); then
+			diff_size_output=$(diff --unified=150 _build_tmp/$BASE_SHA/$target_name-code_size.txt _build_tmp/$HEAD_SHA/$target_name-code_size.txt)
+
+			echo $diff_map_output
+			echo $diff_size_output
 
 			echo "<details>" >> $GITHUB_ENV
 			echo "<summary><b><code>$target_name</code></b> (click to expand)</summary>" >> $GITHUB_ENV
 			echo "" >> $GITHUB_ENV
 
 			echo "\`\`\`diff" >> $GITHUB_ENV
-			echo "$diff_output" >> $GITHUB_ENV
+			echo "$diff_map_output" >> $GITHUB_ENV
+			echo "\`\`\`" >> $GITHUB_ENV
+
+			echo "\`\`\`diff" >> $GITHUB_ENV
+			echo "$diff_size_output" >> $GITHUB_ENV
 			echo "\`\`\`" >> $GITHUB_ENV
 
 			echo "" >> $GITHUB_ENV
@@ -28,9 +36,13 @@ target_name=$target
 
 			no_map_diff=false
 		fi
+
 	elif [[ " ${added_targets[*]} " =~ " $target " ]]; then
+
 		echo "$target added, showing map information"
-		map_output=$(cat _build_tmp/$HEAD_SHA/$target_name.txt)
+
+		map_output=$(cat _build_tmp/$HEAD_SHA/$target_name-map.txt)
+		size_output=$(cat _build_tmp/$HEAD_SHA/$target_name-code_size.txt)
 
 		echo "<details>" >> $GITHUB_ENV
 		echo "<summary><b><code>$target_name</code></b> (click to expand)</summary>" >> $GITHUB_ENV
@@ -40,11 +52,16 @@ target_name=$target
 		echo "$map_output" >> $GITHUB_ENV
 		echo "\`\`\`" >> $GITHUB_ENV
 
+		echo "\`\`\`" >> $GITHUB_ENV
+		echo "$size_output" >> $GITHUB_ENV
+		echo "\`\`\`" >> $GITHUB_ENV
+
 		echo "" >> $GITHUB_ENV
 		echo "</details>" >> $GITHUB_ENV
 		echo "" >> $GITHUB_ENV
 
 		no_map_diff=false
+
 	fi
 
 done
