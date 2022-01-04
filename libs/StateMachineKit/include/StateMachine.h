faf44dd7fbbc648229528da0d5f9bd9cb8273733
Ladislas de Toldi <ladislas@detoldi.me>
2022-01-04 23:59:18 +0100

    :construction: (sml): Add initial simple state machine

--- libs/StateMachineKit/include/StateMachine.h
+++ libs/StateMachineKit/include/StateMachine.h
@@ -5,10 +5,31 @@
 #ifndef _LEKA_OS_LIB_STATE_MACHINE_KIT_H_
 #define _LEKA_OS_LIB_STATE_MACHINE_KIT_H_
 
+#include "boost/sml.hpp"
+
 namespace leka {
 
-class StateMachine
-{
+namespace sml::event {
+
+	struct start {
+	};
+	struct timeout {
+	};
+
+}	// namespace sml::event
+
+struct StateMachine {
+	auto operator()() const
+	{
+		using namespace boost::sml;
+
+		return make_transition_table(
+			// clang-format off
+			 * "idle"_s    + event<sml::event::start>   = "running"_s
+			,  "running"_s + event<sml::event::timeout> = "idle"_s
+			// clang-format on
+		);
+	}
 };
 
 }	// namespace leka
