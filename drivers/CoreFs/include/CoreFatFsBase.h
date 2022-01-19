26299c6389c102a79333cdd632985329f4c819df
Ladislas de Toldi <ladislas@detoldi.me>
2022-01-19 11:14:11 +0100

    :rotating_light: (clang-tidy): CoreFatFs - Modernize + Fix errors

--- drivers/CoreFs/include/CoreFatFsBase.h
+++ drivers/CoreFs/include/CoreFatFsBase.h
@@ -11,19 +11,21 @@
 
 namespace leka {
 
+// TODO (@ladislas) - return int, bool or std::optional instead of FRESULT
 class CoreFatFsBase
 {
   public:
 	virtual ~CoreFatFsBase() = default;
 
-	virtual FRESULT open(const char *path)																			= 0;
-	virtual FRESULT close()																							= 0;
-	virtual FRESULT read(void *buffer, uint32_t number_of_bytes_to_read, uint32_t *number_of_bytes_read)			= 0;
-	virtual FRESULT write(const void *buffer, uint32_t number_of_bytes_to_write, uint32_t *number_of_bytes_written) = 0;
-	virtual FRESULT seek(uint32_t bytes_offset_from_top_of_file)													= 0;
-	virtual uint32_t getSize()																						= 0;
+	virtual auto open(const char *path) -> FRESULT																 = 0;
+	virtual auto close() -> FRESULT																				 = 0;
+	virtual auto read(void *buffer, uint32_t number_of_bytes_to_read, uint32_t *number_of_bytes_read) -> FRESULT = 0;
+	virtual auto write(const void *buffer, uint32_t number_of_bytes_to_write, uint32_t *number_of_bytes_written)
+		-> FRESULT														= 0;
+	virtual auto seek(uint32_t byte_offset_from_top_of_file) -> FRESULT = 0;
+	virtual auto getSize() -> uint32_t									= 0;
 
-	virtual FIL *getPointer() = 0;
+	virtual auto getPointer() -> FIL * = 0;
 };
 
 }	// namespace leka
