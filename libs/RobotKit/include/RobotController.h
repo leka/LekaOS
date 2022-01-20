d18ee1e38b3385f82d4007a5a4fd8161e6cd6976
Yann Locatelli <yann.locatelli2@gmail.com>
2022-01-20 17:17:29 +0100

    :sparkles: (rc): Add RobotController implementation

---
+++ libs/RobotKit/include/RobotController.h
@@ -0,0 +1,27 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_ROBOT_CONTROLLER_H_
+#define _LEKA_OS_ROBOT_CONTROLLER_H_
+
+#include "StateMachine.h"
+#include "interface/RobotController.h"
+
+namespace leka {
+
+class RobotController : public interface::RobotController
+{
+  public:
+	RobotController() = default;
+
+	void startSystem() final {};
+	void stopSystem() final {};
+
+  private:
+	boost::sml::sm<system::robot::StateMachine> _sm {static_cast<interface::RobotController &>(*this)};
+};
+
+}	// namespace leka
+
+#endif	 // _LEKA_OS_ROBOT_CONTROLLER_H_
