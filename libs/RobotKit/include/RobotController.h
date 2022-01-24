666df32fec26846d8b736c7c184e9fdc015c4062
Yann Locatelli <yann.locatelli2@gmail.com>
2022-01-24 17:22:07 +0100

    :sparkles: (rc): Replace running state to charging state + implementation

--- libs/RobotKit/include/RobotController.h
+++ libs/RobotKit/include/RobotController.h
@@ -7,6 +7,7 @@
 
 #include "StateMachine.h"
 #include "interface/RobotController.h"
+#include "interface/drivers/Battery.h"
 
 namespace leka {
 
@@ -16,10 +17,9 @@ class RobotController : public interface::RobotController
   public:
 	sm_t state_machine {static_cast<interface::RobotController &>(*this)};
 
-	RobotController() = default;
+	explicit RobotController(interface::Battery &battery) : _battery(battery) {};
 
-	void startSystem() final {};
-	void stopSystem() final {};
+	auto isCharging() -> bool final { return _battery.isCharging(); };
 
 	void raise(auto event) { state_machine.process_event(event); };
 
@@ -30,8 +30,17 @@ class RobotController : public interface::RobotController
 		// Initializations
 		// Setup callbacks for each events
 
+		auto on_charge_did_start = [this]() { raise(event::charge_did_start {}); };
+		_battery.onChargeDidStart(on_charge_did_start);
+
+		auto on_charge_did_stop = [this]() { raise(event::charge_did_stop {}); };
+		_battery.onChargeDidStop(on_charge_did_stop);
+
 		raise(event::setup_complete {});
 	};
+
+  private:
+	interface::Battery &_battery;
 };
 
 }	// namespace leka
--- libs/RobotKit/tests/RobotController_test.cpp
+++ libs/RobotKit/tests/RobotController_test.cpp
@@ -6,6 +6,7 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "mocks/leka/Battery.h"
 
 using namespace leka;
 using namespace leka::system;
@@ -13,13 +14,30 @@ namespace bsml	= boost::sml;
 namespace lksm	= system::robot::sm;
 namespace event = system::robot::sm::event;
 
+using testing::Return;
+
+ACTION_P(GetCallback, pointer)
+{
+	*pointer = mbed::Callback<void()>(arg0);
+}
+
 class RobotControllerTest : public testing::Test
 {
   protected:
-	// void SetUp() override {}
+	void SetUp() override
+	{
+		EXPECT_CALL(battery, onChargeDidStart).WillOnce(GetCallback(&on_charge_did_start));
+		EXPECT_CALL(battery, onChargeDidStop).WillOnce(GetCallback(&on_charge_did_stop));
+
+		rc.registerEvents();
+	}
 	// void TearDown() override {}
 
-	RobotController<bsml::sm<robot::StateMachine, bsml::testing>> rc {};
+	mock::Battery battery {};
+	RobotController<bsml::sm<robot::StateMachine, bsml::testing>> rc {battery};
+
+	mbed::Callback<void()> on_charge_did_start {};
+	mbed::Callback<void()> on_charge_did_stop {};
 };
 
 TEST_F(RobotControllerTest, initialization)
@@ -31,17 +49,54 @@ TEST_F(RobotControllerTest, stateSetupEventSetupComplete)
 {
 	rc.state_machine.set_current_states(lksm::state::setup);
 
+	EXPECT_CALL(battery, onChargeDidStart).Times(1);
+	EXPECT_CALL(battery, onChargeDidStop).Times(1);
+
 	rc.registerEvents();
 
 	EXPECT_TRUE(rc.state_machine.is(lksm::state::idle));
 }
 
-TEST_F(RobotControllerTest, startSystemAction)
+TEST_F(RobotControllerTest, stateIdleEventChargeDidStartGuardIsChargingTrue)
 {
-	rc.startSystem();
+	rc.state_machine.set_current_states(lksm::state::idle);
+
+	EXPECT_CALL(battery, isCharging).WillOnce(Return(true));
+
+	on_charge_did_start();
+
+	EXPECT_TRUE(rc.state_machine.is(lksm::state::charging));
 }
 
-TEST_F(RobotControllerTest, stopSystemAction)
+TEST_F(RobotControllerTest, stateIdleEventChargeDidStartGuardIsChargingFalse)
 {
-	rc.stopSystem();
+	rc.state_machine.set_current_states(lksm::state::idle);
+
+	EXPECT_CALL(battery, isCharging).WillOnce(Return(false));
+
+	on_charge_did_start();
+
+	EXPECT_TRUE(rc.state_machine.is(lksm::state::idle));
+}
+
+TEST_F(RobotControllerTest, stateChargingEventChargeDidStopGuardIsChargingTrue)
+{
+	rc.state_machine.set_current_states(lksm::state::charging);
+
+	EXPECT_CALL(battery, isCharging).WillOnce(Return(true));
+
+	on_charge_did_stop();
+
+	EXPECT_TRUE(rc.state_machine.is(lksm::state::charging));
+}
+
+TEST_F(RobotControllerTest, stateChargingEventChargeDidStopGuardIsChargingFalse)
+{
+	rc.state_machine.set_current_states(lksm::state::charging);
+
+	EXPECT_CALL(battery, isCharging).WillOnce(Return(false));
+
+	on_charge_did_stop();
+
+	EXPECT_TRUE(rc.state_machine.is(lksm::state::idle));
 }
