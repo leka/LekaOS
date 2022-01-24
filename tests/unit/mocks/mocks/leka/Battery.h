3276e3810ed3b6557aa9bc766a12da317bf5178c
Yann Locatelli <yann.locatelli2@gmail.com>
2022-01-24 17:58:33 +0100

    :clown_face: (battery): Fix rename of voltage and level + add missing callbacks

--- tests/unit/mocks/mocks/leka/Battery.h
+++ tests/unit/mocks/mocks/leka/Battery.h
@@ -13,8 +13,11 @@ namespace leka::mock {
 class Battery : public interface::Battery
 {
   public:
-	MOCK_METHOD(float, getVoltage, (), (override));
-	MOCK_METHOD(uint8_t, getLevel, (), (override));
+	MOCK_METHOD(void, onChargeDidStart, (mbed::Callback<void()> const &), (override));
+	MOCK_METHOD(void, onChargeDidStop, (mbed::Callback<void()> const &), (override));
+
+	MOCK_METHOD(float, voltage, (), (override));
+	MOCK_METHOD(uint8_t, level, (), (override));
 	MOCK_METHOD(bool, isCharging, (), (override));
 };
 
