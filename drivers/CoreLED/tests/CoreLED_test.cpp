f66432bd60a7905b7921e87feab00c208b6d969c
Ladislas de Toldi <ladislas@detoldi.me>
2022-01-27 00:07:32 +0100

    :white_check_mark: (tests): Add turnOff test after set/showColor

--- drivers/CoreLED/tests/CoreLED_test.cpp
+++ drivers/CoreLED/tests/CoreLED_test.cpp
@@ -99,6 +99,29 @@ TEST_F(CoreLEDTests, turnOnWithoutSetColor)
 	EXPECT_EQ(coreled.isOn(), false);
 }
 
+TEST_F(CoreLEDTests, turnOffAfterSetAndShowColor)
+{
+	coreled.setColor(RGB::magenta);
+	coreled.showColor();
+
+	auto expected_color = RGB::black;
+
+	{
+		InSequence seq;
+
+		EXPECT_CALL(spimock, write(testing::ElementsAre(0x00, 0x00, 0x00, 0x00))).Times(1);
+		EXPECT_CALL(spimock,
+					write(testing::ElementsAre(_, expected_color.red, expected_color.green, expected_color.blue)))
+			.Times(20);
+		EXPECT_CALL(spimock, write(testing::ElementsAre(0x00, 0x00, 0x00, 0x00))).Times(1);
+		EXPECT_CALL(spimock, write(testing::ElementsAre(0x00, 0x00))).Times(1);
+	}
+
+	coreled.turnOff();
+
+	EXPECT_EQ(coreled.isOn(), false);
+}
+
 TEST_F(CoreLEDTests, turnOffWithoutSetColor)
 {
 	auto expected_color = RGB::black;
