5cb61365489dbea763d40f4d620e5c8bcc582dc1
Yann Locatelli <yann.locatelli2@gmail.com>
2022-01-28 17:01:47 +0100

    :sparkles: (boot): Bootloader can now access to configuration file in SD

--- app/bootloader/main.cpp
+++ app/bootloader/main.cpp
@@ -11,6 +11,9 @@
 #include "rtos/ThisThread.h"
 
 #include "CoreBattery.h"
+#include "FATFileSystem.h"
+#include "FileSystemKit.h"
+#include "SDBlockDevice.h"
 #include "bootutil/bootutil.h"
 #include "bootutil/image.h"
 
@@ -22,11 +25,28 @@ constexpr auto default_address = uint32_t {0x8040000 + 0x1000};	  // Start appli
 static auto charge_status_input = mbed::InterruptIn {PinName::BATTERY_CHARGE_STATUS};
 static auto battery				= leka::CoreBattery {PinName::BATTERY_VOLTAGE, charge_status_input};
 
+SDBlockDevice sd_blockdevice(SD_SPI_MOSI, SD_SPI_MISO, SD_SPI_SCK);
+FATFileSystem fatfs("fs");
+
+FileSystemKit::File _configuration_file {"/fs/conf/bootloader.conf"};
+
+void initializeSD()
+{
+	constexpr auto default_sd_blockdevice_frequency = uint64_t {25'000'000};
+
+	sd_blockdevice.init();
+	sd_blockdevice.frequency(default_sd_blockdevice_frequency);
+
+	fatfs.mount(&sd_blockdevice);
+}
+
 auto main() -> int
 {
 	while (battery.voltage() < CoreBattery::Capacity::empty) {
 		rtos::ThisThread::sleep_for(10s);
 	}
+
+	initializeSD();
 
 	auto address = default_address;
 
