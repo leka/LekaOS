e88e51a231e319d5f3bec02eb81ef38f1cca4777
Hugo Pezziardi <hugo@pezziardi.net>
2022-02-02 17:49:12 +0100

    :art: (spikes): Use index and array to setColor

--- spikes/lk_coreled/main.cpp
+++ spikes/lk_coreled/main.cpp
@@ -17,19 +17,27 @@ using namespace leka;
 using namespace std::chrono;
 
 auto constexpr NUM_BELT_LEDS = 20;
-auto constexpr NUM_EAR_LEDS	 = 2;
+auto constexpr NUM_EARS_LEDS = 2;
 
 constexpr std::array<RGB, 6> colors_available = {
 	RGB::pure_green, RGB::pure_red, RGB::pure_blue, RGB::yellow, RGB::cyan, RGB::magenta,
 };
 
+constexpr std::array<RGB, NUM_BELT_LEDS> color_belt_array = {
+	RGB::cyan,	  RGB::magenta, RGB::yellow,  RGB::cyan,	RGB::magenta, RGB::yellow,	RGB::cyan,
+	RGB::magenta, RGB::yellow,	RGB::cyan,	  RGB::magenta, RGB::yellow,  RGB::cyan,	RGB::magenta,
+	RGB::yellow,  RGB::cyan,	RGB::magenta, RGB::yellow,	RGB::cyan,	  RGB::magenta,
+};
+
+constexpr std::array<RGB, NUM_EARS_LEDS> color_ears_array = {RGB::cyan, RGB::magenta};
+
 CoreSPI corespi_belt(LED_BELT_SPI_MOSI, NC, LED_BELT_SPI_SCK);
 CoreSPI corespi_ears(LED_EARS_SPI_MOSI, NC, LED_EARS_SPI_SCK);
 
 CoreLED<NUM_BELT_LEDS> belt(corespi_belt);
-CoreLED<NUM_EAR_LEDS> ears(corespi_ears);
+CoreLED<NUM_EARS_LEDS> ears(corespi_ears);
 
-void changeColor(interface::LED<NUM_EAR_LEDS> &ears, interface::LED<NUM_BELT_LEDS> &belt)
+void changeColor(interface::LED<NUM_EARS_LEDS> &ears, interface::LED<NUM_BELT_LEDS> &belt)
 {
 	static auto index = uint8_t {0};
 
@@ -41,11 +49,26 @@ void changeColor(interface::LED<NUM_EAR_LEDS> &ears, interface::LED<NUM_BELT_LED
 		belt.showColor();
 
 		index++;
-	} else {
-		ears.hideColor();
-		belt.hideColor();
+	} else if (index == colors_available.size()) {
+		ears.setColorWithArray(color_ears_array);
+		ears.showColor();
 
-		index = 0;
+		belt.setColorWithArray(color_belt_array);
+		belt.showColor();
+
+		index++;
+	} else {
+		static auto led = uint8_t {0};
+		ears.setColorAtIndex(led / 10, RGB::pure_green);
+		ears.showColor();
+
+		belt.setColorAtIndex(led, RGB::pure_green);
+		belt.showColor();
+
+		led++;
+		if (led == NUM_BELT_LEDS) {
+			led = 0;
+		}
 	}
 }
 
