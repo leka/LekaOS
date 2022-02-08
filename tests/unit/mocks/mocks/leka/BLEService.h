2f5446f6f5de38cbeaa0b456813fe2755a835693
Yann Locatelli <yann.locatelli2@gmail.com>
2022-02-09 00:13:45 +0100

    :clown_face: (ble): Mock BLEService

---
+++ tests/unit/mocks/mocks/leka/BLEService.h
@@ -0,0 +1,29 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_BLE_SERVICE_MOCK_H_
+#define _LEKA_OS_BLE_SERVICE_MOCK_H_
+
+#include "gmock/gmock.h"
+#include "internal/BLEService.h"
+
+namespace leka::mock {
+
+class BLEService : public interface::BLEService
+{
+  public:
+	BLEService(const UUID &uuid, std::span<GattCharacteristic *> characteristics)
+		: interface::BLEService(uuid, characteristics)
+	{
+	}
+
+	MOCK_METHOD(void, onDataReceived, (const data_received_handle_t &params), (override));
+
+	MOCK_METHOD(void, onDataReadyToSend, (const data_to_send_handle_t &function), (override));
+	MOCK_METHOD(void, sendData, (), (override));
+};
+
+}	// namespace leka::mock
+
+#endif	 // _LEKA_OS_BLE_SERVICE_MOCK_H_
