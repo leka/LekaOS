d111a7ad53cf88d54217e2ae927d62db05f706bf
Yann Locatelli <yann.locatelli2@gmail.com>
2022-02-08 15:19:46 +0100

    :sparkles: (ble): Add BLEService

---
+++ libs/BLEKit/include/internal/BLEService.h
@@ -0,0 +1,39 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef _LEKA_OS_LIB_BLE_SERVICE_INTERFACE_H_
+#define _LEKA_OS_LIB_BLE_SERVICE_INTERFACE_H_
+
+#include <span>
+
+#include "ble/gatt/GattCharacteristic.h"
+#include "ble/gatt/GattService.h"
+
+namespace leka::interface {
+
+class BLEService : public GattService
+{
+  public:
+	using data_received_handle_t = GattWriteCallbackParams;
+	using data_to_send_handle_t =
+		std::function<void(GattAttribute::Handle_t characteristic_updated, const uint8_t *data, uint16_t n_data_bytes)>;
+
+	BLEService(const UUID &uuid, std::span<GattCharacteristic *> characteristics)
+		: GattService(uuid, characteristics.data(), std::size(characteristics)) {};
+
+	[[nodiscard]] auto getCharacteristicCount() const -> uint8_t { return GattService::getCharacteristicCount(); };
+	[[nodiscard]] auto getCharacteristic(uint8_t index) -> GattCharacteristic *
+	{
+		return GattService::getCharacteristic(index);
+	};
+
+	virtual void onDataReceived(const data_received_handle_t &handle) = 0;
+
+	virtual void onDataReadyToSend(const data_to_send_handle_t &function) = 0;
+	virtual void sendData()												  = 0;
+};
+
+}	// namespace leka::interface
+
+#endif	 // _LEKA_OS_LIB_BLE_SERVICE_INTERFACE_H_
