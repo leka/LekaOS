722a45b506780200fa56f6c221db0236032a67fd
Mourad Latoundji <{mouradlatoundji@gmail.com}>
2022-02-20 20:48:59 +0100

    :white_check_mark: (File)Add test for 'New Line' function

--- libs/FileSystemKit/source/File.cpp
+++ libs/FileSystemKit/source/File.cpp
@@ -113,12 +113,12 @@ auto FileSystemKit::File::tell() -> size_t
 
 auto FileSystemKit::File::new_line() -> size_t
 {
-	auto data = std::to_array<uint8_t>({0x0a, 0x0d});
+	auto data = std::to_array<uint8_t>({0x0A, 0x0D});
 	return std::fwrite(data.data(), sizeof(uint8_t), data.size(), _file.get());
 }
 
 auto FileSystemKit::File::carriage_return() -> size_t
 {
-	auto data = std::to_array<uint8_t>({0x0d});
+	auto data = std::to_array<uint8_t>({0x0D});
 	return std::fwrite(data.data(), sizeof(uint8_t), data.size(), _file.get());
 }
