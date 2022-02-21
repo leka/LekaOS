c49d6abc04351759808e16f073dc961e905805a7
Hugo Pezziardi <hugo@pezziardi.net>
2022-02-21 17:17:58 +0100

    :art: (libs): Add colorGradient function

--- libs/ColorKit/include/ColorKit.h
+++ libs/ColorKit/include/ColorKit.h
@@ -5,5 +5,11 @@
 #pragma once
 
 #include "RGB.h"
+#include "internal/conversion.h"
+#include "internal/interpolation.h"
+
 namespace leka::ColorKit {
+
+auto colorGradient(const RGB &color1, const RGB &color2, const float &position) -> RGB;
+
 }	// namespace leka::ColorKit
