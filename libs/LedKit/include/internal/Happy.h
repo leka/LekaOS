e7ac02bbd8c721a8947511433a521c7a4aad7c16
Hugo Pezziardi <hugo@pezziardi.net>
2022-02-23 12:20:34 +0100

    :sparkles: (animation): Add Happy animation

---
+++ libs/LedKit/include/internal/Happy.h
@@ -0,0 +1,66 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// +   Stage 1      +     Stage 2  +     Stage 3    +
+// |                |              |                |
+// |                |              |                |
+// |                |              |                |
+// |                |--------------|                |
+// |               /|              |\               |
+// |              / |              | \              |
+// |             /  |              |  \             |
+// |            /   |              |   \            |
+// |           /    |              |    \           |
+// |          /     |              |     \          |
+// |         /      |              |      \         |
+// |        /       |              |       \        |
+// |       /        |              |        \       |
+// |      /         |              |         \      |
+// |     /          |              |          \     |
+// |    /           |              |           \    |
+// |   /            |              |            \   |
+// |  /             |              |             \  |
+// | /              |              |              \ |
+// |/               |              |               \|
+// |                |              |                |
+// |                |              |                |
+// |                |              |                |
+
+#pragma once
+
+// ? LCOV_EXCL_START - Exclude from coverage report
+
+#include "LEDAnimation.h"
+
+namespace leka::led::animation {
+
+class Happy : public interface::LEDAnimation
+{
+  public:
+	explicit Happy(interface::LED &ears, interface::LED &belt) : _ears(ears), _belt(belt) {};
+
+	void start() final;
+	void run() final;
+	void stop() final;
+
+  private:
+	interface::LED &_ears;
+	interface::LED &_belt;
+	uint8_t _step  = 0;
+	uint8_t _stage = 1;
+
+	[[nodiscard]] auto mapStep(uint8_t step) const -> float;
+
+	void stage1();
+	void stage2();
+	void stage3();
+	void stage4();
+	void stage5();
+
+	void turnLedBlack();
+};
+
+}	// namespace leka::led::animation
+
+// ? LCOV_EXCL_STOP - Exclude from coverage report
