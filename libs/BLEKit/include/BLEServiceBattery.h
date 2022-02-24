a28859b9bd993e7c2c6136d60b45b8d640cab5f0
Ladislas de Toldi <ladislas@detoldi.me>
2022-02-24 11:09:26 +0100

    :construction: (REVIEW): Replace header guards with #pragma once

--- libs/BLEKit/include/BLEServiceBattery.h
+++ libs/BLEKit/include/BLEServiceBattery.h
@@ -2,8 +2,7 @@
 // Copyright 2021 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef _LEKA_OS_LIB_BLE_SERVICE_BATTERY_H_
-#define _LEKA_OS_LIB_BLE_SERVICE_BATTERY_H_
+#pragma once
 
 #include "internal/BLEService.h"
 
@@ -43,5 +42,3 @@ class BLEServiceBattery : public interface::BLEService
 };
 
 }	// namespace leka
-
-#endif	 // _LEKA_OS_LIB_BLE_SERVICE_BATTERY_H_
