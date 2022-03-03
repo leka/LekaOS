e8964f6fd0f3f6dbe0c6de84d1105576175fefcf
Hugo Pezziardi <hugo@pezziardi.net>
2022-03-03 13:05:38 +0100

    :sparkles: (led animation): Add Sneeze animation

---
+++ libs/LedKit/include/internal/Sneeze.h
@@ -0,0 +1,46 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+// ? LCOV_EXCL_START - Exclude from coverage report
+
+#include "LEDAnimation.h"
+
+namespace leka::led::animation {
+
+class Sneeze : public interface::LEDAnimation
+{
+  public:
+	explicit Sneeze(interface::LED &ears, interface::LED &belt) : _ears(ears), _belt(belt) {};
+
+	void start() final;
+	void run() final;
+	void stop() final;
+
+  private:
+	interface::LED &_ears;
+	interface::LED &_belt;
+	uint8_t _step			 = 0;
+	uint8_t _stage			 = 1;
+	uint8_t _sneeze_position = 0;
+
+	[[nodiscard]] auto mapStep(uint8_t step) const -> float;
+
+	void stage1();
+	void stage2();
+	void stage3();
+	void stage4();
+	void stage5();
+	void stage6();
+
+	void sneezeFromRight();
+	void sneezeFromLeft();
+
+	void turnLedBlack();
+};
+
+}	// namespace leka::led::animation
+
+// ? LCOV_EXCL_STOP - Exclude from coverage report
