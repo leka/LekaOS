21795e42c1fb9dfca52457a8df378d19c64b9d08
Hugo Pezziardi <hugo@pezziardi.net>
2022-03-07 17:37:22 +0100

    :art: (spikes): Update lk_led_kit

--- spikes/lk_led_kit/main.cpp
+++ spikes/lk_led_kit/main.cpp
@@ -27,25 +27,7 @@ auto animation_event_queue = events::EventQueue {};
 
 auto ledkit = LedKit {animation_thread, animation_event_queue, ears, belt};
 
-auto colors_available = std::to_array({
-	RGB::pure_green,
-	RGB::pure_red,
-	RGB::pure_blue,
-	RGB::yellow,
-	RGB::cyan,
-	RGB::magenta,
-});
-
 HelloWorld hello;
-
-RGB sick1		   = RGB {30, 255, 0};
-RGB sick2		   = RGB {94, 104, 1};
-RGB singing_green  = RGB {29, 168, 0};
-RGB singing_red	   = RGB {255, 65, 61};
-RGB singing_yellow = RGB {255, 217, 61};
-RGB singing_blue   = RGB {21, 125, 230};
-
-RGB red_angry = RGB {255, 98, 98};
 
 auto main() -> int
 {
@@ -53,15 +35,12 @@ auto main() -> int
 
 	log_info("Hello, World!\n\n");
 
-	auto start = rtos::Kernel::Clock::now();
-
-	rtos::ThisThread::sleep_for(2s);
-
 	hello.start();
 
 	while (true) {
-		auto t = rtos::Kernel::Clock::now() - start;
-		log_info("A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME, hello.world,
-				 int(t.count() / 1000));
+		rtos::ThisThread::sleep_for(40s);
+
+		ledkit.stop();
+		rtos::ThisThread::sleep_for(1s);
 	}
 }
