dfa7d5c2790f42846b020bd9519cf56ca7fd0922
Hugo Pezziardi <hugo@pezziardi.net>
2022-03-08 11:01:45 +0100

    :sparkles: (animation): Add LoadingYellow animation

---
+++ libs/LedKit/include/internal/LoadingYellow.h
@@ -0,0 +1,66 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// +  Stage 1   +  2  +  3  +  4  +  5  +  6  +  7  +  8  +  9  +    10      +
+// |            |     |     |     |     |     |     |     |     |            |
+// |            |\    |     |\    |     |\    |     |\    |     |\           | --- Yellow
+// |           /| \   |    /| \   |    /| \   |    /| \   |    /| \          |
+// |          / |  \  |   / |  \  |   / |  \  |   / |  \  |   / |  \         |
+// |         /  |   \ |  /  |   \ |  /  |   \ |  /  |   \ |  /  |   \        |
+// |        /   |    \| /   |    \| /   |    \| /   |    \| /   |    \       |
+// |       /    |     |/    |     |/    |     |/    |     |/    |     \      |
+// |      /     |     |     |     |     |     |     |     |     |      \     |
+// |     /      |     |     |     |     |     |     |     |     |       \    |
+// |    /       |     |     |     |     |     |     |     |     |        \   |
+// |   /        |     |     |     |     |     |     |     |     |         \  |
+// |  /         |     |     |     |     |     |     |     |     |          \ |
+// | /          |     |     |     |     |     |     |     |     |           \| --- Black
+// |/           |     |     |     |     |     |     |     |     |            |
+// |            |     |     |     |     |     |     |     |     |            |
+
+#pragma once
+
+// ? LCOV_EXCL_START - Exclude from coverage report
+
+#include "LEDAnimation.h"
+
+namespace leka::led::animation {
+
+class LoadingYellow : public interface::LEDAnimation
+{
+  public:
+	explicit LoadingYellow(interface::LED &ears, interface::LED &belt) : _ears(ears), _belt(belt) {};
+
+	void start() final;
+	void run() final;
+	void stop() final;
+
+  private:
+	interface::LED &_ears;
+	interface::LED &_belt;
+	uint8_t _step  = 0;
+	uint8_t _stage = 1;
+
+	[[nodiscard]] auto mapStep(uint8_t step) const -> float;
+
+	void stage1();
+	void stage2();
+	void stage3();
+	void stage4();
+	void stage5();
+	void stage6();
+	void stage7();
+	void stage8();
+	void stage9();
+	void stage10();
+
+	void increaseBrightness();
+	void decreaseBrightness(float treshold);
+
+	void turnLedBlack();
+};
+
+}	// namespace leka::led::animation
+
+// ? LCOV_EXCL_STOP - Exclude from coverage report
