b83d5f7abf9ec596d06278ff5a89e552e42ed200
Yann Locatelli <yann.locatelli2@gmail.com>
2022-03-14 16:38:42 +0100

    :sparkles: (mcu): Add CoreMCU

---
+++ drivers/CoreMCU/include/CoreMCU.h
@@ -0,0 +1,15 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+namespace leka {
+
+class CoreMCU
+{
+  public:
+	explicit CoreMCU() = default;
+};
+
+}	// namespace leka
