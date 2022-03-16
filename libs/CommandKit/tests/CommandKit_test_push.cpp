2c5fba6499eb0565fe7d96be63c6de08d19f4db4
Ladislas de Toldi <ladislas@detoldi.me>
2022-03-16 17:02:09 +0100

    FIXUP - add more push tests

--- libs/CommandKit/tests/CommandKit_test_push.cpp
+++ libs/CommandKit/tests/CommandKit_test_push.cpp
@@ -38,6 +38,92 @@ TEST_F(CommandKitPushTest, pushDataGood)
 	cmdkit.push(data);
 }
 
+TEST_F(CommandKitPushTest, pushDataGoodInTwoPackets)
+{
+	auto cmd  = mock::Command {0x42};
+	auto cmds = std::to_array<interface::Command *>({&cmd});
+
+	cmdkit.registerCommand(cmds);
+
+	// clang-format off
+	auto data1 = std::to_array<uint8_t>({   0x2A, 0x2A, 0x2A, 0x2A, 0x01, /* 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x91 */});
+	auto data2 = std::to_array<uint8_t>({/* 0x2A, 0x2A, 0x2A, 0x2A, 0x01, */ 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x91   });
+	// clang-format on
+
+	EXPECT_CALL(cmd, HasBeenCalled).Times(1);
+
+	cmdkit.push(data1);
+	cmdkit.push(data2);
+}
+
+TEST_F(CommandKitPushTest, pushDataGoodInTwoPacketsStartingWithFramePatternOnly)
+{
+	auto cmd  = mock::Command {0x42};
+	auto cmds = std::to_array<interface::Command *>({&cmd});
+
+	cmdkit.registerCommand(cmds);
+
+	// clang-format off
+	auto data1 = std::to_array<uint8_t>({   0x2A, 0x2A, 0x2A, 0x2A, /* 0x01, 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x91 */});
+	auto data2 = std::to_array<uint8_t>({/* 0x2A, 0x2A, 0x2A, 0x2A, */ 0x01, 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x91   });
+	// clang-format on
+
+	EXPECT_CALL(cmd, HasBeenCalled).Times(1);
+
+	cmdkit.push(data1);
+	cmdkit.push(data2);
+}
+
+TEST_F(CommandKitPushTest, pushDataGoodInTwoPacketsStartingWithHalfFramePatternOnly)
+{
+	auto cmd  = mock::Command {0x42};
+	auto cmds = std::to_array<interface::Command *>({&cmd});
+
+	cmdkit.registerCommand(cmds);
+
+	// clang-format off
+	auto data1 = std::to_array<uint8_t>({   0x2A, 0x2A, /* 0x2A, 0x2A, 0x01, 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x91 */});
+	auto data2 = std::to_array<uint8_t>({/* 0x2A, 0x2A, */ 0x2A, 0x2A, 0x01, 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x91   });
+	// clang-format on
+
+	EXPECT_CALL(cmd, HasBeenCalled).Times(1);
+
+	cmdkit.push(data1);
+	cmdkit.push(data2);
+}
+
+TEST_F(CommandKitPushTest, pushDataGoodInTwoPacketsCuttingCommand)
+{
+	auto cmd  = mock::Command {0x42};
+	auto cmds = std::to_array<interface::Command *>({&cmd});
+
+	cmdkit.registerCommand(cmds);
+
+	// clang-format off
+	auto data1 = std::to_array<uint8_t>({   0x2A, 0x2A, 0x2A, 0x2A, 0x01, 0x42, 0x11, 0x02, /* 0x00, 0x7F, 0xFF, 0x91 */});
+	auto data2 = std::to_array<uint8_t>({/* 0x2A, 0x2A, 0x2A, 0x2A, 0x01, 0x42, 0x11, 0x02, */ 0x00, 0x7F, 0xFF, 0x91   });
+	// clang-format on
+
+	EXPECT_CALL(cmd, HasBeenCalled).Times(0);
+
+	cmdkit.push(data1);
+	cmdkit.push(data2);
+}
+
+TEST_F(CommandKitPushTest, pushDataGoodButNoEnough)
+{
+	auto cmd  = mock::Command {0x42};
+	auto cmds = std::to_array<interface::Command *>({&cmd});
+
+	cmdkit.registerCommand(cmds);
+
+	auto data = std::to_array<uint8_t>({0x2A, 0x2A, 0x2A, 0x2A, 0x01, 0x42, 0x11});
+
+	EXPECT_CALL(cmd, HasBeenCalled).Times(0);
+
+	cmdkit.push(data);
+}
+
 TEST_F(CommandKitPushTest, pushDataBadNoFramePattern)
 {
 	auto cmd  = mock::Command {0x42};
@@ -45,6 +131,7 @@ TEST_F(CommandKitPushTest, pushDataBadNoFramePattern)
 
 	cmdkit.registerCommand(cmds);
 
+	// ? missing frame pattern
 	auto data = std::to_array<uint8_t>({0x01, 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x91});
 
 	EXPECT_CALL(cmd, HasBeenCalled).Times(0);
@@ -61,6 +148,36 @@ TEST_F(CommandKitPushTest, pushDataBadWrongChecksum)
 
 	// ? wrong checksum
 	auto data = std::to_array<uint8_t>({0x2A, 0x2A, 0x2A, 0x2A, 0x01, 0x42, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x00});
+
+	EXPECT_CALL(cmd, HasBeenCalled).Times(0);
+
+	cmdkit.push(data);
+}
+
+TEST_F(CommandKitPushTest, pushDataBadWrongCommandId)
+{
+	auto cmd  = mock::Command {0x42};
+	auto cmds = std::to_array<interface::Command *>({&cmd});
+
+	cmdkit.registerCommand(cmds);
+
+	// ? wrong command id
+	auto data = std::to_array<uint8_t>({0x2A, 0x2A, 0x2A, 0x2A, 0x01, 0x00, 0x11, 0x02, 0x00, 0x7F, 0xFF, 0x00});
+
+	EXPECT_CALL(cmd, HasBeenCalled).Times(0);
+
+	cmdkit.push(data);
+}
+
+TEST_F(CommandKitPushTest, pushDataMissingData)
+{
+	auto cmd  = mock::Command {0x42};
+	auto cmds = std::to_array<interface::Command *>({&cmd});
+
+	cmdkit.registerCommand(cmds);
+
+	// ? missing data
+	auto data = std::to_array<uint8_t>({0x2A, 0x2A, 0x2A, 0x2A, 0x01, 0x42, 0x11, 0x02, 0x00, 0x7F});
 
 	EXPECT_CALL(cmd, HasBeenCalled).Times(0);
 
