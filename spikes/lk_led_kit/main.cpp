ca1cbe53432b1047b73be2e13c113de406b304b4
Hugo Pezziardi <hugo@pezziardi.net>
2022-03-21 16:48:34 +0100

    :art: (spikes): LedKit - Reorganize global objects w/ namespaces

--- spikes/lk_led_kit/main.cpp
+++ spikes/lk_led_kit/main.cpp
@@ -17,22 +17,57 @@
 using namespace leka;
 using namespace std::chrono;
 
-auto corespi_belt = CoreSPI {LED_BELT_SPI_MOSI, NC, LED_BELT_SPI_SCK};
-auto corespi_ears = CoreSPI {LED_EARS_SPI_MOSI, NC, LED_EARS_SPI_SCK};
+namespace {
 
-auto ears = CoreLED<LedKit::kNumberOfLedsEars> {corespi_ears};
-auto belt = CoreLED<LedKit::kNumberOfLedsBelt> {corespi_belt};
+namespace leds {
 
-auto animation_thread = rtos::Thread {};
-auto event_flags	  = CoreEventFlags {};
+	namespace internal {
 
-auto ledkit = LedKit {animation_thread, event_flags, ears, belt};
+		namespace ears {
 
-HelloWorld hello;
+			auto spi			= CoreSPI {LED_EARS_SPI_MOSI, NC, LED_EARS_SPI_SCK};
+			constexpr auto size = 2;
+
+		}	// namespace ears
+
+		namespace belt {
+
+			auto spi			= CoreSPI {LED_BELT_SPI_MOSI, NC, LED_BELT_SPI_SCK};
+			constexpr auto size = 20;
+
+		}	// namespace belt
+
+	}	// namespace internal
+
+	namespace animations {
+
+		auto thread		 = rtos::Thread {};
+		auto event_flags = CoreEventFlags {};
+
+	}	// namespace animations
+
+	auto ears = CoreLED<internal::ears::size> {internal::ears::spi};
+	auto belt = CoreLED<internal::belt::size> {internal::belt::spi};
+
+	void turnOff()
+	{
+		ears.setColor(RGB::black);
+		belt.setColor(RGB::black);
+		ears.show();
+		belt.show();
+	}
+
+}	// namespace leds
+
+auto hello	= HelloWorld {};
+auto ledkit = LedKit {leds::animations::thread, leds::animations::event_flags, leds::ears, leds::belt};
+
+}	// namespace
 
 auto main() -> int
 {
 	logger::init();
+	leds::turnOff();
 
 	log_info("Hello, World!\n\n");
 
