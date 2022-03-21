0822526a126ad9fdc18fd5b4b5826de1e94e5c9b
Yann Locatelli <yann.locatelli2@gmail.com>
2022-03-21 16:58:24 +0100

    :children_crossing: (rc): Add Commands received callback via BLE

--- libs/BLEKit/include/BLEServiceCommands.h
+++ libs/BLEKit/include/BLEServiceCommands.h
@@ -20,15 +20,22 @@ class BLEServiceCommands : public interface::BLEService
 	{
 		if (params.handle == tx_characteristic.getValueHandle()) {
 			std::copy(params.data, params.data + params.len, _buffer.begin());
-			_cmdkit.push(std::span {_buffer.data(), params.len});
+
+			if (_on_commands_received) {
+				_on_commands_received(std::span {_buffer.data(), params.len});
+			}
 		}
 	};
+
+	void onCommandsReceived(std::function<void(std::span<uint8_t>)> callback) { _on_commands_received = callback; }
 
   private:
 	std::array<uint8_t, 128> _buffer {};
 	WriteOnlyArrayGattCharacteristic<uint8_t, 128> tx_characteristic {
 		service::commands::characteristic::tx, _buffer.begin(),
 		GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE};
+
+	std::function<void(std::span<uint8_t>)> _on_commands_received;
 
 	std::array<GattCharacteristic *, 1> _characteristic_table {&tx_characteristic};
 	CommandKit &_cmdkit;
