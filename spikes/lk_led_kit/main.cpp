81ec9e616c7fc311a872dabd5dfbb1d1a5cc1564
Hugo Pezziardi <hugo@pezziardi.net>
2022-03-24 15:42:21 +0100

    :art: (spikes): Update spikes

--- spikes/lk_led_kit/main.cpp
+++ spikes/lk_led_kit/main.cpp
@@ -42,22 +42,84 @@ auto main() -> int
 
 	while (true) {
 		ledkit.start(&LedKit::animation::sleeping);
+		rtos::ThisThread::sleep_for(10s);
 
-		rtos::ThisThread::sleep_for(3s);
+		ledkit.start(&LedKit::animation::sleeping);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::afraid_blue);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::afraid_red);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::afraid_red_blue);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::amazed);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::angry);
+		rtos::ThisThread::sleep_for(20s);
+
+		ledkit.start(&LedKit::animation::angry_short);
+		rtos::ThisThread::sleep_for(10s);
+
 		ledkit.start(&LedKit::animation::blink_green);
-
 		rtos::ThisThread::sleep_for(10s);
-		ledkit.start(&LedKit::animation::spin_blink);
 
+		ledkit.start(&LedKit::animation::bubbles);
 		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::disgusted);
+		rtos::ThisThread::sleep_for(10s);
+
 		ledkit.start(&LedKit::animation::fire);
-
 		rtos::ThisThread::sleep_for(10s);
-		ledkit.start(&LedKit::animation::sprinkles);
 
+		ledkit.start(&LedKit::animation::fly);
 		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::happy);
+		rtos::ThisThread::sleep_for(10s);
+
 		ledkit.start(&LedKit::animation::rainbow);
+		rtos::ThisThread::sleep_for(10s);
 
+		ledkit.start(&LedKit::animation::sad);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::sad_cry);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::sick);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::singing);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::sleeping);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::sneeze);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::spin_blink);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::sprinkles);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::underwater);
+		rtos::ThisThread::sleep_for(20s);
+
+		ledkit.start(&LedKit::animation::wake_up);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::wink);
+		rtos::ThisThread::sleep_for(10s);
+
+		ledkit.start(&LedKit::animation::yawn);
 		rtos::ThisThread::sleep_for(10s);
 	}
 }
