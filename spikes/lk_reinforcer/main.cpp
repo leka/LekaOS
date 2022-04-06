0d972cf1abd6fc92501b192a241374cf3442177b
Ladislas de Toldi <ladislas@detoldi.me>
2022-04-06 10:23:04 +0200

    :art: (spikes): Reinforcer/Led Animations - Refactor globals into namespace/anonymous namespace

--- spikes/lk_reinforcer/main.cpp
+++ spikes/lk_reinforcer/main.cpp
@@ -17,68 +17,96 @@
 using namespace leka;
 using namespace std::chrono;
 
+//
+// MARK: - Global definitions
+//
+
+namespace {
+
 namespace leds {
 
-namespace spi {
+	namespace internal {
 
-	auto belt = CoreSPI {LED_BELT_SPI_MOSI, NC, LED_BELT_SPI_SCK};
-	auto ears = CoreSPI {LED_EARS_SPI_MOSI, NC, LED_EARS_SPI_SCK};
+		namespace ears {
 
-}	// namespace spi
+			auto spi			= CoreSPI {LED_EARS_SPI_MOSI, NC, LED_EARS_SPI_SCK};
+			constexpr auto size = 2;
 
-namespace animations {
+		}	// namespace ears
 
-	auto thread		 = rtos::Thread {};
-	auto event_flags = CoreEventFlags {};
+		namespace belt {
 
-}	// namespace animations
+			auto spi			= CoreSPI {LED_BELT_SPI_MOSI, NC, LED_BELT_SPI_SCK};
+			constexpr auto size = 20;
 
-auto ears = CoreLED<LedKit::kNumberOfLedsEars> {spi::ears};
-auto belt = CoreLED<LedKit::kNumberOfLedsBelt> {spi::belt};
+		}	// namespace belt
+
+		namespace animations {
+
+			auto thread		 = rtos::Thread {};
+			auto event_flags = CoreEventFlags {};
+
+		}	// namespace animations
+
+	}	// namespace internal
+
+	auto ears = CoreLED<internal::ears::size> {internal::ears::spi};
+	auto belt = CoreLED<internal::belt::size> {internal::belt::spi};
+
+	auto kit = LedKit {internal::animations::thread, internal::animations::event_flags, ears, belt};
+
+	void turnOff()
+	{
+		ears.setColor(RGB::black);
+		belt.setColor(RGB::black);
+		ears.show();
+		belt.show();
+	}
 
 }	// namespace leds
 
-auto ledkit = LedKit {leds::animations::thread, leds::animations::event_flags, leds::ears, leds::belt};
+auto hello = HelloWorld {};
 
-HelloWorld hello;
+}	// namespace
 
 auto main() -> int
 {
 	logger::init();
+	leds::turnOff();
 
 	log_info("Hello, World!\n\n");
 
 	hello.start();
 
 	while (true) {
-		ledkit.start(&LedKit::animation::rainbow);
+		leds::kit.start(&LedKit::animation::rainbow);
 		rtos::ThisThread::sleep_for(10s);
 
-		ledkit.stop();
+		leds::kit.stop();
 		rtos::ThisThread::sleep_for(1s);
 
-		ledkit.start(&LedKit::animation::fire);
+		leds::kit.start(&LedKit::animation::fire);
 		rtos::ThisThread::sleep_for(10s);
 
-		ledkit.stop();
+		leds::kit.stop();
 		rtos::ThisThread::sleep_for(1s);
 
-		ledkit.start(&LedKit::animation::sprinkles);
+		leds::kit.start(&LedKit::animation::sprinkles);
 		rtos::ThisThread::sleep_for(5s);
 
-		ledkit.stop();
+		leds::kit.stop();
 		rtos::ThisThread::sleep_for(1s);
 
-		ledkit.start(&LedKit::animation::spin_blink);
+		leds::kit.start(&LedKit::animation::spin_blink);
 		rtos::ThisThread::sleep_for(40s);
 
-		ledkit.stop();
+		leds::kit.stop();
 		rtos::ThisThread::sleep_for(1s);
 
-		ledkit.start(&LedKit::animation::blink_green);
+		leds::kit.start(&LedKit::animation::blink_green);
 		rtos::ThisThread::sleep_for(40s);
 
-		ledkit.stop();
+		leds::kit.stop();
 		rtos::ThisThread::sleep_for(1s);
 	}
 }
