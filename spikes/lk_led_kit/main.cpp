23b710f30d0e2ce1d909ba43458b1e3737eeae94
Hugo Pezziardi <hugo@pezziardi.net>
2022-04-07 17:55:40 +0200

    :art: (spikes): Adapt spikes to ledkit changes

--- spikes/lk_led_kit/main.cpp
+++ spikes/lk_led_kit/main.cpp
@@ -7,31 +7,12 @@
 #include "drivers/HighResClock.h"
 #include "rtos/ThisThread.h"
 
+#include "CoreEventFlags.h"
 #include "CoreLED.h"
 #include "CoreSPI.h"
 #include "HelloWorld.h"
 #include "LedKit.h"
 #include "LogKit.h"
-#include "animations/AfraidBlue.h"
-#include "animations/AfraidRed.h"
-#include "animations/AfraidRedBlue.h"
-#include "animations/Amazed.h"
-#include "animations/Angry.h"
-#include "animations/AngryShort.h"
-#include "animations/Bubbles.h"
-#include "animations/Disgusted.h"
-#include "animations/Fly.h"
-#include "animations/Happy.h"
-#include "animations/Sad.h"
-#include "animations/SadCry.h"
-#include "animations/Sick.h"
-#include "animations/Singing.h"
-#include "animations/Sleeping.h"
-#include "animations/Sneeze.h"
-#include "animations/Underwater.h"
-#include "animations/WakeUp.h"
-#include "animations/Wink.h"
-#include "animations/Yawn.h"
 
 using namespace leka;
 using namespace std::chrono;
@@ -42,10 +23,10 @@ auto corespi_ears = CoreSPI {LED_EARS_SPI_MOSI, NC, LED_EARS_SPI_SCK};
 auto ears = CoreLED<LedKit::kNumberOfLedsEars> {corespi_ears};
 auto belt = CoreLED<LedKit::kNumberOfLedsBelt> {corespi_belt};
 
-auto animation_thread	   = rtos::Thread {};
-auto animation_event_queue = events::EventQueue {};
+auto animation_thread = rtos::Thread {};
+auto event_flags	  = CoreEventFlags {};
 
-auto ledkit = LedKit {animation_thread, animation_event_queue, ears, belt};
+auto ledkit = LedKit {animation_thread, event_flags, ears, belt};
 
 HelloWorld hello;
 
@@ -55,31 +36,28 @@ auto main() -> int
 
 	log_info("Hello, World!\n\n");
 
+	ledkit.init();
+
 	hello.start();
 
 	while (true) {
 		ledkit.start(&LedKit::animation::sleeping);
-		rtos::ThisThread::sleep_for(10s);
 
-		ledkit.stop();
-		rtos::ThisThread::sleep_for(1s);
-
-		ledkit.start(&LedKit::animation::sleeping);
-		rtos::ThisThread::sleep_for(10s);
-
-		ledkit.stop();
-		rtos::ThisThread::sleep_for(1s);
-
+		rtos::ThisThread::sleep_for(3s);
 		ledkit.start(&LedKit::animation::blink_green);
+
 		rtos::ThisThread::sleep_for(10s);
+		ledkit.start(&LedKit::animation::spin_blink);
 
-		ledkit.stop();
-		rtos::ThisThread::sleep_for(1s);
+		rtos::ThisThread::sleep_for(10s);
+		ledkit.start(&LedKit::animation::fire);
 
+		rtos::ThisThread::sleep_for(10s);
+		ledkit.start(&LedKit::animation::sprinkles);
+
+		rtos::ThisThread::sleep_for(10s);
 		ledkit.start(&LedKit::animation::rainbow);
-		rtos::ThisThread::sleep_for(10s);
 
-		ledkit.stop();
-		rtos::ThisThread::sleep_for(1s);
+		rtos::ThisThread::sleep_for(10s);
 	}
 }
