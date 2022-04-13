e89704c6ced9991ce593b66eaa68ac853a12d113
Hugo Pezziardi <hugo@pezziardi.net>
2022-04-13 17:10:11 +0200

    :sparkles: (CoreRFIDReader): Add CoreRFIDReader

---
+++ tests/unit/mocks/mocks/leka/CoreRFIDReader.h
@@ -0,0 +1,30 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.
+
+#pragma once
+
+#include <cstddef>
+
+#include "gmock/gmock.h"
+#include "interface/drivers/RFIDReader.h"
+
+namespace leka::mock {
+
+class CoreRFIDReader : public interface::RFIDReader
+{
+  public:
+	MOCK_METHOD(void, init, (), (override));
+	MOCK_METHOD(void, registerTagAvailableCallback, (tagAvailableCallback), (override));
+	MOCK_METHOD(void, onDataAvailable, (), (override));
+	MOCK_METHOD(bool, setBaudrate, (uint8_t), (override));
+	MOCK_METHOD(bool, setCommunicationProtocol, (rfid::Protocol), (override));
+	MOCK_METHOD(void, sendCommandToTag, (std::span<uint8_t>), (override));
+	MOCK_METHOD(bool, receiveDataFromTag, (std::span<uint8_t> data), (override));
+	MOCK_METHOD(void, setModeTagDetection, (), (override));
+	MOCK_METHOD(bool, checkForTagDetection, (), (override));
+};
+
+}	// namespace leka::mock
+
+// std::array<uint8_t, 32> _rx_buf {};
