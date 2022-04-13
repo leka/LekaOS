9f35dd60b75d29b123cc6fc7ea828428cb1b592e
Yann Locatelli <yann.locatelli2@gmail.com>
2022-04-13 16:05:13 +0200

    :zap: (ble): Add file reception in spike
    Use CircularQueue and CoreEventQueue to transfer data from BLE to SD on receiving file

--- spikes/lk_ble/main.cpp
+++ spikes/lk_ble/main.cpp
@@ -11,13 +11,22 @@
 #include "BLEServiceMonitoring.h"
 #include "BLEServiceUpdate.h"
 
+#include "CircularQueue.h"
+#include "CoreEventQueue.h"
+#include "FATFileSystem.h"
+#include "FileManagerKit.h"
 #include "LogKit.h"
+#include "SDBlockDevice.h"
 
 using namespace leka;
 using namespace std::chrono;
 
 auto level			 = uint8_t {0};
 auto charging_status = bool {false};
+
+auto file_reception_circular_queue = CircularQueue<uint8_t, 1024> {};
+auto write_buffer				   = std::array<uint8_t, 128> {};
+auto event_queue				   = CoreEventQueue {};
 
 auto service_device_information = BLEServiceDeviceInformation {};
 auto service_battery			= BLEServiceBattery {};
@@ -30,6 +39,20 @@ auto services = std::to_array<interface::BLEService *>(
 
 auto blekit = BLEKit {};
 
+SDBlockDevice sd_blockdevice(SD_SPI_MOSI, SD_SPI_MISO, SD_SPI_SCK);
+FATFileSystem fatfs("fs");
+FileManagerKit::File reception_file {};
+
+void initializeSD()
+{
+	constexpr auto default_sd_blockdevice_frequency = uint64_t {25'000'000};
+
+	sd_blockdevice.init();
+	sd_blockdevice.frequency(default_sd_blockdevice_frequency);
+
+	fatfs.mount(&sd_blockdevice);
+}
+
 auto main() -> int
 {
 	logger::init();
@@ -41,6 +64,32 @@ auto main() -> int
 	service_device_information.setSerialNumber(serial_number);
 
 	blekit.init();
+
+	initializeSD();
+
+	event_queue.dispatch_forever();
+
+	if (reception_file.open(service_file_reception.getFilePath().data(), "w")) {
+		reception_file.close();
+	}
+
+	auto on_file_data_write = []() {
+		auto path = service_file_reception.getFilePath();
+
+		if (reception_file.open(path.data(), "a")) {
+			write_buffer.fill('\0');
+			auto data_read = file_reception_circular_queue.pop(write_buffer.data(), std::size(write_buffer));
+			reception_file.write(write_buffer.data(), data_read);
+			reception_file.close();
+		}
+	};
+
+	auto on_file_data_received = [&on_file_data_write](std::span<uint8_t> buffer) {
+		file_reception_circular_queue.push(buffer.data(), std::size(buffer));
+
+		event_queue.call(on_file_data_write);
+	};
+	service_file_reception.onFileDataReceived(on_file_data_received);
 
 	while (true) {
 		log_info("Main thread running...");
