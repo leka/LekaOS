34f5f83c84fd57b92359864ebbe6e33fbf4ebfb5
Ladislas de Toldi <ladislas@detoldi.me>
2022-04-18 22:47:39 +0200

    :recycle: (command kit): Use new push, drop, hasPattern methods from CircularQueue

--- libs/CommandKit/source/CommandKit.cpp
+++ libs/CommandKit/source/CommandKit.cpp
@@ -12,7 +12,7 @@ CommandKit::CommandKit() = default;
 
 void CommandKit::push(std::span<uint8_t> data)
 {
-	_input_buffer.push(data.data(), data.size());
+	_input_buffer.push(data);
 
 	constexpr auto kMinCommandSize = 7;
 	if (_input_buffer.size() >= kMinCommandSize && _is_ready_to_process) {
@@ -36,15 +36,14 @@ void CommandKit::processCommands()
 
 	auto pos = 0U;
 
-	if (!_input_buffer.hasPattern(kStartPattern.data(), kStartPattern.size(), pos)) {
+	if (!_input_buffer.hasPattern(kStartPattern, pos)) {
 		_input_buffer.clear();
 		_is_ready_to_process = true;
 		return;
 	}
 
 	for (auto i = 0U; i < pos + kStartPattern.size(); ++i) {
-		auto _ = uint8_t {};
-		_input_buffer.pop(_);
+		_input_buffer.drop();
 	}
 
 	executeCommands();
