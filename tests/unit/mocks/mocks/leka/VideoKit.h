76be9e264208f9fc7c3bff956372c6235e23749f
Yann Locatelli <yann.locatelli2@gmail.com>
2022-04-26 18:05:01 +0200

    :clown_face: (video): Add VideoKit mock

---
+++ tests/unit/mocks/mocks/leka/VideoKit.h
@@ -0,0 +1,26 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "gmock/gmock.h"
+#include "interface/libs/VideoKit.h"
+
+namespace leka::mock {
+
+class VideoKit : public interface::VideoKit
+{
+  public:
+	MOCK_METHOD(void, initializeScreen, (), (override));
+
+	MOCK_METHOD(void, turnOn, (), (override));
+	MOCK_METHOD(void, turnOff, (), (override));
+
+	MOCK_METHOD(void, displayImage, (const std::filesystem::path &), (override));
+
+	MOCK_METHOD(void, playVideo, (const std::filesystem::path &, bool), (override));
+	MOCK_METHOD(void, stopVideo, (), (override));
+};
+
+}	// namespace leka::mock
