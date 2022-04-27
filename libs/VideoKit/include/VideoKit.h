7791261158da6c504ca361802b6217ee8ce87bcf
Yann Locatelli <yann.locatelli2@gmail.com>
2022-04-27 11:33:10 +0200

    :building_construction: (video): VideoKit inherits from interface::VideoKit

--- libs/VideoKit/include/VideoKit.h
+++ libs/VideoKit/include/VideoKit.h
@@ -4,24 +4,24 @@
 
 #pragma once
 
-#include <filesystem>
+#include "interface/libs/VideoKit.h"
 
 namespace leka {
 
-class VideoKit
+class VideoKit : public interface::VideoKit
 {
   public:
 	explicit VideoKit() = default;
 
-	void initializeScreen() {};
+	void initializeScreen() final;
 
-	void turnOn() {};
-	void turnOff() {};
+	void turnOn() final;
+	void turnOff() final;
 
-	void displayImage(const std::filesystem::path &path) {};
+	void displayImage(const std::filesystem::path &path) final;
 
-	void playVideo(const std::filesystem::path &path, bool must_loop = false) {};
-	void stopVideo() {};
+	void playVideo(const std::filesystem::path &path, bool must_loop = false) final;
+	void stopVideo() final;
 };
 
 }	// namespace leka
