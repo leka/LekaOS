23652b3750907f18fbe8709e78d07a24bf4e7ff8
Ladislas de Toldi <ladislas@detoldi.me>
2022-05-06 12:08:32 +0200

    :recycle: (interface): Create interface::DigitalOut alias
    Instead of using mbed::interface::DigitalOut direclty.

---
+++ include/interface/drivers/DigitalOut.h
@@ -0,0 +1,13 @@
+// Leka - LekaOS
+// Copyright 2020 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "drivers/interfaces/InterfaceDigitalOut.h"
+
+namespace leka::interface {
+
+using DigitalOut = mbed::interface::DigitalOut;
+
+}
