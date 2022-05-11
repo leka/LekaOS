665c227f377288e0256a30cef3444d216b666c87
Mourad Latoundji <mouradlatoundji@gmail.com>
2022-05-11 17:47:52 +0200

    :art: (drivers): Update CoreIOExpander

--- drivers/CoreIOExpander/include/CoreIOExpander.h
+++ drivers/CoreIOExpander/include/CoreIOExpander.h
@@ -15,31 +15,39 @@ namespace leka {
 class CoreIOExpanderMCP23017 : public interface::IOExpander<uint16_t>
 {
   public:
-	explicit CoreIOExpanderMCP23017(interface::I2C &i2c);
+	explicit CoreIOExpanderMCP23017(interface::I2C &i2c, mbed::DigitalOut &mux_reset);
 
 	void init();
 
-	void config(uint16_t dir_config, uint16_t pullup_config, uint16_t polarity_config);
+	void config_IODIR(uint16_t dir_config);
 
-	void writeOutputValue(uint16_t value);
+	void config_GPPU(uint16_t pullup_config);
+
+	void config_IPOL(uint16_t polarity_config);
+
+	void config_GPIO(uint16_t io_config);
 
 	void setPinAsInput(uint16_t pin) final;
 
-	void setPinAsOutput(uint16_t pin);
-
 	auto readInputPin(uint16_t pin) -> int final;
 
-	void setModeForPin(uint16_t pin, PinMode mode) final;
+	void setModeForInputPin(uint16_t pin, PinMode mode) final;
 
-	auto getModeForPin(uint16_t pin) -> PinMode final;
+	auto getModeForInputPin(uint16_t pin) -> PinMode final;
+
+	void setPinAsOutput(uint16_t pin) final;
+
+	auto readOutputPin(uint16_t pin) -> int final;
+
+	void writeOutputPin(uint16_t pin, int value) final;
 
   private:
 	void writeRegister(uint8_t reg, uint16_t value);
 	auto readRegister(uint8_t reg) -> uint16_t;
 
 	interface::I2C &_i2c;
+	mbed::DigitalOut &_mux_reset;
 	const uint8_t _I2C_ADDRESS = 0x4E;
 	uint16_t shadow_GPIO, shadow_IODIR, shadow_GPPU, shadow_IPOL;
-	mbed::DigitalOut _mux_reset;
 };
 }	// namespace leka
--- drivers/CoreIOExpander/source/CoreIOExpander.cpp
+++ drivers/CoreIOExpander/source/CoreIOExpander.cpp
@@ -4,23 +4,23 @@
 
 #include "CoreIOExpander.h"
 
-#include "PinNames.h"
-
 #include "span"
 
 using namespace leka;
 
-CoreIOExpanderMCP23017::CoreIOExpanderMCP23017(interface::I2C &i2c)
-	: _i2c(i2c), _mux_reset(SENSOR_PROXIMITY_MUX_RESET, 0)
+CoreIOExpanderMCP23017::CoreIOExpanderMCP23017(interface::I2C &i2c, mbed::DigitalOut &mux_reset)
+	: _i2c(i2c), _mux_reset(mux_reset)
 {
-	_mux_reset = 1;
 	init();
-	// config(0x3F00, 0x3F00, 0x0000);
-	// writeOutputValue(0x003F);
+	// config_IODIR(0x3F00);
+	// config_GPPU(0x3F00);
+	// config_GPIO(0x003F);
 };
 
 void CoreIOExpanderMCP23017::init()
 {
+	_mux_reset = 1;
+
 	writeRegister(static_cast<uint8_t>(0x05), static_cast<uint16_t>(0x0000));
 
 	writeRegister(mcp23017::registers::IODIR, static_cast<uint16_t>(0xFFFF));
@@ -35,14 +35,28 @@ void CoreIOExpanderMCP23017::init()
 	shadow_IPOL	 = 0;
 }
 
-void CoreIOExpanderMCP23017::config(uint16_t dir_config, uint16_t pullup_config, uint16_t polarity_config)
+void CoreIOExpanderMCP23017::config_IODIR(uint16_t dir_config)
 {
 	shadow_IODIR = dir_config;
 	writeRegister(mcp23017::registers::IODIR, static_cast<uint16_t>(shadow_IODIR));
+}
+
+void CoreIOExpanderMCP23017::config_GPPU(uint16_t pullup_config)
+{
 	shadow_GPPU = pullup_config;
 	writeRegister(mcp23017::registers::GPPU, static_cast<uint16_t>(shadow_GPPU));
+}
+
+void CoreIOExpanderMCP23017::config_IPOL(uint16_t polarity_config)
+{
 	shadow_IPOL = polarity_config;
 	writeRegister(mcp23017::registers::IPOL, static_cast<uint16_t>(shadow_IPOL));
+}
+
+void CoreIOExpanderMCP23017::config_GPIO(uint16_t io_config)
+{
+	shadow_GPIO = io_config;
+	writeRegister(mcp23017::registers::GPIO, static_cast<uint16_t>(shadow_GPIO));
 }
 
 void CoreIOExpanderMCP23017::setPinAsInput(uint16_t pin)
@@ -52,28 +66,17 @@ void CoreIOExpanderMCP23017::setPinAsInput(uint16_t pin)
 	writeRegister(mcp23017::registers::IODIR, static_cast<uint16_t>(shadow_IODIR));
 }
 
-void CoreIOExpanderMCP23017::setPinAsOutput(uint16_t pin)
-{
-	shadow_IODIR = readRegister(mcp23017::registers::IODIR);
-	shadow_IODIR &= ~(pin);
-	writeRegister(mcp23017::registers::IODIR, static_cast<uint16_t>(shadow_IODIR));
-}
-
 auto CoreIOExpanderMCP23017::readInputPin(uint16_t pin) -> int
 {
-	shadow_GPIO = readRegister(mcp23017::registers::GPIO);
-	auto value	= static_cast<int>(shadow_GPIO & (pin));
-
-	return value;
+	if ((shadow_IODIR & pin) != 0) {
+		shadow_GPIO = readRegister(mcp23017::registers::GPIO);
+		auto value	= static_cast<int>(shadow_GPIO & (pin));
+		return value;
+	}
+	return 0;
 }
 
-void CoreIOExpanderMCP23017::writeOutputValue(uint16_t value)
-{
-	shadow_GPIO = value;
-	writeRegister(mcp23017::registers::GPIO, static_cast<uint16_t>(shadow_GPIO));
-}
-
-void CoreIOExpanderMCP23017::setModeForPin(uint16_t pin, PinMode mode)
+void CoreIOExpanderMCP23017::setModeForInputPin(uint16_t pin, PinMode mode)
 {
 	if ((shadow_IODIR & pin) != 0) {
 		auto shadow_GPPU = readRegister(mcp23017::registers::GPPU);
@@ -91,15 +94,52 @@ void CoreIOExpanderMCP23017::setModeForPin(uint16_t pin, PinMode mode)
 	}
 }
 
-auto CoreIOExpanderMCP23017::getModeForPin(uint16_t pin) -> PinMode
+auto CoreIOExpanderMCP23017::getModeForInputPin(uint16_t pin) -> PinMode
 {
-	shadow_GPPU = readRegister(mcp23017::registers::GPPU);
-	auto mode	= static_cast<int>(shadow_GPPU & (pin));
+	if ((shadow_IODIR & pin) != 0) {
+		shadow_GPPU = readRegister(mcp23017::registers::GPPU);
+		auto mode	= static_cast<int>(shadow_GPPU & (pin));
 
-	if (mode != 0) {
-		return PinMode::PullUp;
+		if (mode != 0) {
+			return PinMode::PullUp;
+		}
 	}
 	return PinMode::PullNone;
+}
+
+void CoreIOExpanderMCP23017::setPinAsOutput(uint16_t pin)
+{
+	shadow_IODIR = readRegister(mcp23017::registers::IODIR);
+	shadow_IODIR &= ~(pin);
+	writeRegister(mcp23017::registers::IODIR, static_cast<uint16_t>(shadow_IODIR));
+}
+
+auto CoreIOExpanderMCP23017::readOutputPin(uint16_t pin) -> int
+{
+	if ((shadow_IODIR & pin) == 0) {
+		shadow_GPIO = readRegister(mcp23017::registers::GPIO);
+		auto value	= static_cast<int>(shadow_GPIO & (pin));
+		return value;
+	}
+	return 0;
+}
+
+void CoreIOExpanderMCP23017::writeOutputPin(uint16_t pin, int value)
+{
+	if ((shadow_IODIR & pin) == 0) {
+		shadow_GPIO = readRegister(mcp23017::registers::GPIO);
+		switch (value) {
+			case 0x01:
+				shadow_GPIO |= (pin);
+				break;
+			case 0x00:
+				shadow_GPIO &= ~(pin);
+				break;
+			default:
+				break;
+		}
+		writeRegister(mcp23017::registers::GPIO, static_cast<uint16_t>(shadow_GPIO));
+	}
 }
 
 void CoreIOExpanderMCP23017::writeRegister(uint8_t reg, uint16_t value)
@@ -116,10 +156,7 @@ auto CoreIOExpanderMCP23017::readRegister(uint8_t reg) -> uint16_t
 	auto addr	= std::array<uint8_t, 1> {reg};
 	auto buffer = std::array<uint8_t, 2> {};
 
-	/**
-	 * TODO: Add Mutex Lock and Unlock
-	 *
-	 */
+	// TODO(mourad): Add Mutex Lock and Unlock ?
 
 	_i2c.write(_I2C_ADDRESS, addr.data(), addr.size(), true);
 	_i2c.read(_I2C_ADDRESS, buffer.data(), buffer.size(), false);
