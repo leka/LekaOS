07da07677833afd1da0700e104fcc525d00fd64f
Yann Locatelli <yann.locatelli2@gmail.com>
2022-05-21 00:38:09 +0200

    :sparkles: (ble): Add OS version characteristic
    In DeviceInformation service - Software characteristic

--- libs/BLEKit/include/BLEServiceDeviceInformation.h
+++ libs/BLEKit/include/BLEServiceDeviceInformation.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "CastUtils.h"
+#include "interface/drivers/FirmwareVersion.h"
 #include "internal/BLEService.h"
 #include "internal/ServicesCharacteristics.h"
 
@@ -20,6 +21,17 @@ class BLEServiceDeviceInformation : public interface::BLEService
 		std::copy(std::begin(value), std::begin(value) + std::size(_serial_number), _serial_number.begin());
 
 		auto data = std::make_tuple(_serial_number_characteristic.getValueHandle(), _serial_number);
+		sendData(data);
+	}
+
+	void setOSVersion(const FirmwareVersion &version) const
+	{
+		_os_version.fill('\0');
+		auto version_cstr = version.asStdArray();
+
+		std::copy(std::begin(version_cstr), std::begin(version_cstr) + std::size(_os_version), _os_version.begin());
+
+		auto data = std::make_tuple(_os_version_characteristic.getValueHandle(), _os_version);
 		sendData(data);
 	}
 
@@ -47,8 +59,15 @@ class BLEServiceDeviceInformation : public interface::BLEService
 		service::device_information::characteristic::serial_number, _serial_number.data(), _serial_number.size(),
 		_serial_number.size(), GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ};
 
-	std::array<GattCharacteristic *, 3> _characteristic_table {
-		&_manufacturer_name_characteristic, &_model_number_characteristic, &_serial_number_characteristic};
+	inline static auto _os_version = utils::cast::from_c_string_to_uint8_t_array("000.000.00000");
+
+	GattCharacteristic _os_version_characteristic {service::device_information::characteristic::os_version,
+												   _os_version.data(), _os_version.size(), _os_version.size(),
+												   GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ};
+
+	std::array<GattCharacteristic *, 4> _characteristic_table {
+		&_manufacturer_name_characteristic, &_model_number_characteristic, &_serial_number_characteristic,
+		&_os_version_characteristic};
 };
 
 }	// namespace leka
