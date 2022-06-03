c90a4e712e36189f0d83a95816f1ec1e82f5091d
Yann Locatelli <yann.locatelli2@gmail.com>
2022-06-04 01:26:34 +0200

    :sparkles: (assets): Add spike lk_lcd_serial
    Display image or Play video for a given absolute path with Serial communication

---
+++ spikes/lk_lcd_serial/SerialCommunication.h
@@ -0,0 +1,32 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <string>
+
+#include "PinNames.h"
+
+#include "drivers/BufferedSerial.h"
+#include "rtos/EventFlags.h"
+
+#define NEW_MESSAGE (1UL << 10)
+
+class SerialCommunication
+{
+  public:
+	explicit SerialCommunication(rtos::EventFlags &event_flags) : _event_flags(event_flags) {}
+
+	void checkNewMessage();
+
+	[[nodiscard]] auto getMessage() -> std::string &;
+	void sendMessage(const std::string_view &message);
+
+  private:
+	rtos::EventFlags &_event_flags;
+
+	mbed::BufferedSerial _serial {CONSOLE_TX, CONSOLE_RX, 115200};
+
+	std::string _reception_string;
+};
