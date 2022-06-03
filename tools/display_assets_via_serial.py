0dd7cd2839615d8f9243799fdb1f2794ff110775
Yann Locatelli <yann.locatelli2@gmail.com>
2022-06-04 01:27:54 +0200

    :hammer: (assets): Add script to send images or videos paths to board

---
+++ tools/display_assets_via_serial.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import os
+import sys, getopt
+from time import sleep
+from serial import Serial
+
+
+### CONFIGURATIONS
+serial_port = '/dev/tty.usbmodem14103'
+
+source_directory = 'fs/home/img' # Relative path from LekaOS root
+
+interval_between_two_response = 1 # in seconds
+delay_to_reboot = 5 # in seconds
+
+start_from_index = 0
+delete_file_occuring_reboot = False
+
+
+
+def printHelp():
+	print ("./tools/display_image_via_serial.py\n\t"
+		"--help\n\t"
+		"--serial_port <serial_port>\n\t"
+		"--are_images\n\t"
+		"--are_videos\n\t"
+		"--source_directory <source_directory>\n\t"
+		"--interval_between_two_response <interval_between_two_response>,\n\t"
+		"--delay_to_reboot <delay_to_reboot>,\n\t"
+		"--start_from_index <start_from_index>\n\t"
+		"--delete_file_occuring_reboot\n\t")
+
+argv = sys.argv[1:]
+try:
+	opts, args = getopt.getopt(argv,"",["help",
+										"serial_port=",
+										"are_images",
+										"are_video",
+										"source_directory=",
+										"interval_between_two_response=",
+										"delay_to_reboot=",
+										"start_from_index=",
+										"delete_file_occuring_reboot"])
+except getopt.GetoptError:
+	printHelp()
+	sys.exit(2)
+for opt, arg in opts:
+	if opt in ("--help"):
+		printHelp()
+		sys.exit()
+	elif opt in ("--serial_port"):
+		serial_port = arg
+	elif opt in ("--are_images"):
+		source_directory = "fs/home/img"
+	elif opt in ("--are_videos"):
+		source_directory = "fs/home/vid"
+	elif opt in ("--source_directory"):
+		source_directory = arg
+	elif opt in ("--interval_between_two_response"):
+		interval_between_two_response = float(arg)
+	elif opt in ("--delay_to_reboot"):
+		delay_to_reboot = float(arg)
+	elif opt in ("--start_from_index"):
+		start_from_index = int(arg)
+	elif opt in ("--delete_file_occuring_reboot"):
+		delete_file_occuring_reboot = True
+
+
+
+### SETUP
+## Serial communication
+com_timeout = .1
+com = Serial(serial_port, 115200, timeout=com_timeout)
+line = ''
+iter_without_response = 0
+no_response_limit = delay_to_reboot / com_timeout
+
+## Image files
+destination_directory = '/' + source_directory # Absolute path from SD root
+
+index = start_from_index
+image_files = os.listdir(source_directory)
+image_files.sort()
+
+
+
+def getLine():
+	return com.readline().decode("utf-8")
+
+def sendLine(filename):
+	full_path = destination_directory + '/' + filename
+	print(full_path)
+	com.write(full_path.encode("utf-8"))
+
+def waitResponse():
+	global line
+	global iter_without_response
+	line = ''
+	iter_without_response = 0
+	while(not(line) and (iter_without_response < no_response_limit)):
+		sleep(.005)
+		line = getLine()
+		iter_without_response += 1
+
+def responseIsValid():
+	return line.find('ACK') != -1
+
+def needReboot():
+	must_reboot = iter_without_response >= no_response_limit
+	if(must_reboot):
+		com.send_break()
+		sleep(5)
+		return True
+
+def deleteFile(filename):
+	os.remove(source_directory + '/' + filename)
+
+
+
+while((index < len(image_files))):
+	print(f"{index + 1:04d} / {len(image_files):04d} : ", end='')
+	sendLine(image_files[index])
+
+	waitResponse()
+
+	if(needReboot()):
+		if(delete_file_occuring_reboot):
+			deleteFile(image_files[index])
+		continue
+	if(responseIsValid()):
+		index += 1
+	sleep(interval_between_two_response)
