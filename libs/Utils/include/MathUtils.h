e8b8993a9b6cf9ec093ec1294ec6e49757d5f96d
Hugo Pezziardi <hugo@pezziardi.net>
2022-06-21 10:50:43 +0200

    :sparkles: (math utils): Add sinPeriod function

--- libs/Utils/include/MathUtils.h
+++ libs/Utils/include/MathUtils.h
@@ -4,7 +4,9 @@
 
 #pragma once
 
+#include <cmath>
 #include <cstdint>
+#include <numbers>
 #include <span>
 
 namespace leka::utils::math {
@@ -30,5 +32,14 @@ auto computeLinearCoefficients(Point p1, Point p2) -> LinearCoefficients;
 auto checksum8(std::span<uint8_t> data) -> uint8_t;
 
 [[nodiscard]] auto random8(uint8_t min, uint8_t max) -> uint8_t;
+
+template <typename OutputType>
+auto sinPeriod(uint8_t period, uint8_t step, uint8_t offset, OutputType out_min, OutputType out_max) -> float
+{
+	auto pi			   = std::numbers::pi_v<float>;
+	auto mapped_offset = math::map(offset, uint8_t {0}, uint8_t {255}, 0.F, 2.F * pi);
+	auto sinus = static_cast<float>(std::sin(2 * pi * (1.F / static_cast<float>(period)) * step + mapped_offset));
+	return (out_max - out_min) * (sinus + 1.F) / 2.F;
+}
 
 }	// namespace leka::utils::math
