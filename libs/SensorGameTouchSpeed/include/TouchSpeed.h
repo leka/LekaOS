3c2e0bc0738af4ea27d6d4cc0b3940a084d55a14
Sibylle Malonga <malongasibylle@gmail.com>
2022-07-07 00:04:11 +0200

    :sparkles: Add lib GameTouchSpeed

---
+++ libs/SensorGameTouchSpeed/include/TouchSpeed.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "RGB.h"
+namespace leka {
+class TouchSpeed
+{
+  public:
+	void HelloWorld();
+
+	auto is_target_touched() -> bool;
+
+	auto setRandomCaptorWithColors() -> void;
+
+	auto update_touched_colors() -> void;
+	auto setColorWinIfWon(leka::RGB) -> void;
+	auto setColorLoseIfLose(leka::RGB) -> void;
+
+	auto playOneRound(int maximum_duration) -> bool;
+	void MaxDurationForRound();
+	auto isMaxDurationRound() -> bool;
+	auto setFlags() -> void;
+	auto isTargetHit() -> bool;
+	auto isTargetHitBeforeTimesUp() -> bool;
+
+	void playLevel(int level);
+	void isNextLevelReached();
+
+	void game();
+};
+}	// namespace leka
---
+++ libs/SensorGameTouchSpeed/source/TouchSpeed.cpp
@@ -0,0 +1,28 @@
+#include "TouchSpeed.h"
+
+#include "ColorKit.h"
+#include "TouchSensorKit.h"
+namespace leka {
+
+void TouchSpeed::HelloWorld() {}
+
+auto TouchSpeed::is_target_touched() -> bool {}
+
+auto TouchSpeed::setRandomCaptorWithColors() -> void {}
+
+auto TouchSpeed::update_touched_colors() -> void {}
+auto TouchSpeed::setColorWinIfWon(leka::RGB) -> void {}
+auto TouchSpeed::setColorLoseIfLose(leka::RGB) -> void {}
+
+auto TouchSpeed::playOneRound(int maximum_duration) -> bool {}
+void TouchSpeed::MaxDurationForRound() {}
+auto TouchSpeed::isMaxDurationRound() -> bool {}
+auto TouchSpeed::setFlags() -> void {}
+auto isTargetHit() -> bool {}
+auto isTargetHitBeforeTimesUp() -> bool;
+
+void TouchSpeed::playLevel(int level) {};
+
+void TouchSpeed::game() {};
+void TouchSpeed::isNextLevelReached() {};
+}	// namespace leka
