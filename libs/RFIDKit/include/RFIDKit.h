5f3c43327970b22be86140cc0bcfc8acd1d6ffcc
Hugo Pezziardi <hugo@pezziardi.net>
2022-07-12 14:22:09 +0200

    :sparkles: (libs): Clean up & refactor RFIDKit

--- libs/RFIDKit/include/RFIDKit.h
+++ libs/RFIDKit/include/RFIDKit.h
@@ -4,53 +4,60 @@
 
 #pragma once
 
-#include <cstddef>
-#include <span>
+#include <boost/sml.hpp>
 
+#include "ISO14443A.h"
 #include "MagicCard.h"
 #include "interface/drivers/RFIDReader.h"
 
 namespace leka {
 
-enum class state : uint8_t
-{
-
-	WAITING_FOR_TAG				   = 0x00,
-	TAG_COMMUNICATION_PROTOCOL_SET = 0x01,
-	WAIT_FOR_ATQA_RESPONSE		   = 0x02,
-	TAG_IDENTIFIED				   = 0x03,
-	TAG_AVAILABLE				   = 0x04,
-
-};
-
-class RFIDKit : public interface::RFIDReader::ISO14443
+class RFIDKit
 {
   public:
 	explicit RFIDKit(interface::RFIDReader &rfid_reader) : _rfid_reader(rfid_reader) {};
 
-	void init() final;
+	void init()
+	{
+		using namespace rfid::sm;
 
-	void runStateMachine() final;
+		static auto tagDetectedCallback = [this]() { state_machine.process_event(event::tag_detected {}); };
 
-	void onTagActivated(std::function<void(MagicCard &)> callback);
+		_rfid_reader.registerOnTagDetectedCallback(tagDetectedCallback);
+		registerMagicCard();
+
+		_rfid_reader.init();
+		_rfid_reader.setTagDetectionMode();
+	}
+
+	void registerMagicCard()
+	{
+		auto on_magic_card_readable_callback = [this]() {
+			rfid::Tag tag = _rfid_reader.getTagData();
+			if (isTagSignatureValid(tag)) {
+				_card = MagicCard {tag.data[5]};
+
+				_on_tag_available_callback(_card);
+			}
+		};
+		_rfid_reader.registerOnTagValidCallback(on_magic_card_readable_callback);
+	}
+
+	auto isTagSignatureValid(rfid::Tag tag) -> bool
+	{
+		return (tag.data[0] == leka_tag_header[0] && tag.data[1] == leka_tag_header[1] &&
+				tag.data[2] == leka_tag_header[2] && tag.data[3] == leka_tag_header[3]);
+	}
+
+	void onTagActivated(std::function<void(MagicCard &_card)> callback) { _on_tag_available_callback = callback; }
 
   private:
-	void sendREQA();
-	void sendReadRegister0();
-	void sendReadRegister4();
-
-	auto isTagSignatureValid() -> bool;
-
-	auto receiveATQA() -> bool;
-	auto receiveReadTagData() -> bool;
-
-	auto computeCRC(uint8_t const *data) const -> std::array<uint8_t, 2>;
-
 	interface::RFIDReader &_rfid_reader;
-	rfid::Tag _tag {};
 	MagicCard _card = MagicCard::none;
 	std::function<void(MagicCard &)> _on_tag_available_callback;
-	state _state = state::WAITING_FOR_TAG;
+	boost::sml::sm<rfid::ISO14443A> state_machine {_rfid_reader};
+
+	static constexpr std::array<uint8_t, 4> leka_tag_header = {0x4C, 0x45, 0x4B, 0x41};
 };
 
 }	// namespace leka
