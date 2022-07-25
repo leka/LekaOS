a2ae137bda0910e50e0a0aa5ad517d9796186b91
Mourad Latoundji <mouradlatoundji@gmail.com>
2022-07-25 21:53:27 +0200

    :art: (libs): Update TouchSensorKit

--- libs/TouchSensorKit/source/TouchSensorKit.cpp
+++ libs/TouchSensorKit/source/TouchSensorKit.cpp
@@ -11,59 +11,102 @@ using namespace std::chrono_literals;
 
 void TouchSensorKit::init()
 {
-	for (Position position: positions) {
-		setPowerMode(position, PowerMode::normal);
+	for (Sensor sensor: _sensors) {
+		sensor.component.init();
+	}
+}
+
+void TouchSensorKit::start()
+{
+	for (Sensor sensor: _sensors) {
+		sensor.component.read();
+	}
+}
+
+void TouchSensorKit::run()
+{
+	for (Sensor sensor: _sensors) {
+		sensor.component.read();
+	}
+}
+void TouchSensorKit::stop()
+{
+	for (Sensor sensor: _sensors) {
+		sensor.component.read();
 	}
 }
 
 auto TouchSensorKit::isTouched(Position position) -> bool
 {
 	const auto pos = static_cast<uint8_t>(position);
-
-	auto read = (1 == _sensors.at(pos).read());
-	if (read && _on_sensor_touched_callback != nullptr) {
-		_on_sensor_touched_callback();
+	for (Sensor sensor: _sensors) {
+		if (pos == sensor.position) {
+			auto lastState	  = _sensor.state;
+			auto currentState = (_sensor.read());
+			if (!lastState && currentState) {
+				if (_on_sensor_touched_callback != nullptr) {
+					_on_sensor_touched_callback(position);
+				}
+				return true
+			}
+		}
 	}
-	return read;
-}
-
-auto TouchSensorKit::isTouchedAny() -> bool
-{
-	for (Position position: positions) {
-		if (isTouched(position)) {
-			return true;
-		};
-	};
 	return false;
 }
 
-void TouchSensorKit::setPowerMode(Position position, PowerMode power_mode)
+auto TouchSensorKit::isReleased(Position position) -> bool
 {
 	const auto pos = static_cast<uint8_t>(position);
-	_sensors.at(pos).setPowerMode(power_mode);
+	for (Sensor sensor: _sensors) {
+		if (pos == sensor.position) {
+			auto lastState	  = _sensor.state;
+			auto currentState = (_sensor.read());
+			if (lastState && !currentState) {
+				if (_on_sensor_touched_callback != nullptr) {
+					_on_sensor_touched_callback(position);
+				}
+				return true
+			}
+		}
+	}
+	return false;
 }
 
-void TouchSensorKit::resetByPowerMode()
+void TouchSensorKit::calibrate(Position position) {}
+
+void TouchSensorKit::read(uint8_t position)
 {
-	for (Position position: positions) {
-		if (isTouched(position)) {
-			setPowerMode(position, PowerMode::low);
-			rtos::ThisThread::sleep_for(20ms);
-			setPowerMode(position, PowerMode::normal);
-			rtos::ThisThread::sleep_for(20ms);
+	for (Sensor sensor: _sensors) {
+		if (pos == sensor.position) {
+			_sensors.state = _sensors.component.read();
 		}
 	}
 }
 
-void TouchSensorKit::setSensitivity(Position position, uint16_t value, bool saved)
+void TouchSensorKit::reset(uint8_t position)
 {
-	const auto pos = static_cast<uint8_t>(position);
-	_sensors.at(pos).setSensitivity(value, saved);
-
-	rtos::ThisThread::sleep_for(10ms);
+	for (Sensor sensor: _sensors) {
+		if (pos == sensor.position) {
+			_sensors.state = _sensors.component.reset();
+		}
+	}
 }
 
-void TouchSensorKit::registerOnSensorTouched(std::function<void()> const &on_sensor_touched_callback)
+void TouchSensorKit::setSensitivity(uint8_t position, uint16_t value, bool saved)
+{
+	for (Sensor sensor: _sensors) {
+		if (pos == sensor.position) {
+			_sensors.state = _sensors.component.setSensitivity(value, saved);
+		}
+	}
+}
+
+void TouchSensorKit::registerOnSensorTouched(std::function<void(Position &)> const &on_sensor_touched_callback)
 {
 	_on_sensor_touched_callback = on_sensor_touched_callback;
+}
+
+void TouchSensorKit::registerOnSensorReleased(std::function<void(Position &)> const &on_sensor_released_callback)
+{
+	_on_sensor_released_callback = on_sensor_released_callback;
 }
