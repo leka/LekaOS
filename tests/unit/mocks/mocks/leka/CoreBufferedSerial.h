8bdc5e67515ea3956fa57382cb28d76628b2694b
Hugo Pezziardi <hugo@pezziardi.net>
2022-07-26 17:25:01 +0200

    :clown_face: (mock): CoreBufferedSerial - Fix mock return type

--- tests/unit/mocks/mocks/leka/CoreBufferedSerial.h
+++ tests/unit/mocks/mocks/leka/CoreBufferedSerial.h
@@ -12,8 +12,8 @@ namespace leka::mock {
 class CoreBufferedSerial : public interface::BufferedSerial
 {
   public:
-	MOCK_METHOD(ssize_t, read, (uint8_t *, std::size_t), (override));
-	MOCK_METHOD(ssize_t, write, (const uint8_t *, std::size_t), (override));
+	MOCK_METHOD(std::size_t, read, (uint8_t *, std::size_t), (override));
+	MOCK_METHOD(std::size_t, write, (const uint8_t *, std::size_t), (override));
 	MOCK_METHOD(bool, readable, (), (override));
 	MOCK_METHOD(void, sigio, (mbed::Callback<void()>), (override));
 };
