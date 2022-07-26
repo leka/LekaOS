9123763a1767c55476dc6df4e55e7ad5b179008f
Hugo Pezziardi <hugo@pezziardi.net>
2022-07-26 15:58:35 +0200

    :white_check_mark: (tests): Add ISO14443A tests

---
+++ libs/RFIDKit/tests/ISO14443A_test.cpp
@@ -0,0 +1,140 @@
+// Leka - LekaOS
+// Copyright 2021 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ISO14443A.h"
+
+#include "gtest/gtest.h"
+#include "mocks/leka/CoreRFIDReader.h"
+
+using namespace leka;
+using namespace rfid::sm;
+
+using ::testing::Return;
+using ::testing::ReturnPointee;
+using ::testing::ReturnRef;
+
+class ISO14443ATest : public ::testing::Test
+{
+  protected:
+	// void SetUp() override {}
+	// void TearDown() override {}
+
+	mock::CoreRFIDReader mock_reader {};
+	boost::sml::sm<rfid::ISO14443A, boost::sml::testing> sm {static_cast<interface::RFIDReader &>(mock_reader)};
+
+	static constexpr rfid::Command<1> command_requestA		  = {.data = {0x26}, .flags = leka::rfid::Flag::sb_7};
+	static constexpr rfid::Command<2> command_read_register_4 = {
+		.data = {0x30, 0x04}, .flags = leka::rfid::Flag::crc | leka::rfid::Flag::sb_8};
+
+	std::function<void()> magic_card_callback {};
+};
+
+TEST_F(ISO14443ATest, initialization)
+{
+	ASSERT_NE(&sm, nullptr);
+}
+
+TEST_F(ISO14443ATest, initialState)
+{
+	EXPECT_TRUE(sm.is(state::idle));
+	EXPECT_FALSE(sm.is(state::send_reqa));
+	EXPECT_FALSE(sm.is(state::requesting_atqa));
+	EXPECT_FALSE(sm.is(state::requesting_tag_data));
+}
+
+TEST_F(ISO14443ATest, stateIdleEventTagDetectedGuardIsTagDetectedTrue)
+{
+	sm.set_current_states(state::idle);
+
+	EXPECT_CALL(mock_reader, setCommunicationProtocol).Times(1);
+	EXPECT_CALL(mock_reader, isTagDetected).Times(1).WillOnce(Return(true));
+
+	sm.process_event(event::tag_detected {});
+
+	EXPECT_TRUE(sm.is(state::send_reqa));
+}
+
+TEST_F(ISO14443ATest, stateIdleEventTagDetectedGuardIsTagDetectedFalse)
+{
+	sm.set_current_states(state::idle);
+
+	EXPECT_CALL(mock_reader, setTagDetectionMode).Times(1);
+	EXPECT_CALL(mock_reader, isTagDetected).Times(2).WillRepeatedly(Return(false));
+
+	sm.process_event(event::tag_detected {});
+
+	EXPECT_TRUE(sm.is(state::idle));
+}
+
+TEST_F(ISO14443ATest, stateSendReqaEventTagDetected)
+{
+	sm.set_current_states(state::send_reqa);
+
+	EXPECT_CALL(mock_reader, sendToTag);
+
+	sm.process_event(event::tag_detected {});
+
+	EXPECT_TRUE(sm.is(state::requesting_atqa));
+}
+
+TEST_F(ISO14443ATest, stateRequestingAtqaEventTagDetectedGuardIsTrue)
+{
+	sm.set_current_states(state::requesting_atqa);
+
+	constexpr std::array<uint8_t, rfid::ATQA_answer_size> expected_ATQA_answer {0x44, 0x00};
+	constexpr std::array<uint8_t, rfid::register_answer_size> data {
+		0x44, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+	EXPECT_CALL(mock_reader, didTagCommunicationSucceed).Times(1).WillOnce(Return(true));
+	EXPECT_CALL(mock_reader, getDataFromTag).WillOnce(Return(data));
+	EXPECT_CALL(mock_reader, sendToTag).Times(1);
+
+	sm.process_event(event::tag_detected {});
+
+	EXPECT_TRUE(sm.is(state::requesting_tag_data));
+}
+
+TEST_F(ISO14443ATest, stateRequestingAtqaEventTagDetectedGuardIsFalse)
+{
+	sm.set_current_states(state::requesting_atqa);
+
+	EXPECT_CALL(mock_reader, didTagCommunicationSucceed).Times(2).WillRepeatedly(Return(false));
+	EXPECT_CALL(mock_reader, setTagDetectionMode).Times(1);
+
+	sm.process_event(event::tag_detected {});
+
+	EXPECT_TRUE(sm.is(state::idle));
+}
+
+TEST_F(ISO14443ATest, stateRequestingTagDataEventTagDetectedGuardIsTrue)
+{
+	sm.set_current_states(state::requesting_tag_data);
+
+	constexpr size_t register_answer_size = 18;
+
+	std::array<uint8_t, 18> data = {0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01,
+									0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x24, 0xF5};
+
+	EXPECT_CALL(mock_reader, didTagCommunicationSucceed).Times(1).WillOnce(Return(true));
+
+	EXPECT_CALL(mock_reader, getDataFromTag).WillOnce(Return(data));
+	EXPECT_CALL(mock_reader, onTagDataReceived).Times(1);
+	EXPECT_CALL(mock_reader, setTagDetectionMode).Times(1);
+
+	sm.process_event(event::tag_detected {});
+
+	EXPECT_TRUE(sm.is(state::idle));
+}
+
+TEST_F(ISO14443ATest, stateRequestingTagDataEventTagDetectedGuardIsFalse)
+{
+	sm.set_current_states(state::requesting_tag_data);
+
+	EXPECT_CALL(mock_reader, didTagCommunicationSucceed).Times(2).WillRepeatedly(Return(false));
+	EXPECT_CALL(mock_reader, setTagDetectionMode).Times(1);
+
+	sm.process_event(event::tag_detected {});
+
+	EXPECT_TRUE(sm.is(state::idle));
+}
