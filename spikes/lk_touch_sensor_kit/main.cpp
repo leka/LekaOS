a37d455e81e9ce4f8464895bfa791e1215697b40
Mourad Latoundji <mouradlatoundji@gmail.com>
2022-07-27 17:38:20 +0200

    :art: (spike): Update lk_touch_sensor_kit

--- spikes/lk_touch_sensor_kit/main.cpp
+++ spikes/lk_touch_sensor_kit/main.cpp
@@ -5,6 +5,7 @@
 #include "drivers/BufferedSerial.h"
 #include "rtos/ThisThread.h"
 
+#include "CoreTouchSensor.h"
 #include "HelloWorld.h"
 #include "Leds.h"
 #include "LogKit.h"
@@ -19,7 +20,7 @@ struct Ear {
 	uint8_t index;
 };
 
-auto ears = std::array<Ear, 2> {Ear {Position::ear_left, 1}, Ear {Position::ear_right, 0}};
+auto ears = std::array<Ear, 2> {Ear {Position::ear_right, 0}, Ear {Position::ear_left, 1}};
 
 struct Belt {
 	Position position;
@@ -27,8 +28,8 @@ struct Belt {
 	uint8_t end_index;
 };
 
-auto belt = std::array<Belt, 4> {Belt {Position::belt_left_back, 16, 18}, Belt {Position::belt_left_front, 10, 13},
-								 Belt {Position::belt_right_back, 6, 9}, Belt {Position::belt_right_front, 1, 3}};
+auto belt = std::array<Belt, 4> {Belt {Position::belt_left_front, 10, 13}, Belt {Position::belt_left_back, 16, 18},
+								 Belt {Position::belt_right_front, 1, 3}, Belt {Position::belt_right_back, 6, 9}};
 
 void setColorEar(Ear ear, RGB color)
 {
@@ -41,31 +42,148 @@ void setColorBelt(Belt belt, RGB color)
 	leds::belt.setColorRange(belt.start_index, belt.end_index, color);
 	leds::belt.show();
 }
-}	// namespace TouchLed
 
-auto touch_sensor_kit = TouchSensorKit();
-
-void printState()
+void turnOnLeds(Position position)
 {
-	log_info("Ear left touched: %s", touch_sensor_kit.isTouched(Position::ear_left) ? "true" : "false");
-	log_info("Ear right touched: %s", touch_sensor_kit.isTouched(Position::ear_right) ? "true" : "false");
-	log_info("Belt left front touched: %s", touch_sensor_kit.isTouched(Position::belt_left_front) ? "true" : "false");
-	log_info("Belt left back touched: %s", touch_sensor_kit.isTouched(Position::belt_left_back) ? "true" : "false");
-	log_info("Belt right front touched: %s", touch_sensor_kit.isTouched(Position::belt_right_front) ? "true" : "false");
-	log_info("Belt right back touched: %s", touch_sensor_kit.isTouched(Position::belt_left_back) ? "true" : "false");
-	log_info("\n\n");
+	for (Ear sensor: ears) {
+		if (sensor.position == position) {
+			TouchLed::setColorEar(sensor, RGB::pure_blue);
+		}
+	}
+	for (Belt sensor: belt) {
+		if (sensor.position == position) {
+			TouchLed::setColorBelt(sensor, RGB::pure_blue);
+		}
+	}
 }
 
-void updateLeds()
+void turnOffLeds(Position position)
 {
-	for (TouchLed::Ear touchled: TouchLed::ears) {
-		touch_sensor_kit.isTouched(touchled.position) ? TouchLed::setColorEar(touchled, RGB::pure_blue)
-													  : TouchLed::setColorEar(touchled, RGB::black);
+	for (Ear sensor: ears) {
+		if (sensor.position == position) {
+			TouchLed::setColorEar(sensor, RGB::black);
+		}
 	}
-	for (TouchLed::Belt touchled: TouchLed::belt) {
-		touch_sensor_kit.isTouched(touchled.position) ? TouchLed::setColorBelt(touchled, RGB::pure_blue)
-													  : TouchLed::setColorBelt(touchled, RGB::black);
+	for (Belt sensor: belt) {
+		if (sensor.position == position) {
+			TouchLed::setColorBelt(sensor, RGB::black);
+		}
 	}
+}
+
+void turnOnAllLeds()
+{
+	for (Ear sensor: ears) {
+		TouchLed::setColorEar(sensor, RGB::pure_blue);
+	}
+	for (Belt sensor: belt) {
+		TouchLed::setColorBelt(sensor, RGB::pure_blue);
+	}
+}
+void turnOffAllLeds()
+{
+	for (Ear sensor: ears) {
+		TouchLed::setColorEar(sensor, RGB::black);
+	}
+	for (Belt sensor: belt) {
+		TouchLed::setColorBelt(sensor, RGB::black);
+	}
+}
+
+}	// namespace TouchLed
+
+auto event_loop		  = EventLoopKit {};
+auto touch_sensor_kit = TouchSensorKit {event_loop,
+										touch::sensor::ear_left,
+										touch::sensor::ear_right,
+										touch::sensor::belt_left_front,
+										touch::sensor::belt_left_back,
+										touch::sensor::belt_right_front,
+										touch::sensor::belt_left_back};
+
+void printSensorTouched(Position position)
+{
+	switch (position) {
+		case Position::ear_left:
+			log_info("Ear left is touched");
+			break;
+		case Position::ear_right:
+			log_info("Ear right is touched");
+			break;
+		case Position::belt_left_front:
+			log_info("Belt left front is touched");
+			break;
+		case Position::belt_left_back:
+			log_info("Belt left back is touched");
+			break;
+		case Position::belt_right_front:
+			log_info("Belt right front is touched");
+			break;
+		case Position::belt_right_back:
+			log_info("Belt right back is touched");
+			break;
+		default:
+			break;
+	}
+	log_info("\n\n");
+	rtos::ThisThread::sleep_for(1s);
+}
+
+void printSensorReleased(Position position)
+{
+	switch (position) {
+		case Position::ear_left:
+			log_info("Ear left is released");
+			break;
+		case Position::ear_right:
+			log_info("Ear right is released");
+			break;
+		case Position::belt_left_front:
+			log_info("Belt left front is released");
+			break;
+		case Position::belt_left_back:
+			log_info("Belt left back is released");
+			break;
+		case Position::belt_right_front:
+			log_info("Belt right front is released");
+			break;
+		case Position::belt_right_back:
+			log_info("Belt right back is released");
+			break;
+	}
+	log_info("\n\n");
+	rtos::ThisThread::sleep_for(1s);
+}
+
+void calibration()
+{
+	TouchLed::turnOffAllLeds();
+
+	log_info("Put your hand on ear left sensor");
+	touch_sensor_kit.calibrate(Position::ear_left);
+	setColorEar(TouchLed::ears.at(1), RGB::pure_red);
+
+	log_info("Put your hand on ear right sensor");
+	touch_sensor_kit.calibrate(Position::ear_right);
+	setColorEar(TouchLed::ears.at(0), RGB::pure_red);
+
+	log_info("Put your hand on belt left front sensor");
+	touch_sensor_kit.calibrate(Position::belt_left_front);
+	setColorBelt(TouchLed::belt.at(0), RGB::pure_red);
+
+	log_info("Put your hand on belt left back sensor");
+	touch_sensor_kit.calibrate(Position::belt_left_back);
+	setColorBelt(TouchLed::belt.at(1), RGB::pure_red);
+
+	log_info("Put your hand on belt right front sensor");
+	touch_sensor_kit.calibrate(Position::belt_right_front);
+	setColorBelt(TouchLed::belt.at(2), RGB::pure_red);
+
+	log_info("Put your hand on belt right back sensor");
+	touch_sensor_kit.calibrate(Position::belt_right_back);
+	setColorBelt(TouchLed::belt.at(3), RGB::pure_red);
+
+	TouchLed::turnOffAllLeds();
 }
 
 auto main() -> int
@@ -77,8 +195,6 @@ auto main() -> int
 
 	log_info("Hello, World!\n\n");
 
-	touch_sensor_kit.init();
-
 	auto start = rtos::Kernel::Clock::now();
 
 	rtos::ThisThread::sleep_for(2s);
@@ -86,22 +202,23 @@ auto main() -> int
 	auto t = rtos::Kernel::Clock::now() - start;
 	log_info("A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME, hello.world,
 			 int(t.count() / 1000));
-	for (Position position: positions) {
-		touch_sensor_kit.setSensitivity(position, 0x000F, true);
-	}
+
 	rtos::ThisThread::sleep_for(2s);
 
-	auto updateLeds_func = [&]() { updateLeds(); };
-	auto printState_func = [&]() { printState(); };
+	TouchLed::turnOffAllLeds();
 
-	touch_sensor_kit.resetByPowerMode();
-	// touch_sensor_kit.registerOnSensorTouched(printState_func);
+	touch_sensor_kit.init();
+	calibration();
 
-	rtos::ThisThread::sleep_for(500ms);
+	auto turnOnLeds_func  = [&](Position position) { TouchLed::turnOnLeds(position); };
+	auto turnOffLeds_func = [&](Position position) { TouchLed::turnOffLeds(position); };
+
+	touch_sensor_kit.registerOnSensorTouched(turnOnLeds_func);
+	touch_sensor_kit.registerOnSensorReleased(turnOffLeds_func);
+
+	touch_sensor_kit.start();
 
 	while (true) {
-		updateLeds();
-		touch_sensor_kit.resetByPowerMode();
-		rtos::ThisThread::sleep_for(1s);
+		rtos::ThisThread::sleep_for(100ms);
 	}
 }
