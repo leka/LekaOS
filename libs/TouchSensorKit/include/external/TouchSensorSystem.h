81c7fcdddbb5488989ec06f6f3dc254a9d7b4090
Mourad Latoundji <mouradlatoundji@gmail.com>
2022-07-28 11:49:04 +0200

    :art: (libs): Update TouchSensorKit

--- libs/TouchSensorKit/include/external/TouchSensorSystem.h
+++ libs/TouchSensorKit/include/external/TouchSensorSystem.h
@@ -12,65 +12,57 @@
 #include "IOKit/DigitalIn.h"
 #include "IOKit/DigitalOut.h"
 
-namespace leka {
+namespace leka::touch {
 
-inline auto corei2c			  = CoreI2C {PinName::SENSOR_PROXIMITY_MUX_I2C_SDA, PinName::SENSOR_PROXIMITY_MUX_I2C_SCL};
-inline auto io_expander_reset = mbed::DigitalOut {PinName::SENSOR_PROXIMITY_MUX_RESET, 0};
-inline auto io_expander		  = CoreIOExpanderMCP23017 {corei2c, io_expander_reset};
+// inline auto corei2c			  = CoreI2C {PinName::SENSOR_PROXIMITY_MUX_I2C_SDA,
+// PinName::SENSOR_PROXIMITY_MUX_I2C_SCL}; inline auto io_expander_reset = mbed::DigitalOut
+// {PinName::SENSOR_PROXIMITY_MUX_RESET, 0}; inline auto io_expander		  = CoreIOExpanderMCP23017 {corei2c,
+// io_expander_reset};
 
-namespace detect_pin {
-	inline auto ear_left		 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB5};
-	inline auto ear_right		 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB4};
-	inline auto belt_left_back	 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB3};
-	inline auto belt_left_front	 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB2};
-	inline auto belt_right_back	 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB1};
-	inline auto belt_right_front = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB0};
-}	// namespace detect_pin
+// namespace detect_pin {
+// 	inline auto ear_left		 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB5};
+// 	inline auto ear_right		 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB4};
+// 	inline auto belt_left_back	 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB3};
+// 	inline auto belt_left_front	 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB2};
+// 	inline auto belt_right_back	 = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB1};
+// 	inline auto belt_right_front = io::expanded::DigitalIn<> {io_expander, mcp23017::pin::PB0};
+// }	// namespace detect_pin
 
-namespace power_mode_pin {
-	inline auto ear_left		 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA5};
-	inline auto ear_right		 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA4};
-	inline auto belt_left_back	 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA3};
-	inline auto belt_left_front	 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA2};
-	inline auto belt_right_back	 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA1};
-	inline auto belt_right_front = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA0};
-}	// namespace power_mode_pin
+// namespace power_mode_pin {
+// 	inline auto ear_left		 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA5};
+// 	inline auto ear_right		 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA4};
+// 	inline auto belt_left_back	 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA3};
+// 	inline auto belt_left_front	 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA2};
+// 	inline auto belt_right_back	 = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA1};
+// 	inline auto belt_right_front = io::expanded::DigitalOut<> {io_expander, mcp23017::pin::PA0};
+// }	// namespace power_mode_pin
 
-namespace dac {
-	inline auto left  = CoreQDACMCP4728 {corei2c, 0xC0};
-	inline auto right = CoreQDACMCP4728 {corei2c, 0xC2};
-	namespace channel {
-		inline auto ear_left		 = Channel::C;
-		inline auto ear_right		 = Channel::C;
-		inline auto belt_left_back	 = Channel::B;
-		inline auto belt_left_front	 = Channel::A;
-		inline auto belt_right_back	 = Channel::B;
-		inline auto belt_right_front = Channel::A;
-	}	// namespace channel
-}	// namespace dac
+// namespace dac {
+// 	inline auto left  = CoreQDACMCP4728 {corei2c, 0xC0};
+// 	inline auto right = CoreQDACMCP4728 {corei2c, 0xC2};
+// 	namespace channel {
+// 		inline auto ear_left		 = Channel::C;
+// 		inline auto ear_right		 = Channel::C;
+// 		inline auto belt_left_back	 = Channel::B;
+// 		inline auto belt_left_front	 = Channel::A;
+// 		inline auto belt_right_back	 = Channel::B;
+// 		inline auto belt_right_front = Channel::A;
+// 	}	// namespace channel
+// }	// namespace dac
 
-namespace sensor {
-	inline auto ear_left =
-		CoreTouchSensor {detect_pin::ear_left, power_mode_pin::ear_left, dac::left, dac::channel::ear_left};
-	inline auto ear_right =
-		CoreTouchSensor {detect_pin::ear_right, power_mode_pin::ear_right, dac::right, dac::channel::ear_right};
-	inline auto belt_left_back = CoreTouchSensor {detect_pin::belt_left_back, power_mode_pin::belt_left_back, dac::left,
-												  dac::channel::belt_left_back};
-	inline auto belt_left_front	 = CoreTouchSensor {detect_pin::belt_left_front, power_mode_pin::belt_left_front,
-													dac::left, dac::channel::belt_left_front};
-	inline auto belt_right_back	 = CoreTouchSensor {detect_pin::belt_right_back, power_mode_pin::belt_right_back,
-													dac::right, dac::channel::belt_right_back};
-	inline auto belt_right_front = CoreTouchSensor {detect_pin::belt_right_front, power_mode_pin::belt_right_front,
-													dac::right, dac::channel::belt_right_front};
-}	// namespace sensor
+// namespace sensor {
+// 	inline auto ear_left =
+// 		CoreTouchSensor {detect_pin::ear_left, power_mode_pin::ear_left, dac::left, dac::channel::ear_left};
+// 	inline auto ear_right =
+// 		CoreTouchSensor {detect_pin::ear_right, power_mode_pin::ear_right, dac::right, dac::channel::ear_right};
+// 	inline auto belt_left_back = CoreTouchSensor {detect_pin::belt_left_back, power_mode_pin::belt_left_back, dac::left,
+// 												  dac::channel::belt_left_back};
+// 	inline auto belt_left_front	 = CoreTouchSensor {detect_pin::belt_left_front, power_mode_pin::belt_left_front,
+// 													dac::left, dac::channel::belt_left_front};
+// 	inline auto belt_right_back	 = CoreTouchSensor {detect_pin::belt_right_back, power_mode_pin::belt_right_back,
+// 													dac::right, dac::channel::belt_right_back};
+// 	inline auto belt_right_front = CoreTouchSensor {detect_pin::belt_right_front, power_mode_pin::belt_right_front,
+// 													dac::right, dac::channel::belt_right_front};
+// }	// namespace sensor
 
-namespace position {
-	inline constexpr auto ear_left		   = uint8_t {0};
-	inline constexpr auto ear_right		   = uint8_t {1};
-	inline constexpr auto belt_left_back   = uint8_t {2};
-	inline constexpr auto belt_left_front  = uint8_t {3};
-	inline constexpr auto belt_right_back  = uint8_t {4};
-	inline constexpr auto belt_right_front = uint8_t {5};
-}	// namespace position
-
-}	// namespace leka
+}	// namespace leka::touch
--- libs/TouchSensorKit/source/TouchSensorKit.cpp
+++ libs/TouchSensorKit/source/TouchSensorKit.cpp
@@ -11,10 +11,13 @@ using namespace std::chrono_literals;
 
 void TouchSensorKit::init()
 {
-	for (Position position: _positions) {
-		init(position);
-	}
 	_event_loop.registerCallback([this] { run(); });
+	_ear_left.init();
+	_ear_right.init();
+	_belt_left_back.init();
+	_belt_left_front.init();
+	_belt_right_back.init();
+	_belt_right_front.init();
 }
 
 void TouchSensorKit::start()
@@ -25,94 +28,117 @@ void TouchSensorKit::start()
 
 void TouchSensorKit::run()
 {
-	for (Position position: _positions) {
-		updateState(position);
+	auto states		   = std::array<bool, kNumberOfSensors> {};
+	auto previousState = bool {};
+	auto currentState  = bool {};
+	while (true) {
+		for (Position position: _positions) {
+			currentState = readAtPosition(position);
+
+			if (!previousState && currentState && _on_sensor_touched_callback != nullptr) {
+				_on_sensor_touched_callback(position);
+			}
+			if (previousState && !currentState && _on_sensor_released_callback != nullptr) {
+				_on_sensor_released_callback(position);
+			}
+
+			previousState = currentState;
+		}
+		rtos::ThisThread::sleep_for(100ms);
 	}
-	rtos::ThisThread::sleep_for(100ms);
 }
 void TouchSensorKit::stop()
 {
 	_event_loop.stop();
 }
 
-void TouchSensorKit::updateState(Position position)
-{
-	const auto index = static_cast<size_t>(position);
-
-	auto previousState = bool {};
-	auto currentState  = bool {};
-
-	previousState	 = _state.at(index);
-	_state.at(index) = read(position);
-	currentState	 = _state.at(index);
-
-	if (!previousState && currentState && _on_sensor_touched_callback != nullptr) {
-		_on_sensor_touched_callback(position);
-	}
-	if (previousState && !currentState && _on_sensor_released_callback != nullptr) {
-		_on_sensor_released_callback(position);
-	}
-}
-
-void TouchSensorKit::calibrate(Position position)
-{
-	auto value = uint16_t {CoreTouchSensor::default_min_sensitivity_value};
-	auto step  = uint16_t {10};
-
-	constexpr auto accurate_read_count = uint8_t {10};
-	auto read_count					   = uint8_t {0};
-
-	setSensitivity(position, value);
-
-	while (read_count < accurate_read_count && value <= CoreTouchSensor::default_max_sensitivity_value) {
-		if (read(position)) {
-			read_count = 0;
-			value += step;
-			setSensitivity(position, value);
-			log_info(" Value : %d\n\n", value);
-
-		} else {
-			read_count++;
-		}
-		rtos::ThisThread::sleep_for(100ms);
-	}
-	setSensitivity(position, value, true);
-}
-
-void TouchSensorKit::registerOnSensorTouched(std::function<void(Position &)> const &on_sensor_touched_callback)
+void TouchSensorKit::registerOnSensorTouched(std::function<void(Position)> const &on_sensor_touched_callback)
 {
 	_on_sensor_touched_callback = on_sensor_touched_callback;
 }
 
-void TouchSensorKit::registerOnSensorReleased(std::function<void(Position &)> const &on_sensor_released_callback)
+void TouchSensorKit::registerOnSensorReleased(std::function<void(Position)> const &on_sensor_released_callback)
 {
 	_on_sensor_released_callback = on_sensor_released_callback;
 }
 
-void TouchSensorKit::init(Position position)
+void TouchSensorKit::updateStateAtPosition(Position position)
 {
-	const auto index = static_cast<size_t>(position);
-
-	_sensors.at(index)->init();
+	switch (position) {
+		case Position::ear_left:
+			_state.at(static_cast<size_t>(position)) = _ear_left.read();
+			break;
+		case Position::ear_right:
+			_state.at(static_cast<size_t>(position)) = _ear_right.read();
+			break;
+		case Position::belt_left_back:
+			_state.at(static_cast<size_t>(position)) = _belt_left_back.read();
+			break;
+		case Position::belt_left_front:
+			_state.at(static_cast<size_t>(position)) = _belt_left_front.read();
+			break;
+		case Position::belt_right_back:
+			_state.at(static_cast<size_t>(position)) = _belt_right_back.read();
+			break;
+		case Position::belt_right_front:
+			_state.at(static_cast<size_t>(position)) = _belt_right_front.read();
+			break;
+	}
 }
 
-auto TouchSensorKit::read(Position position) -> bool
+auto TouchSensorKit::geAtPosition(Position position) -> bool
 {
-	const auto index = static_cast<size_t>(position);
-
-	return _sensors.at(index)->read();
+	return _state.at(static_cast<size_t>(position));
 }
 
-void TouchSensorKit::reset(Position position)
+void TouchSensorKit::resetAtPosition(Position position)
 {
-	const auto index = static_cast<size_t>(position);
-
-	_sensors.at(index)->reset();
+	switch (position) {
+		case Position::ear_left:
+			_ear_left.reset();
+			break;
+		case Position::ear_right:
+			_ear_right.reset();
+			break;
+		case Position::belt_left_back:
+			_belt_left_back.reset();
+			break;
+		case Position::belt_left_front:
+			_belt_left_front.reset();
+			break;
+		case Position::belt_right_back:
+			_belt_right_back.reset();
+			break;
+		case Position::belt_right_front:
+			_belt_right_front.reset();
+			break;
+		default:
+			break;
+	}
 }
 
-void TouchSensorKit::setSensitivity(Position position, uint16_t value, bool saved)
+void TouchSensorKit::setSensitivityAtPosition(Position position, uint16_t value, bool saved)
 {
-	const auto index = static_cast<size_t>(position);
-
-	_sensors.at(index)->setSensitivity(value, saved);
+	switch (position) {
+		case Position::ear_left:
+			_ear_left.setSensitivity(value, saved);
+			break;
+		case Position::ear_right:
+			_ear_right.setSensitivity(value, saved);
+			break;
+		case Position::belt_left_back:
+			_belt_left_back.setSensitivity(value, saved);
+			break;
+		case Position::belt_left_front:
+			_belt_left_front.setSensitivity(value, saved);
+			break;
+		case Position::belt_right_back:
+			_belt_right_back.setSensitivity(value, saved);
+			break;
+		case Position::belt_right_front:
+			_belt_right_front.setSensitivity(value, saved);
+			break;
+		default:
+			break;
+	}
 }
