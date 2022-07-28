04d42ace9869b74538fb6654fa8741d35cb62d84
Mourad Latoundji <mouradlatoundji@gmail.com>
2022-07-28 11:49:04 +0200

    :art: (libs): Update TouchSensorKit

--- libs/TouchSensorKit/include/TouchSensorKit.h
+++ libs/TouchSensorKit/include/TouchSensorKit.h
@@ -13,12 +13,12 @@ namespace leka {
 
 enum class Position : uint8_t
 {
-	ear_left		 = position::ear_left,
-	ear_right		 = position::ear_right,
-	belt_left_back	 = position::belt_left_back,
-	belt_left_front	 = position::belt_left_front,
-	belt_right_back	 = position::belt_right_back,
-	belt_right_front = position::belt_right_front,
+	ear_left,
+	ear_right,
+	belt_left_back,
+	belt_left_front,
+	belt_right_back,
+	belt_right_front,
 };
 
 class TouchSensorKit
@@ -26,39 +26,49 @@ class TouchSensorKit
   public:
 	static constexpr auto kNumberOfSensors = uint8_t {6};
 
-	explicit TouchSensorKit(interface::EventLoop &event_loop,
-							std::array<interface::TouchSensor *, kNumberOfSensors> sensors)
-		: _event_loop(event_loop), _sensors(sensors) {};
+	explicit TouchSensorKit(interface::EventLoop &event_loop, interface::TouchSensor &ear_left,
+							interface::TouchSensor &ear_right, interface::TouchSensor &belt_left_back,
+							interface::TouchSensor &belt_left_front, interface::TouchSensor &belt_right_back,
+							interface::TouchSensor &belt_right_front)
+		: _event_loop(event_loop),
+		  _ear_left(ear_left),
+		  _ear_right(ear_right),
+		  _belt_left_back(belt_left_back),
+		  _belt_left_front(belt_left_front),
+		  _belt_right_back(belt_right_back),
+		  _belt_right_front(belt_right_front) {};
 
 	void init();
 	void start();
 	void run();
 	void stop();
 
-	void updateState(Position position);
-
-	void calibrate(Position position);
-
-	void registerOnSensorTouched(std::function<void(Position &)> const &on_sensor_touched_callback);
-	void registerOnSensorReleased(std::function<void(Position &)> const &on_sensor_released_callback);
+	void registerOnSensorTouched(std::function<void(Position)> const &on_sensor_touched_callback);
+	void registerOnSensorReleased(std::function<void(Position)> const &on_sensor_released_callback);
 
   private:
-	void initTouch(Position position);
-	auto read(Position position) -> bool;
-	void reset(Position position);
-	void setSensitivity(Position position, uint16_t value, bool saved = false);
+	auto readAtPosition(Position position) -> bool;
+	void resetAtPosition(Position position);
+	void setSensitivityAtPosition(Position position, uint16_t value, bool saved = false);
+	void calibrateAtPosition(Position position);
 
 	static constexpr std::array<Position, kNumberOfSensors> _positions {
 		Position::ear_left,		   Position::ear_right,		  Position::belt_left_back,
-		Position::belt_left_front, Position::belt_right_back, Position::belt_right_front,
-	};
+		Position::belt_left_front, Position::belt_right_back, Position::belt_right_front};
 
 	interface::EventLoop &_event_loop;
 
-	std::array<interface::TouchSensor *, kNumberOfSensors> _sensors;
-	std::array<bool, kNumberOfSensors> _state {};
+	interface::TouchSensor &_ear_left;
+	interface::TouchSensor &_ear_right;
+	interface::TouchSensor &_belt_left_back;
+	interface::TouchSensor &_belt_left_front;
+	interface::TouchSensor &_belt_right_back;
+	interface::TouchSensor &_belt_right_front;
 
-	std::function<void(Position &)> _on_sensor_touched_callback {};
-	std::function<void(Position &)> _on_sensor_released_callback {};
+	static constexpr auto default_max_sensitivity_value = uint16_t {0x0FF0};
+	static constexpr auto default_min_sensitivity_value = uint16_t {0x000F};
+
+	std::function<void(Position)> _on_sensor_touched_callback {};
+	std::function<void(Position)> _on_sensor_released_callback {};
 };
 }	// namespace leka
--- libs/TouchSensorKit/tests/TouchSensorKit_test.cpp
+++ libs/TouchSensorKit/tests/TouchSensorKit_test.cpp
@@ -5,17 +5,39 @@
 #include "TouchSensorKit.h"
 
 #include "gtest/gtest.h"
+#include "mocks/leka/CoreTouchSensor.h"
+#include "stubs/leka/EventLoopKit.h"
 
 using namespace leka;
-// using ::testing::_;
 
 class TouchSensorTest : public ::testing::Test
 {
   protected:
+	TouchSensorTest() = default;
+
 	// void SetUp() override {}
 	// void TearDown() override {}
 
-	TouchSensorKit touch_sensor_kit = TouchSensorKit();
+	mock::CoreTouchSensor mock_ear_left;
+	mock::CoreTouchSensor mock_ear_right;
+	mock::CoreTouchSensor mock_belt_left_front;
+	mock::CoreTouchSensor mock_belt_left_back;
+	mock::CoreTouchSensor mock_belt_right_front;
+	mock::CoreTouchSensor mock_belt_right_back;
+
+	stub::EventLoopKit stub_event_loop {};
+
+	TouchSensorKit touch_sensor_kit {stub_event_loop,	   mock_ear_left,		mock_ear_right,
+									 mock_belt_left_front, mock_belt_left_back, mock_belt_right_front,
+									 mock_belt_right_back};
+
+	static void spy_onSensorTouched_func(Position position) {};
+	std::function<void(Position)> OnSensorTouchedCallback = spy_onSensorTouched_func;
+	static void spy_onSensorReleased_func(Position position) {};
+	std::function<void(Position)> OnSensorReleasedCallback = spy_onSensorReleased_func;
+
+	static void func(Position position) {};
+	std::function<void(Position)> callbackFunction = func;
 };
 
 TEST_F(TouchSensorTest, initializationDefault)
@@ -25,34 +47,44 @@ TEST_F(TouchSensorTest, initializationDefault)
 
 TEST_F(TouchSensorTest, init)
 {
+	EXPECT_CALL(mock_ear_left, init).Times(1);
+	EXPECT_CALL(mock_ear_right, init).Times(1);
+	EXPECT_CALL(mock_belt_left_front, init).Times(1);
+	EXPECT_CALL(mock_belt_left_back, init).Times(1);
+	EXPECT_CALL(mock_belt_right_front, init).Times(1);
+	EXPECT_CALL(mock_belt_right_back, init).Times(1);
+
 	touch_sensor_kit.init();
 }
 
 TEST_F(TouchSensorTest, start)
 {
-	touch_sensor_kit.updateState();
-}
-
-TEST_F(TouchSensorTest, run)
-{
-	touch_sensor_kit.updateState();
+	touch_sensor_kit.start();
 }
 
 TEST_F(TouchSensorTest, stop)
 {
-	touch_sensor_kit.updateState();
+	touch_sensor_kit.stop();
 }
-TEST_F(TouchSensorTest, start)
+TEST_F(TouchSensorTest, calibrate)
 {
-	touch_sensor_kit.updateState();
+	const auto default_max_sensitivity_value = uint16_t {0x0FF0};
+	const auto default_min_sensitivity_value = uint16_t {0x000F};
+	const auto step							 = uint16_t {10};
+
+	const auto count = static_cast<uint16_t>((default_max_sensitivity_value - default_min_sensitivity_value) / step);
+
+	EXPECT_CALL(mock_ear_left, read).Times(count + 1);
+	EXPECT_CALL(mock_ear_left, setSensitivity).Times(count + 3);
+	touch_sensor_kit.calibrate(Position::ear_left);
 }
 
-TEST_F(TouchSensorTest, isTouched)
+TEST_F(TouchSensorTest, registerOnSensorTouched)
 {
-	touch_sensor_kit.isTouched(Position::ear_left);
+	touch_sensor_kit.registerOnSensorTouched(OnSensorTouchedCallback);
 }
 
-TEST_F(TouchSensorTest, isReleased)
+TEST_F(TouchSensorTest, registerOnSensorReleased)
 {
-	touch_sensor_kit.isReleased(Position::ear_left);
+	touch_sensor_kit.registerOnSensorReleased(OnSensorReleasedCallback);
 }
