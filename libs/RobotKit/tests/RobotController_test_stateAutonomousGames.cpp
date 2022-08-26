c0ee17880462b13e8c644b8a0e3f4aca21fd15a9
Hugo Pezziardi <hugo@pezziardi.net>
2022-08-26 14:36:38 +0200

    :white_check_mark: (tests): Update tests RC & SM

--- libs/RobotKit/tests/RobotController_test_stateAutonomousGames.cpp
+++ libs/RobotKit/tests/RobotController_test_stateAutonomousGames.cpp
@@ -5,11 +5,16 @@
 #include "./RobotController_test.h"
 #include "SMAutonomousGame.h"
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedConnectedEventCommandReceivedIsChargingFalse)
+TEST_F(RobotControllerTest, stateGameConnectedEventCommandReceivedIsChargingFalse)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped, lksm::state::connected);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>, lksm::state::connected);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(false));
+
+	Sequence on_game_exit_sequence;
+	EXPECT_CALL(mock_videokit, stopVideo).InSequence(on_game_exit_sequence);
+	EXPECT_CALL(mock_motor_left, stop).InSequence(on_game_exit_sequence);
+	EXPECT_CALL(mock_motor_right, stop).InSequence(on_game_exit_sequence);
 
 	Sequence on_working_entry_sequence;
 	EXPECT_CALL(timeout, onTimeout).InSequence(on_working_entry_sequence);
@@ -18,38 +23,36 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedConnectedEventCommandReceivedIs
 	EXPECT_CALL(mock_lcd, turnOn).InSequence(on_working_entry_sequence);
 
 	// TODO: Specify which BLE service and what is expected if necessary
-	EXPECT_CALL(mbed_mock_gatt, write(_, _, _, _));
+	EXPECT_CALL(mbed_mock_gatt, write(_, _, _, _)).Times(AtLeast(1));
 
 	rc.state_machine.process_event(lksm::event::command_received {});
 
 	EXPECT_TRUE(rc.state_machine.is(lksm::state::working, lksm::state::connected));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedDisconnectedEventCommandReceivedIsChargingFalse)
+TEST_F(RobotControllerTest, stateGameDisconnectedEventCommandReceivedIsChargingFalse)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped, lksm::state::disconnected);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>, lksm::state::disconnected);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(false));
 
 	// TODO: Specify which BLE service and what is expected if necessary
-	EXPECT_CALL(mbed_mock_gatt, write(_, _, _, _)).Times(AnyNumber());
+	EXPECT_CALL(mbed_mock_gatt, write(_, _, _, _)).Times(AtLeast(1));
 
 	rc.state_machine.process_event(lksm::event::command_received {});
 
-	EXPECT_TRUE(rc.state_machine.is(lksm::state::emergency_stopped, lksm::state::disconnected));
+	EXPECT_TRUE(rc.state_machine.is(boost::sml::state<game::SMAutonomousGame>, lksm::state::disconnected));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedEventBleConnectionGuardIsNotCharging)
+TEST_F(RobotControllerTest, stateGameEventBleConnectionGuardIsNotCharging)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped, lksm::state::disconnected);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>, lksm::state::disconnected);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(false));
 
-	EXPECT_CALL(mock_videokit, stopVideo).Times(2);
-	EXPECT_CALL(mock_motor_left, stop).Times(2);
-	EXPECT_CALL(mock_motor_right, stop).Times(2);
-	EXPECT_CALL(mock_ears, hide).Times(1);
-	EXPECT_CALL(mock_belt, hide).Times(1);
+	EXPECT_CALL(mock_videokit, stopVideo).Times(AtLeast(1));
+	EXPECT_CALL(mock_motor_left, stop).Times(AtLeast(1));
+	EXPECT_CALL(mock_motor_right, stop).Times(AtLeast(1));
 
 	EXPECT_CALL(mock_belt, setColor).Times(AtLeast(1));
 	EXPECT_CALL(mock_belt, show).Times(AtLeast(1));
@@ -69,11 +72,16 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedEventBleConnectionGuardIsNotCha
 	EXPECT_TRUE(rc.state_machine.is(lksm::state::working, lksm::state::connected));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedEventChargeDidStartGuardIsChargingTrue)
+TEST_F(RobotControllerTest, stateGameEventChargeDidStartGuardIsChargingTrue)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(true));
+
+	Sequence on_game_exit_sequence;
+	EXPECT_CALL(mock_videokit, stopVideo).InSequence(on_game_exit_sequence);
+	EXPECT_CALL(mock_motor_left, stop).InSequence(on_game_exit_sequence);
+	EXPECT_CALL(mock_motor_right, stop).InSequence(on_game_exit_sequence);
 
 	Sequence start_charging_behavior_sequence;
 	EXPECT_CALL(battery, level).InSequence(start_charging_behavior_sequence);
@@ -90,9 +98,9 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedEventChargeDidStartGuardIsCharg
 	EXPECT_TRUE(rc.state_machine.is(lksm::state::charging));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedEventChargeDidStartGuardIsChargingFalse)
+TEST_F(RobotControllerTest, stateGameEventChargeDidStartGuardIsChargingFalse)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(false));
 
@@ -101,14 +109,19 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedEventChargeDidStartGuardIsCharg
 
 	on_charge_did_start();
 
-	EXPECT_TRUE(rc.state_machine.is(lksm::state::emergency_stopped));
+	EXPECT_TRUE(rc.state_machine.is(boost::sml::state<game::SMAutonomousGame>));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedConnectedEventCommandReceivedGuardIsChargingTrue)
+TEST_F(RobotControllerTest, stateGameConnectedEventCommandReceivedGuardIsChargingTrue)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped, lksm::state::connected);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>, lksm::state::connected);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(true));
+
+	Sequence on_game_exit_sequence;
+	EXPECT_CALL(mock_videokit, stopVideo).InSequence(on_game_exit_sequence);
+	EXPECT_CALL(mock_motor_left, stop).InSequence(on_game_exit_sequence);
+	EXPECT_CALL(mock_motor_right, stop).InSequence(on_game_exit_sequence);
 
 	Sequence start_charging_behavior_sequence;
 	EXPECT_CALL(battery, level).InSequence(start_charging_behavior_sequence);
@@ -125,9 +138,9 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedConnectedEventCommandReceivedGu
 	EXPECT_TRUE(rc.state_machine.is(lksm::state::charging, lksm::state::connected));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedDisonnectedEventCommandReceivedGuardIsChargingTrue)
+TEST_F(RobotControllerTest, stateGameDisconnectedEventCommandReceivedGuardIsChargingTrue)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped, lksm::state::disconnected);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>, lksm::state::disconnected);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(true));
 
@@ -136,14 +149,18 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedDisonnectedEventCommandReceived
 
 	rc.state_machine.process_event(lksm::event::command_received {});
 
-	EXPECT_TRUE(rc.state_machine.is(lksm::state::emergency_stopped, lksm::state::disconnected));
+	EXPECT_TRUE(rc.state_machine.is(boost::sml::state<game::SMAutonomousGame>, lksm::state::disconnected));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedEventBleConnectionGuardIsCharging)
+TEST_F(RobotControllerTest, stateGameEventBleConnectionGuardIsCharging)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped, lksm::state::disconnected);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>, lksm::state::disconnected);
 
 	EXPECT_CALL(battery, isCharging).WillRepeatedly(Return(true));
+
+	EXPECT_CALL(mock_videokit, stopVideo).Times(AtLeast(1));
+	EXPECT_CALL(mock_motor_left, stop).Times(AtLeast(1));
+	EXPECT_CALL(mock_motor_right, stop).Times(AtLeast(1));
 
 	Sequence start_charging_behavior_sequence;
 	EXPECT_CALL(battery, level).InSequence(start_charging_behavior_sequence);
@@ -152,12 +169,8 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedEventBleConnectionGuardIsChargi
 	EXPECT_CALL(timeout, onTimeout).InSequence(start_charging_behavior_sequence);
 	EXPECT_CALL(timeout, start).InSequence(start_charging_behavior_sequence);
 
-	EXPECT_CALL(mock_videokit, stopVideo).Times(2);
-	EXPECT_CALL(mock_motor_left, stop).Times(2);
-	EXPECT_CALL(mock_motor_right, stop).Times(2);
 	EXPECT_CALL(mock_ears, hide).Times(1);
 	EXPECT_CALL(mock_belt, hide).Times(1);
-
 	EXPECT_CALL(mock_belt, setColor).Times(AtLeast(1));
 	EXPECT_CALL(mock_belt, show).Times(AtLeast(1));
 
@@ -169,13 +182,16 @@ TEST_F(RobotControllerTest, stateEmergencyStoppedEventBleConnectionGuardIsChargi
 	EXPECT_TRUE(rc.state_machine.is(lksm::state::charging));
 }
 
-TEST_F(RobotControllerTest, stateEmergencyStoppedEventAutonomousGameTransition)
+TEST_F(RobotControllerTest, stateGameEventAutonomousGameTransition)
 {
-	rc.state_machine.set_current_states(lksm::state::emergency_stopped);
+	rc.state_machine.set_current_states(boost::sml::state<game::SMAutonomousGame>);
 
+	EXPECT_CALL(mock_videokit, stopVideo).Times(AtLeast(1));
+	EXPECT_CALL(mock_motor_left, stop).Times(AtLeast(1));
+	EXPECT_CALL(mock_motor_right, stop).Times(AtLeast(1));
 	EXPECT_CALL(mock_videokit, displayImage).Times(1);
 
-	rc.state_machine.process_event(lksm::event::autonomous_game_transition {});
+	rc.raiseAutonomousGameTransition();
 
 	EXPECT_TRUE(rc.state_machine.is(boost::sml::state<game::SMAutonomousGame>));
 }
--- libs/RobotKit/tests/RobotController_test_stateSleeping.cpp
+++ libs/RobotKit/tests/RobotController_test_stateSleeping.cpp
@@ -110,3 +110,19 @@ TEST_F(RobotControllerTest, stateSleepingEventEmergencyStop)
 
 	EXPECT_TRUE(rc.state_machine.is(lksm::state::emergency_stopped));
 }
+
+TEST_F(RobotControllerTest, stateSleepingEventAutonomousGameTransition)
+{
+	rc.state_machine.set_current_states(lksm::state::sleeping);
+
+	Sequence on_exit_sleeping_sequence;
+	EXPECT_CALL(timeout, stop).InSequence(on_exit_sleeping_sequence);
+	EXPECT_CALL(mock_videokit, stopVideo).InSequence(on_exit_sleeping_sequence);
+	expectedCallsStopMotors();
+
+	EXPECT_CALL(mock_videokit, displayImage).Times(1);
+
+	rc.raiseAutonomousGameTransition();
+
+	EXPECT_TRUE(rc.state_machine.is(boost::sml::state<game::SMAutonomousGame>));
+}
