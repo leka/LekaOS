7f36fa4a459cd2047be86508cb2bdac85003d1ee
Hugo Pezziardi <hugo@pezziardi.net>
2022-09-06 18:00:48 +0200

    :recycle: (Activity): Call backup RFID callback when redefining it

--- libs/ActivityKit/source/activities/DisplayTags.cpp
+++ libs/ActivityKit/source/activities/DisplayTags.cpp
@@ -14,13 +14,11 @@ void DisplayTags::start()
 
 	_backup_callback = _rfidkit.getCallback();
 
-	auto on_tag_detected_callback = [this](const MagicCard &card) {
-		if (card == MagicCard::remote_standard) {
-			stop();
-		} else {
-			snprintf(_path_buffer.data(), _path_buffer.size(), "fs/home/img/id/%.4x.jpg", card.getId());
-			_videokit.displayImage(_path_buffer.data());
-		}
+	auto on_tag_detected_callback = [this](MagicCard &card) {
+		snprintf(_path_buffer.data(), _path_buffer.size(), "fs/home/img/id/%.4x.jpg", card.getId());
+		_videokit.displayImage(_path_buffer.data());
+
+		_backup_callback(card);
 	};
 
 	_rfidkit.onTagActivated(on_tag_detected_callback);
