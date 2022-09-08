8972dfd549a8e24999165c7d009c8ce0fba87a29
CedricDia <diavorini.cedric.dev@gmail.com>
2022-09-08 10:09:16 +0200

    :construction: [drivers] add getData in array in interfaces

--- drivers/CoreIMUlsm6dsox/include/interface/Accelerometer.h
+++ drivers/CoreIMUlsm6dsox/include/interface/Accelerometer.h
@@ -2,65 +2,62 @@
 // Copyright 2021 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-
 #pragma once
 
-#include "LogKit.h"
-#include <math.h>
+#include <array>
 
 namespace leka {
-    enum odr_xl_t
-    {
-        XL_ODR_OFF    = 0,
-        XL_ODR_12Hz5  = 1,
-        XL_ODR_26Hz   = 2,
-        XL_ODR_52Hz   = 3,
-        XL_ODR_104Hz  = 4,
-        XL_ODR_208Hz  = 5,
-        XL_ODR_417Hz  = 6,
-        XL_ODR_833Hz  = 7,
-        XL_ODR_1667Hz = 8,
-        XL_ODR_3333Hz = 9,
-        XL_ODR_6667Hz = 10,
-        XL_ODR_1Hz6   = 11,
-    };
+enum odr_xl_t
+{
+	XL_ODR_OFF	  = 0,
+	XL_ODR_12Hz5  = 1,
+	XL_ODR_26Hz	  = 2,
+	XL_ODR_52Hz	  = 3,
+	XL_ODR_104Hz  = 4,
+	XL_ODR_208Hz  = 5,
+	XL_ODR_417Hz  = 6,
+	XL_ODR_833Hz  = 7,
+	XL_ODR_1667Hz = 8,
+	XL_ODR_3333Hz = 9,
+	XL_ODR_6667Hz = 10,
+	XL_ODR_1Hz6	  = 11,
+};
 
-    enum fs_xl_t
-    {
-        XL_FS_2g   = 0,
-        XL_FS_16g  = 1,
-        XL_FS_4g   = 2,
-        XL_FS_8g   = 3,
-    };
-}
+enum fs_xl_t
+{
+	XL_FS_2g  = 0,
+	XL_FS_16g = 1,
+	XL_FS_4g  = 2,
+	XL_FS_8g  = 3,
+};
+}	// namespace leka
 
 namespace leka::interface {
 
-    class Accelerometer
-    {
-        public:
-            
-            virtual ~Accelerometer() = default;
+class Accelerometer
+{
+  public:
+	virtual ~Accelerometer() = default;
 
-            virtual auto getDataX_mg() -> float_t       = 0;
-            virtual auto getDataY_mg() -> float_t       = 0;
-            virtual auto getDataZ_mg() -> float_t       = 0;
-            
-            virtual auto getDataX_g() -> float_t        = 0;
-            virtual auto getDataY_g() -> float_t        = 0;
-            virtual auto getDataZ_g() -> float_t        = 0;
+	virtual auto getDataX_mg() -> float = 0;
+	virtual auto getDataY_mg() -> float = 0;
+	virtual auto getDataZ_mg() -> float = 0;
 
-            virtual void setDataRate(odr_xl_t odr_xl)   = 0;
-            virtual auto getDataRateAcc() -> int32_t    = 0;
-            
-            virtual void SetFullScale(fs_xl_t fs_xl)    = 0;
+	virtual auto getDataX_g() -> float = 0;
+	virtual auto getDataY_g() -> float = 0;
+	virtual auto getDataZ_g() -> float = 0;
 
-            virtual void TurnOffAcc()                   = 0;
+	virtual void getData_mg(std::array<float, 3> &xl_data) = 0;
+	virtual void getData_g(std::array<float, 3> &xl_data)  = 0;
 
+	virtual void setDataRate(odr_xl_t odr_xl) = 0;
+	virtual auto getDataRateAcc() -> int32_t  = 0;
 
-        private:
+	virtual void SetFullScale(fs_xl_t fs_xl) = 0;
 
-    };
+	virtual void TurnOffAcc() = 0;
 
+  private:
+};
 
-}
+}	// namespace leka::interface
--- drivers/CoreIMUlsm6dsox/include/interface/Gyroscope.h
+++ drivers/CoreIMUlsm6dsox/include/interface/Gyroscope.h
@@ -2,65 +2,62 @@
 // Copyright 2021 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-
 #pragma once
 
-#include "LogKit.h"
-#include <math.h>
+#include <array>
 
 namespace leka {
-    enum odr_g_t
-    {
-        GY_ODR_OFF    = 0,
-        GY_ODR_12Hz5  = 1,
-        GY_ODR_26Hz   = 2,
-        GY_ODR_52Hz   = 3,
-        GY_ODR_104Hz  = 4,
-        GY_ODR_208Hz  = 5,
-        GY_ODR_417Hz  = 6,
-        GY_ODR_833Hz  = 7,
-        GY_ODR_1667Hz = 8,
-        GY_ODR_3333Hz = 9,
-        GY_ODR_6667Hz = 10,
-    };
+enum odr_g_t
+{
+	GY_ODR_OFF	  = 0,
+	GY_ODR_12Hz5  = 1,
+	GY_ODR_26Hz	  = 2,
+	GY_ODR_52Hz	  = 3,
+	GY_ODR_104Hz  = 4,
+	GY_ODR_208Hz  = 5,
+	GY_ODR_417Hz  = 6,
+	GY_ODR_833Hz  = 7,
+	GY_ODR_1667Hz = 8,
+	GY_ODR_3333Hz = 9,
+	GY_ODR_6667Hz = 10,
+};
 
-    enum fs_g_t
-    {
-        GY_FS_250dps   = 0,
-        GY_FS_125dps   = 1,
-        GY_FS_500dps   = 2,
-        GY_FS_1000dps  = 4,
-        GY_FS_2000dps  = 6,
-    };
-}
+enum fs_g_t
+{
+	GY_FS_250dps  = 0,
+	GY_FS_125dps  = 1,
+	GY_FS_500dps  = 2,
+	GY_FS_1000dps = 4,
+	GY_FS_2000dps = 6,
+};
+}	// namespace leka
 
 namespace leka::interface {
 
-    class Gyroscope
-    {
-        public:
-            
-            virtual ~Gyroscope() = default;
+class Gyroscope
+{
+  public:
+	virtual ~Gyroscope() = default;
 
-            virtual auto getDataX_mdps() -> float_t     = 0;
-            virtual auto getDataY_mdps() -> float_t     = 0;
-            virtual auto getDataZ_mdps() -> float_t     = 0;
+	virtual auto getDataX_mdps() -> float = 0;
+	virtual auto getDataY_mdps() -> float = 0;
+	virtual auto getDataZ_mdps() -> float = 0;
 
-            virtual auto getDataX_dps() -> float_t      = 0;
-            virtual auto getDataY_dps() -> float_t      = 0;
-            virtual auto getDataZ_dps() -> float_t      = 0;
+	virtual auto getDataX_dps() -> float = 0;
+	virtual auto getDataY_dps() -> float = 0;
+	virtual auto getDataZ_dps() -> float = 0;
 
-            virtual void setDataRate(odr_g_t odr_gy)    = 0;
-            virtual auto getDataRateGyr() -> int32_t    = 0;
-            
-            virtual void SetFullScale(fs_g_t fs_gy)     = 0;
+	virtual void getData_mdps(std::array<float, 3> &gy_data) = 0;
+	virtual void getData_dps(std::array<float, 3> &gy_data)	 = 0;
 
-            virtual void TurnOffGyr()                   = 0;
+	virtual void setDataRate(odr_g_t odr_gy) = 0;
+	virtual auto getDataRateGyr() -> int32_t = 0;
 
+	virtual void SetFullScale(fs_g_t fs_gy) = 0;
 
-        private:
+	virtual void TurnOffGyr() = 0;
 
-    };
+  private:
+};
 
-
-}
+}	// namespace leka::interface
