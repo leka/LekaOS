9e966b60251e0db42d0aadb14a60a5ca5ef6053d
CedricDia <diavorini.cedric.dev@gmail.com>
2022-09-09 09:09:42 +0200

    :construction: [driver] Review CoreIMUlsm6dsox

--- drivers/CoreIMUlsm6dsox/include/interface/Gyroscope.h
+++ drivers/CoreIMUlsm6dsox/include/interface/Gyroscope.h
@@ -8,62 +8,35 @@
 #include "LogKit.h"
 #include <math.h>
 
-namespace leka {
-    enum odr_g_t
-    {
-        GY_ODR_OFF    = 0,
-        GY_ODR_12Hz5  = 1,
-        GY_ODR_26Hz   = 2,
-        GY_ODR_52Hz   = 3,
-        GY_ODR_104Hz  = 4,
-        GY_ODR_208Hz  = 5,
-        GY_ODR_417Hz  = 6,
-        GY_ODR_833Hz  = 7,
-        GY_ODR_1667Hz = 8,
-        GY_ODR_3333Hz = 9,
-        GY_ODR_6667Hz = 10,
-    };
-
-    enum fs_g_t
-    {
-        GY_FS_250dps   = 0,
-        GY_FS_125dps   = 1,
-        GY_FS_500dps   = 2,
-        GY_FS_1000dps  = 4,
-        GY_FS_2000dps  = 6,
-    };
-}
 
 namespace leka::interface {
 
+    using dps_t = float;
     class Gyroscope
     {
         public:
             
+            struct Data {
+                dps_t x;
+                dps_t y;
+                dps_t z;
+            };
+            using gyro_data_t = Data;
+            
             virtual ~Gyroscope() = default;
 
-            virtual auto getDataX_mdps()                                -> float_t = 0;
-            virtual auto getDataY_mdps()                                -> float_t = 0;
-            virtual auto getDataZ_mdps()                                -> float_t = 0;
+            virtual auto getAngularSpeedX()                                 -> dps_t    = 0;
+            virtual auto getAngularSpeedY()                                 -> dps_t    = 0;
+            virtual auto getAngularSpeedZ()                                 -> dps_t    = 0;
 
-            virtual auto getDataX_dps()                                 -> float_t = 0;
-            virtual auto getDataY_dps()                                 -> float_t = 0;
-            virtual auto getDataZ_dps()                                 -> float_t = 0;
+            virtual void getAngularSpeedXYZ(std::array<dps_t, 3> &)                     = 0;
+            virtual auto getGyrRate() 						                -> int32_t  = 0;
+            virtual auto getGyrRange() 						                -> int32_t  = 0;
 
-            virtual void getData_mdps(std::array<float, 3> &gy_data)               = 0;
-            virtual void getData_dps(std::array<float, 3> &gy_data)                = 0;
-
-            virtual void setDataRate(odr_g_t odr_gy)                               = 0;
-            virtual auto getDataRateGyr()                               -> int32_t = 0;
-            
-            virtual void SetFullScale(fs_g_t fs_gy)                                = 0;
-
-            virtual void TurnOffGyr()                                              = 0;
+            virtual auto turnOffGyr() -> int32_t = 0;
 
 
         private:
 
     };
-
-
 }
