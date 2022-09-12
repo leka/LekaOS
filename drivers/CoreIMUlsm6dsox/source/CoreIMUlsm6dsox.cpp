4f987ce62299165b2474c6fcf835a51199cb1d6d
CedricDia <diavorini.cedric.dev@gmail.com>
2022-09-12 14:12:24 +0200

    :construction: [driver] rename CoreIMU_LSM6DSOX to CoreIMULSM6DSOX

--- drivers/CoreIMUlsm6dsox/source/CoreIMUlsm6dsox.cpp
+++ drivers/CoreIMUlsm6dsox/source/CoreIMUlsm6dsox.cpp
@@ -2,7 +2,7 @@
 // Copyright 2022 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-#include "CoreIMUlsm6dsox.h"
+#include "CoreIMULSM6DSOX.h"
 
 namespace leka {
 
@@ -10,7 +10,7 @@ namespace leka {
 // MARK: - Constructor
 //
 
-CoreIMU_LSM6DSOX::CoreIMU_LSM6DSOX(mbed::I2C &i2c, PinName pin_interrupt)
+CoreIMULSM6DSOX::CoreIMULSM6DSOX(mbed::I2C &i2c, PinName pin_interrupt)
 	: _i2c(i2c), _lsm6dsox_interrupt(pin_interrupt)
 {
 	_register_io_function.write_reg = (stmdev_write_ptr)ptr_io_write;
@@ -23,7 +23,7 @@ CoreIMU_LSM6DSOX::CoreIMU_LSM6DSOX(mbed::I2C &i2c, PinName pin_interrupt)
 // Mark: - Device functions
 //
 
-void CoreIMU_LSM6DSOX::init()
+void CoreIMULSM6DSOX::init()
 {
 	lsm6dsox_init_set(&_register_io_function, LSM6DSOX_DRV_RDY);
 	lsm6dsox_i3c_disable_set(&_register_io_function, LSM6DSOX_I3C_DISABLE);
@@ -42,7 +42,7 @@ void CoreIMU_LSM6DSOX::init()
 	setGyrRange(RangeGyr::_2000dps);
 }
 
-auto CoreIMU_LSM6DSOX::WriteReg(stmdev_ctx_t *ctx, uint8_t reg, const uint8_t *data, uint16_t len) const -> int32_t
+auto CoreIMULSM6DSOX::WriteReg(stmdev_ctx_t *ctx, uint8_t reg, const uint8_t *data, uint16_t len) const -> int32_t
 {
 	auto ret = ctx->write_reg(ctx->handle, reg, data, len);
 	return ret;
@@ -52,7 +52,7 @@ auto CoreIMU_LSM6DSOX::WriteReg(stmdev_ctx_t *ctx, uint8_t reg, const uint8_t *d
 // MARK: - I2C Read/Write methods & pointers
 //
 
-auto CoreIMU_LSM6DSOX::read(uint8_t register_address, uint16_t number_bytes_to_read, uint8_t *pBuffer) -> int32_t
+auto CoreIMULSM6DSOX::read(uint8_t register_address, uint16_t number_bytes_to_read, uint8_t *pBuffer) -> int32_t
 {
 		/* Send component address, with no STOP condition */
 	auto ret = _i2c.write(_address, (const char *)&register_address, 1, true);
@@ -64,7 +64,7 @@ auto CoreIMU_LSM6DSOX::read(uint8_t register_address, uint16_t number_bytes_to_r
 	return ret;
 }
 
-auto CoreIMU_LSM6DSOX::write(uint8_t register_address, uint16_t number_bytes_to_write, uint8_t *pBuffer) -> int32_t
+auto CoreIMULSM6DSOX::write(uint8_t register_address, uint16_t number_bytes_to_write, uint8_t *pBuffer) -> int32_t
 {
 	if (number_bytes_to_write >= 32) return 2;
 
@@ -77,14 +77,14 @@ auto CoreIMU_LSM6DSOX::write(uint8_t register_address, uint16_t number_bytes_to_
 }
 
 
-auto CoreIMU_LSM6DSOX::ptr_io_write(CoreIMU_LSM6DSOX *handle, uint8_t write_address, uint8_t *p_buffer, 
+auto CoreIMULSM6DSOX::ptr_io_write(CoreIMULSM6DSOX *handle, uint8_t write_address, uint8_t *p_buffer, 
 									uint16_t number_bytes_to_write) -> int32_t
 {
 	return handle->write(write_address, number_bytes_to_write, p_buffer);
 }	
 
 
-auto CoreIMU_LSM6DSOX::ptr_io_read(CoreIMU_LSM6DSOX *handle, uint8_t read_address, uint8_t *p_buffer,
+auto CoreIMULSM6DSOX::ptr_io_read(CoreIMULSM6DSOX *handle, uint8_t read_address, uint8_t *p_buffer,
 								   uint16_t number_bytes_to_read) -> int32_t
 {
 	return handle->read(read_address, number_bytes_to_read, p_buffer);
@@ -94,7 +94,7 @@ auto CoreIMU_LSM6DSOX::ptr_io_read(CoreIMU_LSM6DSOX *handle, uint8_t read_addres
 // Mark: - Device functions
 //
 
-void CoreIMU_LSM6DSOX::setPowerMode(power_mode_t mode)
+void CoreIMULSM6DSOX::setPowerMode(power_mode_t mode)
 {
 	if (mode == PowerMode::Off) {
 		lsm6dsox_xl_data_rate_set(&_register_io_function, LSM6DSOX_XL_ODR_OFF);
@@ -120,25 +120,25 @@ void CoreIMU_LSM6DSOX::setPowerMode(power_mode_t mode)
 	}
 }
 
-void CoreIMU_LSM6DSOX::setMLCDataRate(lsm6dsox_mlc_odr_t val)
+void CoreIMULSM6DSOX::setMLCDataRate(lsm6dsox_mlc_odr_t val)
 {
 	lsm6dsox_mlc_data_rate_set(&getRegisterIOFunction(), val);
 }
 
-void CoreIMU_LSM6DSOX::TurnOffEmbeddedFeatures(lsm6dsox_emb_sens_t *emb_sens)
+void CoreIMULSM6DSOX::TurnOffEmbeddedFeatures(lsm6dsox_emb_sens_t *emb_sens)
 {
 	lsm6dsox_embedded_sens_get(&_register_io_function, emb_sens);
 	lsm6dsox_embedded_sens_off(&_register_io_function);
 }
 
-void CoreIMU_LSM6DSOX::RouteSignalsInterruptGetSet(lsm6dsox_pin_int1_route_t *pin_int1_route)
+void CoreIMULSM6DSOX::RouteSignalsInterruptGetSet(lsm6dsox_pin_int1_route_t *pin_int1_route)
 {
 	lsm6dsox_pin_int1_route_get(&_register_io_function, pin_int1_route);
 	pin_int1_route->mlc1 = PROPERTY_ENABLE;
 	lsm6dsox_pin_int1_route_set(&_register_io_function, *pin_int1_route);
 }
 
-void CoreIMU_LSM6DSOX::setBlockDataUpdate(uint8_t val)
+void CoreIMULSM6DSOX::setBlockDataUpdate(uint8_t val)
 {
 	lsm6dsox_ctrl3_c_t reg;
 	auto ret = lsm6dsox_read_reg(&getRegisterIOFunction(), LSM6DSOX_CTRL3_C, (uint8_t *)&reg, 1);
@@ -149,12 +149,12 @@ void CoreIMU_LSM6DSOX::setBlockDataUpdate(uint8_t val)
 	}
 }
 
-void CoreIMU_LSM6DSOX::setIntNotification(lsm6dsox_lir_t val)
+void CoreIMULSM6DSOX::setIntNotification(lsm6dsox_lir_t val)
 {
 	lsm6dsox_int_notification_set(&getRegisterIOFunction(), val);
 }
 
-void CoreIMU_LSM6DSOX::setEmbeddedSens(const lsm6dsox_emb_sens_t *val)
+void CoreIMULSM6DSOX::setEmbeddedSens(const lsm6dsox_emb_sens_t *val)
 {
 	lsm6dsox_emb_func_en_a_t emb_func_en_a;
 	lsm6dsox_emb_func_en_b_t emb_func_en_b;
@@ -177,12 +177,12 @@ void CoreIMU_LSM6DSOX::setEmbeddedSens(const lsm6dsox_emb_sens_t *val)
 	}
 }
 
-void CoreIMU_LSM6DSOX::updateAllRessources(lsm6dsox_all_sources_t *val)
+void CoreIMULSM6DSOX::updateAllRessources(lsm6dsox_all_sources_t *val)
 {
 	lsm6dsox_all_sources_get(&getRegisterIOFunction(), val);
 }
 
-auto CoreIMU_LSM6DSOX::getId(uint8_t &id) -> int32_t
+auto CoreIMULSM6DSOX::getId(uint8_t &id) -> int32_t
 {
 	if (lsm6dsox_device_id_get(&_register_io_function, &id)) {
 		return 0; 
@@ -190,7 +190,7 @@ auto CoreIMU_LSM6DSOX::getId(uint8_t &id) -> int32_t
 	return id;
 }
 	//A revoir
-auto CoreIMU_LSM6DSOX::getMLCOut(uint8_t *buff) -> int32_t
+auto CoreIMULSM6DSOX::getMLCOut(uint8_t *buff) -> int32_t
 {
 	lsm6dsox_mem_bank_set(&getRegisterIOFunction(), LSM6DSOX_EMBEDDED_FUNC_BANK);
 	lsm6dsox_read_reg(&getRegisterIOFunction(), LSM6DSOX_MLC0_SRC, buff, 8);
@@ -200,7 +200,7 @@ auto CoreIMU_LSM6DSOX::getMLCOut(uint8_t *buff) -> int32_t
 }
 
 
-void CoreIMU_LSM6DSOX::updateData()
+void CoreIMULSM6DSOX::updateData()
 {
     lsm6dsox_data_t data;
     lsm6dsox_data_get(&getRegisterIOFunction(), nullptr, &getConfig(), &data);
@@ -212,7 +212,7 @@ void CoreIMU_LSM6DSOX::updateData()
 	_sensors_data.gy.z = data.ui.gy.mdps[2]/1000;
 }
 
-void CoreIMU_LSM6DSOX::TurnOffSensors()
+void CoreIMULSM6DSOX::TurnOffSensors()
 {
 	turnOffAccel();
 	turnOffGyr();
@@ -222,38 +222,38 @@ void CoreIMU_LSM6DSOX::TurnOffSensors()
 // Mark: - Acceleremeter functions
 //
 
-void CoreIMU_LSM6DSOX::setAccelRate(odr_xl_t odr_xl)
+void CoreIMULSM6DSOX::setAccelRate(odr_xl_t odr_xl)
 {
 	lsm6dsox_xl_data_rate_set(&getRegisterIOFunction(), static_cast<lsm6dsox_odr_xl_t>(odr_xl));
 }
 
-void CoreIMU_LSM6DSOX::setAccelRange(fs_xl_t fs_xl)
+void CoreIMULSM6DSOX::setAccelRange(fs_xl_t fs_xl)
 {
 	lsm6dsox_xl_full_scale_set(&getRegisterIOFunction(), static_cast<lsm6dsox_fs_xl_t>(fs_xl));
 }
 
-auto CoreIMU_LSM6DSOX::getAccelRate() -> int32_t
+auto CoreIMULSM6DSOX::getAccelRate() -> int32_t
 {
 	lsm6dsox_ctrl1_xl_t reg;
 	lsm6dsox_read_reg(&getRegisterIOFunction(), LSM6DSOX_CTRL1_XL, (uint8_t *)&reg, 1);
 	return reg.odr_xl;
 }
 
-auto CoreIMU_LSM6DSOX::getAccelRange() -> int32_t
+auto CoreIMULSM6DSOX::getAccelRange() -> int32_t
 {
 	lsm6dsox_ctrl1_xl_t reg;
 	lsm6dsox_read_reg(&getRegisterIOFunction(), LSM6DSOX_CTRL1_XL, (uint8_t *)&reg, 1);
 	return reg.fs_xl;
 }
 
-auto CoreIMU_LSM6DSOX::getAccelXYZ() -> std::tuple<interface::mg_t, interface::mg_t, interface::mg_t>
+auto CoreIMULSM6DSOX::getAccelXYZ() -> std::tuple<interface::mg_t, interface::mg_t, interface::mg_t>
 {
 	updateData();
 	return std::tuple {_sensors_data.xl.x, _sensors_data.xl.y, _sensors_data.xl.z};
 }
 
 
-void CoreIMU_LSM6DSOX::turnOffAccel() 
+void CoreIMULSM6DSOX::turnOffAccel() 
 {
 	lsm6dsox_xl_data_rate_set(&_register_io_function, LSM6DSOX_XL_ODR_OFF);
 }
@@ -263,24 +263,24 @@ void CoreIMU_LSM6DSOX::turnOffAccel()
 // Mark: - Gyroscope functions
 //
 
-void CoreIMU_LSM6DSOX::setGyrRate(odr_g_t odr_gy)
+void CoreIMULSM6DSOX::setGyrRate(odr_g_t odr_gy)
 {
 	lsm6dsox_gy_data_rate_set(&getRegisterIOFunction(), static_cast<lsm6dsox_odr_g_t>(odr_gy));
 }
 
-void CoreIMU_LSM6DSOX::setGyrRange(fs_g_t fs_gy)
+void CoreIMULSM6DSOX::setGyrRange(fs_g_t fs_gy)
 {
 	lsm6dsox_gy_full_scale_set(&getRegisterIOFunction(), static_cast<lsm6dsox_fs_g_t>(fs_gy));
 }
 
-auto CoreIMU_LSM6DSOX::getGyrRate() -> int32_t
+auto CoreIMULSM6DSOX::getGyrRate() -> int32_t
 {
 	lsm6dsox_ctrl2_g_t reg;
 	lsm6dsox_read_reg(&getRegisterIOFunction(), LSM6DSOX_CTRL2_G, (uint8_t *)&reg, 1);
 	return reg.odr_g;
 }
 
-auto CoreIMU_LSM6DSOX::getGyrRange() -> int32_t
+auto CoreIMULSM6DSOX::getGyrRange() -> int32_t
 {
 	lsm6dsox_ctrl2_g_t reg;
 	lsm6dsox_read_reg(&getRegisterIOFunction(), LSM6DSOX_CTRL2_G, (uint8_t *)&reg, 1);
@@ -288,13 +288,13 @@ auto CoreIMU_LSM6DSOX::getGyrRange() -> int32_t
 }
  
 
-auto CoreIMU_LSM6DSOX::getAngularSpeedXYZ()  -> std::tuple<interface::dps_t, interface::dps_t, interface::dps_t>
+auto CoreIMULSM6DSOX::getAngularSpeedXYZ()  -> std::tuple<interface::dps_t, interface::dps_t, interface::dps_t>
 {
 	updateData();
 	return std::tuple {_sensors_data.gy.x, _sensors_data.gy.y, _sensors_data.gy.z};
 }
 
-void CoreIMU_LSM6DSOX::turnOffGyr() 
+void CoreIMULSM6DSOX::turnOffGyr() 
 {
 	lsm6dsox_gy_data_rate_set(&_register_io_function, LSM6DSOX_GY_ODR_OFF);
 }
@@ -303,7 +303,7 @@ void CoreIMU_LSM6DSOX::turnOffGyr()
 //Mark : get All Data at the same timestamp
 //
 
-auto CoreIMU_LSM6DSOX::getAccelAndAngularSpeed() -> std::tuple<interface::mg_t, interface::mg_t, interface::mg_t, interface::dps_t, interface::dps_t, interface::dps_t>
+auto CoreIMULSM6DSOX::getAccelAndAngularSpeed() -> std::tuple<interface::mg_t, interface::mg_t, interface::mg_t, interface::dps_t, interface::dps_t, interface::dps_t>
 {
 	updateData();
 	return std::tuple {_sensors_data.xl.x, _sensors_data.xl.y, _sensors_data.xl.z, _sensors_data.gy.x, _sensors_data.gy.y, _sensors_data.gy.z};
