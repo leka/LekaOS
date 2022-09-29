79830ae77912180c082b4c17dbe10dc9ffd763ef
Ladislas de Toldi <ladislas@detoldi.me>
2022-09-29 15:58:30 +0200

    :recycle: (spikes): boost::ut - refactor tests in separated .cpp files
    - separate tests in different files
    - create small main.cpp that can be used anywhere
    - update CMakeLists.txt file
    - add config.h
    - use MACROS for start/end statistics

---
+++ spikes/lk_boost_ut/tests/test_parametrized.cpp
@@ -0,0 +1,48 @@
+// Leka - LekaOS
+// Copyright 2022 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include <tuple>
+
+#include "./tests/config.h"
+#include "boost/ut.hpp"
+
+using namespace boost::ut;
+
+// ? Example taken from:
+// ? https://github.com/boost-ext/ut/blob/master/example/parameterized.cpp
+
+suite suite_parameterized = [] {
+	for (auto i: std::vector {1, 2, 3}) {
+		test("parametrized: for loop / val = " + std::to_string(i)) = [i] { expect(that % i > 0); };
+	}
+
+	"parametrized: | operator + std::vector"_test = [](auto arg) {
+		log << " val =" << std::to_string(arg) << " ";
+		expect(arg > 0_i) << "all values greater than 0";
+	} | std::vector {1, 2, 3};
+
+	"parametrized: | operator + std::tuple"_test = []<typename T>() mutable {
+		expect(std::is_integral_v<T>) << "all types are integrals";
+	} | std::tuple<bool, int> {};
+
+	std::apply(
+		[]([[maybe_unused]] auto... args) {
+			((test("parametrized: std::apply / val = " + std::to_string(args)) =
+				  [&args] {
+					  using TArgs = decltype(args);
+					  expect(std::is_integral_v<TArgs> >> fatal);
+					  expect(42_i == static_cast<int>(args) or args);
+					  expect(type<TArgs> == type<int> or type<TArgs> == type<bool>);
+				  }),
+			 ...);
+		},
+		std::tuple {42, true});
+
+	"parametrized: template<>"_test = []<class TArg>(TArg arg) {
+		log << " val =" << std::to_string(arg) << " ";
+		expect(std::is_integral_v<TArg> >> fatal);
+		expect(42_i == static_cast<int>(arg) or arg);
+		expect(type<TArg> == type<int> or type<TArg> == type<bool>);
+	} | std::tuple {42, true};
+};
--- spikes/lk_boost_ut/utils.h
+++ spikes/lk_boost_ut/utils.h
@@ -8,38 +8,34 @@
 
 #include "LogKit.h"
 
-namespace utils {
+namespace utils::time {
 
 using namespace leka;
 using namespace std::chrono;
 
-namespace time {
+inline auto start = rtos::Kernel::Clock::now();
+inline auto stop  = rtos::Kernel::Clock::now();
+inline auto delta = static_cast<int>((stop - start).count());
 
-	auto start = rtos::Kernel::Clock::now();
-	auto stop  = rtos::Kernel::Clock::now();
-	auto delta = static_cast<int>((stop - start).count());
+// namespace time
 
-}	// namespace time
+}	// namespace utils::time
 
-inline void start(const char *msg)
-{
-	logger::init();
+// NOLINTNEXTLINE
+#define utils_start(msg)                                                                                               \
+	do {                                                                                                               \
+		logger::init();                                                                                                \
+		log_ll("\n", 1);                                                                                               \
+		log_info("Starting %s...\n", msg);                                                                             \
+		rtos::ThisThread::sleep_for(1s);                                                                               \
+		utils::time::start = rtos::Kernel::Clock::now();                                                               \
+	} while (0)
 
-	log_ll("\n", 1);
-	log_info("Starting %s...\n", msg);
-
-	rtos::ThisThread::sleep_for(1s);
-
-	time::start = rtos::Kernel::Clock::now();
-}
-
-inline void end()
-{
-	time::stop	= rtos::Kernel::Clock::now();
-	time::delta = static_cast<int>((time::stop - time::start).count());
-
-	log_ll("\n", 1);
-	log_info("End of tests (%i ms total)", time::delta);
-}
-
-}	// namespace utils
+// NOLINTNEXTLINE
+#define utils_end()                                                                                                    \
+	do {                                                                                                               \
+		utils::time::stop  = rtos::Kernel::Clock::now();                                                               \
+		utils::time::delta = static_cast<int>((utils::time::stop - utils::time::start).count());                       \
+		log_ll("\n", 1);                                                                                               \
+		log_info("End of tests (%i ms total)", utils::time::delta);                                                    \
+	} while (0)
