2541af80eb1b02ba2788063bedf9ea40f6c33d26
Yann Locatelli <yann.locatelli2@gmail.com>
2022-10-17 17:22:53 +0200

    :truck: (ble): Rename NS receive_file to file_exchange

--- libs/BLEKit/include/BLEServiceFileExchange.h
+++ libs/BLEKit/include/BLEServiceFileExchange.h
@@ -12,7 +12,7 @@ namespace leka {
 class BLEServiceFileExchange : public interface::BLEService
 {
   public:
-	BLEServiceFileExchange() : interface::BLEService(service::receive_file::uuid, _characteristic_table) {};
+	BLEServiceFileExchange() : interface::BLEService(service::file_exchange::uuid, _characteristic_table) {};
 
 	void setFileSHA256(std::array<uint8_t, 32> sha256) const
 	{
@@ -69,15 +69,15 @@ class BLEServiceFileExchange : public interface::BLEService
   private:
 	std::array<char, 256> file_path {};
 	WriteOnlyArrayGattCharacteristic<char, 256> file_path_characteristic {
-		service::receive_file::characteristic::file_path, file_path.begin()};
+		service::file_exchange::characteristic::file_path, file_path.begin()};
 
 	std::array<uint8_t, 128> file_reception_buffer {};
 	WriteOnlyArrayGattCharacteristic<uint8_t, 128> file_reception_buffer_characteristic {
-		service::receive_file::characteristic::file_reception_buffer, file_reception_buffer.begin()};
+		service::file_exchange::characteristic::file_reception_buffer, file_reception_buffer.begin()};
 
 	std::array<uint8_t, 32> file_sha256 {};
 	ReadOnlyArrayGattCharacteristic<uint8_t, 32> file_sha256_characteristic {
-		service::receive_file::characteristic::file_sha256, file_sha256.begin()};
+		service::file_exchange::characteristic::file_sha256, file_sha256.begin()};
 
 	std::function<void(std::span<const uint8_t>)> _on_file_data_callback {};
 	std::function<void(std::span<const char>)> _on_file_path_callback {};
