d52a9e9a9562c110dcb478b2ab81e25ebc331be5
Ladislas de Toldi <ladislas@detoldi.me>
2022-11-14 20:39:38 +0100

    :rotating_light: (BLEKit): Fix warnings, small refactor
    - warning: missing username/bug in TODO
    - warning: 4000 is a magic number; consider replacing it with a named constant

--- libs/BLEKit/source/CoreGap.cpp
+++ libs/BLEKit/source/CoreGap.cpp
@@ -10,8 +10,10 @@ void CoreGap::setDefaultAdvertising()
 	auto default_advertising_data = AdvertisingData {};
 
 	_advertising_data_builder.setName(default_advertising_data.name);
+
+	// TODO(@leka/dev-embedded): current commands::uuid is based on Bluno board, change it to our own
 	_advertising_data_builder.setServiceData(
-		leka::service::commands::uuid,	 // TODO: commands::uuid only for compatibility with LekaApp
+		leka::service::commands::uuid,
 		{default_advertising_data.data(), static_cast<int>(default_advertising_data.size())});
 }
 
@@ -29,6 +31,8 @@ void CoreGap::onInitializationComplete(BLE::InitializationCompleteCallbackContex
 
 void CoreGap::startAdvertising()
 {
+	using namespace std::chrono;
+
 	if (_gap.isAdvertisingActive(_advertising_handle)) {
 		return;
 	}
@@ -36,14 +40,15 @@ void CoreGap::startAdvertising()
 	_gap.setAdvertisingParameters(_advertising_handle, _advertising_params);
 	_gap.setAdvertisingPayload(_advertising_handle, _advertising_data_builder.getAdvertisingData());
 
-	_gap.startAdvertising(_advertising_handle, adv_duration_t(millisecond_t(4000)));
+	constexpr auto kAdvDuration = 4000ms;
+	_gap.startAdvertising(_advertising_handle, adv_duration_t(kAdvDuration.count()));
 }
 
 void CoreGap::setAdvertising(AdvertisingData advertising_data)
 {
 	_advertising_data_builder.setName(advertising_data.name);
 	_advertising_data_builder.setServiceData(
-		leka::service::commands::uuid,	 // TODO: commands::uuid only for compatibility with LekaApp
+		leka::service::commands::uuid,	 // ? for compatibility with Leka iOS app BLE implementation
 		{advertising_data.data(), static_cast<int>(advertising_data.size())});
 
 	_gap.setAdvertisingPayload(_advertising_handle, _advertising_data_builder.getAdvertisingData());
