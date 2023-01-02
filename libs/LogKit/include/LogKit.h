946843bdca4f1440ab1437157e6efc4672b6157c
Ladislas de Toldi <ladislas@detoldi.me>
2023-01-03 00:14:03 +0100

    :rotating_light: (libs): Logkit - Fix -Wsign-conversion

--- libs/LogKit/include/LogKit.h
+++ libs/LogKit/include/LogKit.h
@@ -226,9 +226,10 @@ void format_message(const char *message = nullptr, Args... args)
 }
 
 template <typename... Args>
-auto format_output(const char *message = nullptr, Args... args) -> int
+auto format_output(const char *message = nullptr, Args... args) -> std::size_t
 {
-	return snprintf(buffer::output.data(), std::size(buffer::output), message, args...);
+	auto size = snprintf(buffer::output.data(), std::size(buffer::output), message, args...);
+	return static_cast<std::size_t>(size);
 }
 
 //
