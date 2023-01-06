624a69ec2ab4cfb3f1feb245dc216511f6944718
Yann Locatelli <yann.locatelli2@gmail.com>
2023-01-06 12:42:19 +0100

    :recycle: (firmware): Rename application by application_os
    application_os: "application" is the term used by MCUBoot, "os" is the term used by Leka

--- tools/firmware/build_firmware.sh
+++ tools/firmware/build_firmware.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# ? application_os: "application" is the term used by MCUBoot, "os" is the term used by Leka
+
 mkdir -p _tmp
 mkdir -p _release
 
@@ -9,23 +11,23 @@ while getopts rv: flag
 do
     case "${flag}" in
         r) RECOMPILE_BOOTLOADER="true";;
-        v) APPLICATION_VERSION=$OPTARG;;
+        v) APPLICATION_OS_VERSION=$OPTARG;;
     esac
 done
 
-if [ -z "$APPLICATION_VERSION" ]; then
-	echo "APPLICATION_VERSION is unset"
+if [ -z "$APPLICATION_OS_VERSION" ]; then
+	echo "APPLICATION_OS_VERSION is unset"
 	exit 1
 fi
 
 BUILD_NUMBER=$(date +%s)
-APPLICATION_VERSION="$APPLICATION_VERSION+$BUILD_NUMBER"
+APPLICATION_OS_VERSION="$APPLICATION_OS_VERSION+$BUILD_NUMBER"
 
 BOOTLOADER_HEX="_tmp/bootloader.hex"
-APPLICATION_HEX="_tmp/LekaOS-$APPLICATION_VERSION.hex"
+APPLICATION_OS_HEX="_tmp/LekaOS-$APPLICATION_OS_VERSION.hex"
 
-FIRMWARE_HEX="_release/Firmware-$APPLICATION_VERSION.hex"
-FIRMWARE_BIN="_release/Firmware-$APPLICATION_VERSION.bin"
+FIRMWARE_HEX="_release/Firmware-$APPLICATION_OS_VERSION.hex"
+FIRMWARE_BIN="_release/Firmware-$APPLICATION_OS_VERSION.bin"
 
 # Get bootloader
 if [ "$RECOMPILE_BOOTLOADER" = "true" ];
@@ -34,13 +36,13 @@ then
 	./tools/firmware/build_bootloader.sh $BOOTLOADER_HEX
 fi;
 
-# Get application
-echo "Build application"
-./tools/firmware/build_os.sh $RECOMPILE_BOOTLOADER $APPLICATION_HEX $APPLICATION_VERSION
+# Get application_os
+echo "Build application_os"
+./tools/firmware/build_application_os.sh $RECOMPILE_BOOTLOADER $APPLICATION_OS_HEX $APPLICATION_OS_VERSION
 
-# Merge bootloader and application
-echo "Merge bootloader & application"
-hexmerge.py -o $FIRMWARE_HEX --no-start-addr $BOOTLOADER_HEX $APPLICATION_HEX
+# Merge bootloader and application_os
+echo "Merge bootloader & application_os"
+hexmerge.py -o $FIRMWARE_HEX --no-start-addr $BOOTLOADER_HEX $APPLICATION_OS_HEX
 
 # Convert in binary
 arm-none-eabi-objcopy -I ihex -O binary $FIRMWARE_HEX $FIRMWARE_BIN
