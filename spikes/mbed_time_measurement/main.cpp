1e391edc6177503274289dde4d39a13317c32357
Ladislas de Toldi <ladislas@detoldi.me>
2023-01-06 14:44:09 +0100

    WIP - add ticker

--- spikes/mbed_time_measurement/main.cpp
+++ spikes/mbed_time_measurement/main.cpp
@@ -5,24 +5,34 @@
 #include "PinNames.h"
 
 #include "drivers/DigitalOut.h"
+#include "drivers/Ticker.h"
 #include "drivers/UnbufferedSerial.h"
 #include "rtos/ThisThread.h"
 
 using namespace std::chrono;
 
-auto serial = mbed::UnbufferedSerial {USBTX, USBRX, 115200};
+static inline auto serial = mbed::UnbufferedSerial {USBTX, USBRX, 115200};
+
+static inline auto ticker = mbed::Ticker {};
+
+void tick()
+{
+	serial.write("\n", 1);
+}
 
 auto main() -> int
 {
 	// Initialise the digital pin LED1 as an output
 	mbed::DigitalOut led(LED1);
+	ticker.attach(tick, 10s);
 
 	while (true) {
-		led = !led;
-		serial.write(".\n", 2);
-		rtos::ThisThread::sleep_for(10s);
-		led = !led;
-		serial.write(".\n", 2);
-		rtos::ThisThread::sleep_for(10s);
+		rtos::ThisThread::sleep_for(333s);
+		// // led = !led;
+		// serial.write("\n", 1);
+		// HAL_Delay(10000);
+		// // led = !led;
+		// serial.write("\n", 1);
+		// HAL_Delay(10000);
 	}
 }
