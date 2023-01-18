52b37a6abff757853a14ebe62e6a9648b6cad561
Hugo Pezziardi <hugo@pezziardi.net>
2023-01-18 18:24:55 +0100

    :recycle: (IMUKit): IMUKit depends now on LSM6DSOX

--- libs/IMUKit/source/IMUKit.cpp
+++ libs/IMUKit/source/IMUKit.cpp
@@ -4,34 +4,29 @@
 
 #include "IMUKit.h"
 
-#include "rtos/ThisThread.h"
-
 using namespace leka;
 
 void IMUKit::init()
 {
 	_mahony.begin(kDefaultSamplingConfig.frequency);
-	_event_loop.registerCallback([this] { run(); });
+
+	auto on_drdy_callback = [this](const interface::LSM6DSOX::SensorData &imu_data) { computeAngles(imu_data); };
+	_lsm6dsox.registerOnGyDataReadyCallback(on_drdy_callback);
 }
 
 void IMUKit::start()
 {
-	_is_running = true;
-	_event_loop.start();
-}
-
-void IMUKit::run()
-{
-	while (_is_running) {
-		computeAngles();
-		rtos::ThisThread::sleep_for(kDefaultSamplingConfig.delay);
-	}
+	_lsm6dsox.setPowerMode(interface::LSM6DSOX::PowerMode::Normal);
 }
 
 void IMUKit::stop()
 {
-	_is_running = false;
-	_event_loop.stop();
+	_lsm6dsox.setPowerMode(interface::LSM6DSOX::PowerMode::Off);
+}
+
+void IMUKit::setOrigin()
+{
+	_mahony.setOrigin();
 }
 
 auto IMUKit::getAngles() -> std::array<float, 3>
@@ -39,15 +34,10 @@ auto IMUKit::getAngles() -> std::array<float, 3>
 	return {_mahony.getPitch(), _mahony.getRoll(), _mahony.getYaw()};
 }
 
-void IMUKit::reset()
+void IMUKit::computeAngles(const interface::LSM6DSOX::SensorData &imu_data)
 {
-	_mahony.setOrigin();
-}
-
-void IMUKit::computeAngles()
-{
-	auto accel = _accel.getXYZ();
-	auto gyro  = _gyro.getXYZ();
-	auto mag   = std::make_tuple(0.0F, 0.0F, 0.0F);
-	_mahony.update(accel, gyro, mag);
+	auto xl_data			= std::make_tuple(imu_data.xl.x, imu_data.xl.y, imu_data.xl.z);
+	auto gy_data			= std::make_tuple(imu_data.gy.x, imu_data.gy.y, imu_data.gy.z);
+	constexpr auto mag_data = std::make_tuple(0.0F, 0.0F, 0.0F);
+	_mahony.update(xl_data, gy_data, mag_data);
 }
