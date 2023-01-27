66505c6739f3f79a077763702bf5ee0e562937cf
Yann Locatelli <yann.locatelli2@gmail.com>
2023-01-27 14:07:30 +0100

    :white_check_mark: (file): Move seek UTs

--- libs/FileManagerKit/tests/File_test.cpp
+++ libs/FileManagerKit/tests/File_test.cpp
@@ -57,22 +57,6 @@ class FileTest : public ::testing::Test
 	std::filesystem::path tempFilename_filesystem_path;
 };
 
-TEST_F(FileTest, seek)
-{
-	auto input_data	   = std::to_array<uint8_t>({0x61, 0x62, 0x63, 0x64, 0x65, 0x66});	 // "abcdef"
-	auto output_data   = std::array<uint8_t, 6> {};
-	auto expected_data = std::array<uint8_t, 6> {0x64, 0x65, 0x66, 0x00, 0x00, 0x00};
-
-	file.open(tempFilename, "w+");
-
-	std::ignore = file.write(input_data);
-	file.seek(3);
-	auto bytes_read = file.read(output_data);
-
-	ASSERT_EQ(3, bytes_read);
-	ASSERT_EQ(expected_data, output_data);
-}
-
 TEST_F(FileTest, tellNoFile)
 {
 	auto pos = file.tell();
