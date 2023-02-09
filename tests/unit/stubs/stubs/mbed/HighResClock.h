9cc3074ae958d0e24a72d8a7161db5ceb564a949
Ladislas de Toldi <ladislas@detoldi.me>
2023-02-10 00:28:06 +0100

    :clown_face: (stubs): Add HighResClock + mbed (us) ticker api stubs

---
+++ tests/unit/stubs/stubs/mbed/HighResClock.h
@@ -0,0 +1,9 @@
+// Leka - LekaOS
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "drivers/HighResClock.h"
+
+namespace leka {
+
+}	// namespace leka
---
+++ tests/unit/stubs/stubs/mbed/source/mbed_ticker_api.cpp
@@ -0,0 +1,11 @@
+// Leka - LekaOS
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#include "../HighResClock.h"
+
+auto ticker_read_us(const ticker_data_t *const ticker) -> us_timestamp_t
+{
+	us_timestamp_t ret {};
+	return ret;
+}
