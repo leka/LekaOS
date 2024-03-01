e0277f7dc1a77e91b8ad8eaf97889a29875c1252
Yann Locatelli <yann.locatelli2@gmail.com>
2024-03-01 17:05:02 +0100

    :sparkles: (dac): Add CoreSTM32HalBasicTimer
    Co-Authored-By: Maxime Blanc <66126094+aermanio@users.noreply.github.com>
    Co-Authored-By: SamHadjes <48731907+samhadjes@users.noreply.github.com>

---
+++ drivers/CoreDAC/include/interface/STM32HalBasicTimer.h
@@ -0,0 +1,27 @@
+// Leka - LekaOS
+// Copyright 2024 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "interface/drivers/STM32Hal.h"
+
+namespace leka::interface {
+
+class STM32HalBasicTimer
+{
+  public:
+	virtual ~STM32HalBasicTimer() = default;
+
+	[[nodiscard]] virtual auto getHandle() -> TIM_HandleTypeDef & = 0;
+
+	virtual void linkDACTimer(DAC_ChannelConfTypeDef *config) = 0;
+
+	virtual void initialize(float frequency) = 0;
+	virtual void terminate()				 = 0;
+
+	virtual void start() = 0;
+	virtual void stop()	 = 0;
+};
+
+}	// namespace leka::interface
---
+++ drivers/CoreDAC/tests/mocks/STM32HalTimer.h
@@ -0,0 +1,26 @@
+// Leka - LekaOS
+// Copyright 2024 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "gmock/gmock.h"
+#include "interface/STM32HalBasicTimer.h"
+
+namespace leka::mock {
+
+class STM32HalTimer : public interface::STM32HalBasicTimer
+{
+  public:
+	MOCK_METHOD(TIM_HandleTypeDef &, getHandle, (), (override));
+
+	MOCK_METHOD(void, linkDACTimer, (DAC_ChannelConfTypeDef *), (override));
+
+	MOCK_METHOD(void, initialize, (float), (override));
+	MOCK_METHOD(void, terminate, (), (override));
+
+	MOCK_METHOD(void, start, (), (override));
+	MOCK_METHOD(void, stop, (), (override));
+};
+
+}	// namespace leka::mock
