44e1993aba085615d0a0a04067ffa1e42bfc403c
Yann Locatelli <yann.locatelli2@gmail.com>
2024-03-01 17:10:00 +0100

    :sparkles: (dac): Add CoreDAC
    Co-Authored-By: Maxime Blanc <66126094+aermanio@users.noreply.github.com>
    Co-Authored-By: SamHadjes <48731907+samhadjes@users.noreply.github.com>

--- drivers/CoreDAC/source/HAL_IRQHandlers.cpp
+++ drivers/CoreDAC/source/HAL_IRQHandlers.cpp
@@ -1,10 +1,12 @@
+#include "CoreDAC.h"
 #include "CoreSTM32HalBasicTimer.h"
 
 extern "C" {
 
 namespace audio::internal {
 	extern leka::CoreSTM32HalBasicTimer hal_timer;
-}
+	extern leka::CoreDAC coredac;
+}	// namespace audio::internal
 
 void TIM6_DAC_IRQHandler()
 {
@@ -14,6 +16,11 @@ void TIM6_DAC_IRQHandler()
 void TIM7_DAC_IRQHandler()
 {
 	HAL_TIM_IRQHandler(&audio::internal::hal_timer.getHandle());
+}
+
+void DMA1_Stream5_IRQHandler()
+{
+	HAL_DMA_IRQHandler(audio::internal::coredac.getHandle().DMA_Handle1);
 }
 
 }	// extern "C"
