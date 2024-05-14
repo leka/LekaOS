8cc36c5993e4c74c6d5eefeb5a1fddd3dfaa188e
Ladislas de Toldi <ladislas@detoldi.me>
2024-05-14 11:47:15 +0200

    🔊 (BLEKit): CoreGap - Log LE_DATA_PACKET_LENGTH_EXTENSION support status

--- libs/BLEKit/source/CoreGap.cpp
+++ libs/BLEKit/source/CoreGap.cpp
@@ -2,8 +2,10 @@
 // Copyright 2022 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
+// #include <BLETypes.h>
 #include "CoreGap.h"
 
+#include "LogKit.h"
 #include "internal/ServicesCharacteristics.h"
 
 using namespace leka;
@@ -79,6 +81,9 @@ void CoreGap::onConnectionCallback(const std::function<void()> &callback)
 	_on_connection_callback = [&, callback](connection_handle_t handle) {
 		updateConnectionParameters(handle);
 		callback();
+		auto is_dple_supported =
+			_gap.isFeatureSupported(ble::controller_supported_features_t::LE_DATA_PACKET_LENGTH_EXTENSION);
+		log_info("DPLE supported: %s", is_dple_supported ? "true" : "false");
 	};
 	_gap_event_handler.onConnectionCallback(_on_connection_callback);
 }
