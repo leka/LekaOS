6d5bdab3c58f1b2d7154fb4daa684ec5cbea7f14
Ladislas de Toldi <ladislas@detoldi.me>
2024-05-14 12:07:37 +0200

    ✏️ (BLEKit): BLEServiceMonitoring - Format, add missing _

--- libs/BLEKit/include/BLEServiceMonitoring.h
+++ libs/BLEKit/include/BLEServiceMonitoring.h
@@ -23,28 +23,28 @@ class BLEServiceMonitoring : public interface::BLEService
 
 	void setChargingStatus(bool value)
 	{
-		charging_status = static_cast<uint8_t>(value);
+		_charging_status = static_cast<uint8_t>(value);
 
-		auto data = std::make_tuple(_charging_status_characteristic.getValueHandle(), std::span(&charging_status, 1));
+		auto data = std::make_tuple(_charging_status_characteristic.getValueHandle(), std::span(&_charging_status, 1));
 		sendData(data);
 	}
 
-	auto isScreensaverEnable() const -> bool { return screensaver_enable; }
+	auto isScreensaverEnable() const -> bool { return _screensaver_enable; }
 
 	void onDataReceived(const data_received_handle_t &params) final
 	{
-		if (params.handle == screensaver_enable_characteristic.getValueHandle()) {
-			screensaver_enable = static_cast<bool>(params.data[0]);
+		if (params.handle == _screensaver_enable_characteristic.getValueHandle()) {
+			_screensaver_enable = static_cast<bool>(params.data[0]);
 		}
-		if (params.handle == soft_reboot_characteristic.getValueHandle()) {
-			soft_reboot = static_cast<bool>(params.data[0]);
-			if (soft_reboot && _on_soft_reboot) {
+		if (params.handle == _soft_reboot_characteristic.getValueHandle()) {
+			_soft_reboot = static_cast<bool>(params.data[0]);
+			if (_soft_reboot && _on_soft_reboot) {
 				_on_soft_reboot();
 			}
 		}
-		if (params.handle == hard_reboot_characteristic.getValueHandle()) {
-			hard_reboot = static_cast<bool>(params.data[0]);
-			if (hard_reboot) {
+		if (params.handle == _hard_reboot_characteristic.getValueHandle()) {
+			_hard_reboot = static_cast<bool>(params.data[0]);
+			if (_hard_reboot) {
 				system_reset();
 			}
 		}
@@ -58,27 +58,39 @@ class BLEServiceMonitoring : public interface::BLEService
 	}
 
   private:
-	uint8_t charging_status {0x00};
+	uint8_t _charging_status {0x00};
 	ReadOnlyGattCharacteristic<uint8_t> _charging_status_characteristic {
-		service::monitoring::characteristic::charging_status, &charging_status,
-		GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY};
+		service::monitoring::characteristic::charging_status,
+		&_charging_status,
+		GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY,
+	};
 
-	bool screensaver_enable {true};
-	WriteOnlyGattCharacteristic<bool> screensaver_enable_characteristic {
-		service::monitoring::characteristic::screensaver_enable, &screensaver_enable};
+	bool _screensaver_enable {true};
+	WriteOnlyGattCharacteristic<bool> _screensaver_enable_characteristic {
+		service::monitoring::characteristic::screensaver_enable,
+		&_screensaver_enable,
+	};
 
-	bool soft_reboot {false};
-	WriteOnlyGattCharacteristic<bool> soft_reboot_characteristic {service::monitoring::characteristic::soft_reboot,
-																  &soft_reboot};
-	std::function<void()> _on_soft_reboot {};
+	bool _soft_reboot {false};
+	WriteOnlyGattCharacteristic<bool> _soft_reboot_characteristic {
+		service::monitoring::characteristic::soft_reboot,
+		&_soft_reboot,
 
-	bool hard_reboot {false};
-	WriteOnlyGattCharacteristic<bool> hard_reboot_characteristic {service::monitoring::characteristic::hard_reboot,
-																  &hard_reboot};
+	};
+	std::function<void()> _on_soft_reboot;
+
+	bool _hard_reboot {false};
+	WriteOnlyGattCharacteristic<bool> _hard_reboot_characteristic {
+		service::monitoring::characteristic::hard_reboot,
+		&_hard_reboot,
+	};
 
 	std::array<GattCharacteristic *, 4> _characteristic_table {
-		&_charging_status_characteristic, &screensaver_enable_characteristic, &soft_reboot_characteristic,
-		&hard_reboot_characteristic};
+		&_charging_status_characteristic,
+		&_screensaver_enable_characteristic,
+		&_soft_reboot_characteristic,
+		&_hard_reboot_characteristic,
+	};
 };
 
 }	// namespace leka
